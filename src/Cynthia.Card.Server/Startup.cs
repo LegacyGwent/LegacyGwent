2f78222ac5216073206b442e154317160b8b06ae
Cynthia <172587349@qq.com>
2018-06-11 23:18:12 +0800

    试着改了个小地方

--- src/Cynthia.Card.Server/Startup.cs
+++ src/Cynthia.Card.Server/Startup.cs
@@ -57,7 +57,7 @@ namespace Cynthia.Card.Server
             }
             app.UseSignalR(routes =>
             {
-                routes.MapHub<TestHub>("hub/test");
+                routes.MapHub<TestHub>("/hub/test");
             });
 
             app.UseHttpsRedirection();
