dec5cd08699bb82508b93eb276464271519c1dae
Cynthia <172587349@qq.com>
2018-07-25 07:13:45 +0800

    稍微写了一下客户端

---
+++ src/Cynthia.Card.Server/GwentServerModels/GwentServerGame.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Cynthia.Card.Common;
+
+namespace Cynthia.Card.Server
+{
+    public class GwentServerGame
+    {
+        private GwentServerPlayer _player1;
+        private GwentServerPlayer _player2;
+
+        public GwentServerGame(GwentServerPlayer player1, GwentServerPlayer player2)
+        {
+            _player1 = player1;
+            _player2 = player2;
+        }
+
+        public async Task<bool> Play()
+        {
+            await _player1.ServerOperation(Operation.Create<ServerOperationType>(ServerOperationType.GameStart, "aaa"));
+            return true;
+        }
+    }
+}
