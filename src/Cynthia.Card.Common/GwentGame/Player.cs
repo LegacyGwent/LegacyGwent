1cb8368fbf50c4c47f5ab6cbb987b197e05e69c8
Cynthia <172587349@qq.com>
2018-07-30 13:39:27 +0800

    修正bug

--- src/Cynthia.Card.Common/GwentGame/Player.cs
+++ src/Cynthia.Card.Common/GwentGame/Player.cs
@@ -23,8 +23,8 @@ namespace Cynthia.Card.Common
         public async Task SendToDownstreamAsync(Operation<ServerOperationType> operation) => await _upstream.SendAsync(operation);
         public async Task SendToUpstreamAsync(UserOperationType type, params object[] data) => await _downstream.SendAsync(Operation.Create(type, data));
         public async Task SendToDownstreamAsync(ServerOperationType type, params object[] data) => await _upstream.SendAsync(Operation.Create(type, data));
-        public async Task<Operation<ServerOperationType>> ReceiveFromUpstreamAsync() => (await _upstream.ReceiveAsync<Operation<ServerOperationType>>()).Result;
-        public async Task<Operation<UserOperationType>> ReceiveFromDownstreamAsync() => (await _downstream.ReceiveAsync<Operation<UserOperationType>>()).Result;
+        public async Task<Operation<ServerOperationType>> ReceiveFromUpstreamAsync() => (await _downstream.ReceiveAsync<Operation<ServerOperationType>>()).Result;
+        public async Task<Operation<UserOperationType>> ReceiveFromDownstreamAsync() => (await _upstream.ReceiveAsync<Operation<UserOperationType>>()).Result;
         public event Func<object, Task> ReceiveFromUpstream { add => _downstream.Receive += value; remove => _downstream.Receive -= value; }
         public event Func<object, Task> ReceiveFromDownstream { add => _upstream.Receive += value; remove => _upstream.Receive -= value; }
     }
