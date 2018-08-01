b2c8fa2c4ef3cc2030ba201131229c9ba9373125
Cynthia <172587349@qq.com>
2018-08-01 17:27:00 +0800

    进行了一次数值修正

--- src/Cynthia.Card.Common/GwentGame/DeckModel.cs
+++ src/Cynthia.Card.Common/GwentGame/DeckModel.cs
@@ -2,6 +2,7 @@ namespace Cynthia.Card
 {
     public class DeckModel
     {
+        public string Name { get; set; }
         public string[] Deck { get; set; }
         public string Leader { get; set; }
     }
