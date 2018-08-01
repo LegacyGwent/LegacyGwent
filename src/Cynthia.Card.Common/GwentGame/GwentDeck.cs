6e77be678fba49ec54af564737db25f941e19a04
Cynthia <172587349@qq.com>
2018-08-01 17:27:00 +0800

    进行了一次数值修正

--- src/Cynthia.Card.Common/GwentGame/GwentDeck.cs
+++ src/Cynthia.Card.Common/GwentGame/GwentDeck.cs
@@ -10,10 +10,10 @@ namespace Cynthia.Card
         public static DeckModel CreateBasicDeck()
         {
             var leader = "tl";
-            var deck = "tg".Plural(15)
+            var deck = "tg".Plural(4)
                 .Concat("ts".Plural(6))
-                .Concat("tc".Plural(4)).ToArray();
-            return new DeckModel() { Leader = leader, Deck = deck };
+                .Concat("tc".Plural(15)).ToArray();
+            return new DeckModel() { Leader = leader, Deck = deck, Name = "初始卡组" };
         }
     }
 }
