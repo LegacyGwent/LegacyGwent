5a6b1e38c435d8713bdc230ac37f197c447249b0
Cynthia <172587349@qq.com>
2018-08-16 07:12:17 +0800

    更新了一下游戏卡牌结构和游戏初始化信息

--- src/Cynthia.Card.Common/Models/GameCard.cs
+++ src/Cynthia.Card.Common/Models/GameCard.cs
@@ -9,12 +9,19 @@ namespace Cynthia.Card
         public GwentCard CardInfo { get; set; }
         //---------------------------------------
         //卡牌状态
-        public bool Visible { get; set; } = false;//是否被揭示 | 手牌
+        public bool IsReveal { get; set; } = false;//是否被揭示 | 手牌
         public bool IsShield { get; set; } = false;//是否昆恩 | 手牌,场地
+        public bool IsSpying { get; set; } = false;//是否间谍 | 场地
+        public bool IsResilience { get; set; } = false;//是否坚韧 | 场地
         public int Armor { get; set; } = 0;//护甲 | 场地
         public int Strength { get; set; }//战力 | 手牌,场地,墓地
         public int HealthStatus { get; set; } = 0;//增益减益 | 手牌,场地
         public bool IsLock { get; set; } = false;//是否锁定 | 场地,墓地
         public bool Conceal { get; set; } = false;//是否盖牌 | 场地
+        //------------------------------------------
+        //显示相关
+        public bool IsGray { get; set; } = true;
+        public bool IsCardBack { get; set; } = false;
+        public Faction DeckFaction { get; set; } = Faction.Monsters;
     }
 }
