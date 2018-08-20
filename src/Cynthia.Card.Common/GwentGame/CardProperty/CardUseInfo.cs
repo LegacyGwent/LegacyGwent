37cc9b89256fe2556daf002732c409e72ce4d5a8
Cynthia <172587349@qq.com>
2018-08-20 11:42:00 +0800

    .

--- src/Cynthia.Card.Common/GwentGame/CardProperty/CardUseInfo.cs
+++ src/Cynthia.Card.Common/GwentGame/CardProperty/CardUseInfo.cs
@@ -2,6 +2,7 @@ namespace Cynthia.Card
 {
     public enum CardUseInfo
     {
+        ReSet,
         MyPlace,
         EnemyPlace,
         AnyPlace,
--- test/Cynthia.Card.Test/Program.cs
+++ test/Cynthia.Card.Test/Program.cs
@@ -15,36 +15,10 @@ namespace Cynthia.Card.Test
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("小剧场");
-            Console.WriteLine("-------Start-------");
-            Console.WriteLine("路人A:唔...稍微有个问题不会呢,但是有很多工作呀,去问问琪露诺好了");
-            Task work = BakaAdd(1, 2);//把问题交给琪露诺去完成
-            Console.WriteLine("(路人A工作中)");
-            Console.WriteLine("(路人A工作中)");//琪露诺思考的过程,可以做一些其他事情
-            Console.WriteLine("(路人A工作中)");
-            Console.WriteLine("路人A:嗯...这里的工作需要刚刚哪个问题的结果呢,去等琪露诺做好吧!");
-            await work;//等待琪露诺的思考
-            Console.WriteLine("--------END--------");
+            await Task.Delay(0);
+            var a = new int[] { 0, 1, 2, 3 };
+            Console.WriteLine();
             Console.ReadKey();
-        }
-        public async static Task BakaAdd(int a, int b)
-        {
-            Console.WriteLine($"琪露诺:{a}+{b}等于多少呢....我要好好想想...");
-            await Task.Delay(1000);//琪露诺思考中....
-            Console.WriteLine($"琪露诺:啊!{a}+{b}等于⑨呀");
-        }
-        public static IEnumerable<object> Test(params object[] model)
-        {
-            return model;
-        }
-        public static UserInfo Login(string username, string password)
-        {
-            var users = new List<UserInfo>();
-            users.Add(new UserInfo() { UserName = "gezi", PassWord = "233", PlayerName = "baka" });
-            users.Add(new UserInfo() { UserName = "hfzi", PassWord = "466", PlayerName = "huaka" });
-            users.Add(new UserInfo() { UserName = "itzi", PassWord = "699", PlayerName = "ayay" });
-            var user = users.Where(x => x.UserName == username && x.PassWord == password).ToArray();
-            return user.Length == 0 ? null : user[0];
         }
         private static IDictionary<Group, string> FlavorMap { get; } = new Dictionary<Group, string>
         {
