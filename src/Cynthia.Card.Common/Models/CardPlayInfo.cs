bfd6ef25739e4410749830ed6d68554ae0182fdb
Cynthia <172587349@qq.com>
2018-08-20 12:11:39 +0800

    加入了卡牌信息的数据类

---
+++ src/Cynthia.Card.Common/Models/CardPlayInfo.cs
@@ -0,0 +1,10 @@
+namespace Cynthia.Card
+{
+    //回合操作相关信息
+    public class CardPlayInfo
+    {
+        public int HandCardIndex { get; set; }
+        public int RowIndex { get; set; }
+        public int CardIndex { get; set; }
+    }
+}
