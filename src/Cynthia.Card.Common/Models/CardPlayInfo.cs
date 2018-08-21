3752b863333eaceff887ddbc45fb7869956ef42c
Cynthia <172587349@qq.com>
2018-08-21 12:38:09 +0800

    修改了一下

--- src/Cynthia.Card.Common/Models/CardPlayInfo.cs
+++ src/Cynthia.Card.Common/Models/CardPlayInfo.cs
@@ -1,8 +1,9 @@
 namespace Cynthia.Card
 {
     //回合操作相关信息
-    public class CardPlayInfo
+    public class RoundInfo
     {
+        public bool IsPass { get; set; }
         public int HandCardIndex { get; set; }
         public int RowIndex { get; set; }
         public int CardIndex { get; set; }
