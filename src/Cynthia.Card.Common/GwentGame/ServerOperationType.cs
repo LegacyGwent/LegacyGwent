34bbeec0c685b553dc83615bafb9c2ef76c84d68
Cynthia <172587349@qq.com>
2018-08-30 05:39:22 +0800

    加入了新的指令类型

--- src/Cynthia.Card.Common/GwentGame/ServerOperationType.cs
+++ src/Cynthia.Card.Common/GwentGame/ServerOperationType.cs
@@ -6,7 +6,9 @@ namespace Cynthia.Card
         GameInfomation,//游戏中推送信息
         GetDragOrPass,//告诉玩家回合开始
         RoundEnd,//告诉玩家回合结束
-        CardUseEnd,//告诉玩家卡牌可以落下了
+        MyCardEffectEnd,//告诉玩家我方卡牌效果结束
+        EnemyCardEffectEnd,//告诉玩家敌方卡牌效果结束
+        EnemyCardDrag,//告诉玩家敌方拖拽卡牌
         SelectPlaceCard,//选择场上一些单位
         SelectAnotherCard,//选择一些其他卡
         GameEnd//告诉玩家游戏结束
