754570fd927d33aa6563909b1b41cf75370f56aa
Cynthia <172587349@qq.com>
2018-09-07 18:22:30 +0800

    表示所有卡牌坐标的位置

---
+++ src/Cynthia.Card.Common/Models/RowPosition.cs
@@ -0,0 +1,20 @@
+namespace Cynthia.Card
+{
+    public enum RowPosition
+    {
+        MyRow1,
+        MyRow2,
+        MyRow3,
+        EnemyRow1,
+        EnemyRow2,
+        EnemyRow3,
+        MyHand,
+        EnemyHand,
+        MyStay,
+        EnemyStay,
+        MyDeck,
+        EnemyDeck,
+        MyCemetery,
+        EnemyCemetery
+    }
+}
