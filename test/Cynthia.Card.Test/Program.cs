5a6539c62bfa8afc6ea9a3a0c580147b1c150bb7
Cynthia <172587349@qq.com>
2018-09-10 15:12:52 +0800

    增加调度相关的指令

--- test/Cynthia.Card.Test/Program.cs
+++ test/Cynthia.Card.Test/Program.cs
@@ -13,27 +13,14 @@ namespace Cynthia.Card.Test
 {
     class Program
     {
-        static List<Task> TaskList = new List<Task>();
         static async Task Main(string[] args)
         {
             await Task.Delay(0);
-            _ = TaskTest(1000, "msg1");
-            _ = TaskTest(800, "msg2");
-            _ = TaskTest(600, "msg3");
-            _ = TaskTest(400, "msg4");
-            _ = TaskTest(200, "msg5");
-            _ = TaskTest(800, "msg6");
-            Console.ReadKey();
         }
-        static async Task TaskTest(int delay, object message)
+
+        public static void ShowDebugMessage()
         {
-            await Task.Delay(delay);
-            Console.WriteLine(message);
-        }
-        static async void TaskListTask(int delay, object message)
-        {
-            await Task.Delay(0);
-            TaskList.Add(TaskTest(delay, message));
+
         }
     }
 }
