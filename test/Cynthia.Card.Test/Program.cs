180a578c13caa43a39bac330826414cf3a6f3acd
Cynthia <172587349@qq.com>
2018-10-01 09:25:53 +0800

    指令的修正,和数据包的完善

--- test/Cynthia.Card.Test/Program.cs
+++ test/Cynthia.Card.Test/Program.cs
@@ -18,9 +18,9 @@ namespace Cynthia.Card.Test
             await Task.CompletedTask;
             var list = new List<int>() { 1, 8, 4, 5, 6, 7, 9, 4, 3, 1, 5, 7, 6, 4, 5 };
             var list2 = new List<int>() { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
-            var all = new List<IList<int>>() { list, list2 };
-            //list2.TakeWhile(x => x >= 3).ForAll(System.Console.WriteLine);//取到不符合标注为止
-            //list2.SkipWhile(x => x == 3).ForAll(System.Console.WriteLine);//检测到第一个为真,返回后面的
+            //var all = new List<IList<int>>() { list, list2 };
+            list2.TakeWhile(x => x >= 3).ForAll(x => System.Console.Write("take" + x + "."));
+            list2.SkipWhile(x => x >= 3).ForAll(x => System.Console.Write("skip" + x + "."));
 
             Console.ReadLine();
         }
