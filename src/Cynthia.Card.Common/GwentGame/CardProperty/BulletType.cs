c15b3a1ca06a45f92cac2f4fc813e812f16df854
Cynthia <172587349@qq.com>
2018-10-16 04:09:01 +0800

    设置数值变化以及弹道动画

---
+++ src/Cynthia.Card.Common/GwentGame/CardProperty/BulletType.cs
@@ -0,0 +1,11 @@
+namespace Cynthia.Card
+{
+    public enum BulletType//两张卡之间发射的弹道类型
+    {
+        Arrow,//弧线箭
+        GreenLight,//绿增益光球
+        RedLight,//红减益光球
+        FireBall,//火球
+        Lightnint,//闪电
+    }
+}
