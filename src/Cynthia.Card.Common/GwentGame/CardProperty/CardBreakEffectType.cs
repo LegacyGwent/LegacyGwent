afb33f39b91f85a279bf114f0c98335cad24826a
Cynthia <172587349@qq.com>
2018-10-19 06:49:55 +0800

    删掉了一些注释掉的老代码,添加了新的动画指令

---
+++ src/Cynthia.Card.Common/GwentGame/CardProperty/CardBreakEffectType.cs
@@ -0,0 +1,10 @@
+namespace Cynthia.Card
+{
+    public enum CardBreakEffectType
+    {//直接从场上消失的死亡特效类型
+        Banish,//放逐
+        Consume,//吞噬
+        Burn,//焚烧
+        Plague,//瘟疫
+    }
+}
---
+++ src/Cynthia.Card.Common/GwentGame/CardProperty/CardIconEffectType.cs
@@ -0,0 +1,8 @@
+namespace Cynthia.Card
+{
+    public enum CardIconEffectType
+    {//卡牌展示动画
+        MendArmor,//修补护甲
+        BreakArmor//破坏护甲
+    }
+}
