f33919de194f3e6b84d447228dac64f205e131fd
DeusSeuca <1725877349@qq.com>
2018-11-18 11:20:30 +0800

    准备给卡牌加id

--- src/Cynthia.Card.Common/GwentGame/DeckModel.cs
+++ src/Cynthia.Card.Common/GwentGame/DeckModel.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Cynthia.Card
 {
     public class DeckModel
     {
+        [Key]
+        public string Id{get;set;}
         public string Name { get; set; }
         public string[] Deck { get; set; }
         public string Leader { get; set; }
