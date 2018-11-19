0a225b68b8c16e51e824131e90923503d8085ab1
DeusSeuca <1725877349@qq.com>
2018-11-19 12:10:31 +0800

    cardstatus

--- src/Cynthia.Card.Common/Models/CardStatus.cs
+++ src/Cynthia.Card.Common/Models/CardStatus.cs
@@ -4,7 +4,7 @@ namespace Cynthia.Card
     {
         public CardStatus(string cardIndex)
         {
-            CardId = cardIndex;
+            CardId = cardIndex;/*
             Strength = GwentMap.CardMap[cardIndex].Strength;
             Type = GwentMap.CardMap[cardIndex].CardType;
             IsDoomed = GwentMap.CardMap[cardIndex].IsDoomed;
@@ -12,9 +12,17 @@ namespace Cynthia.Card
             Countdown = GwentMap.CardMap[cardIndex].Countdown;
             CardArtsId = GwentMap.CardMap[cardIndex].CardArtsId;
             Group = GwentMap.CardMap[cardIndex].Group;
-            Faction = GwentMap.CardMap[cardIndex].Faction;
+            Faction = GwentMap.CardMap[cardIndex].Faction;*/
         }
-        public GwentCard CardInfo{get=>GwentMap.CardMap[CardId];}
+        private int? _strength;
+        private CardType? _type;
+        private bool? _isDoomed;
+        private bool? _isCountDown;
+        private int? _countDown;
+        private string _cardArtId;
+        private Group? _group;
+        private Faction? _faction;
+        //public GwentCard CardInfo{get=>GwentMap.CardMap[CardId];}
         public CardStatus()
         {
             IsCardBack = true;
@@ -22,12 +30,12 @@ namespace Cynthia.Card
         }
         //卡牌基本信息索引
         public string CardId { get; set; }
-        public string CardArtsId { get; set; }
-        public Group Group { get; set; }
+        public string CardArtsId { get=>_cardArtId??GwentMap.CardMap[CardId].CardArtsId; set=>_cardArtId=value; }
+        public Group Group { get=>_group??GwentMap.CardMap[CardId].Group; set=>_group=value; }
         //public GwentCard CardInfo { get; set; }
         public RowPosition CardRow { get; set; }
-        public bool IsDoomed { get; set; } = false;//是否佚亡
-        public CardType Type { get; set; }//法术还是单位
+        public bool IsDoomed { get=>_isDoomed??GwentMap.CardMap[CardId].IsDoomed; set=>_isDoomed=value; }//是否佚亡
+        public CardType Type { get=>_type??GwentMap.CardMap[CardId].CardType; set=>_type=value; }//法术还是单位
         //---------------------------------------------------------------
         //卡牌状态
         public bool IsReveal { get; set; } = false;//是否被揭示 | 手牌
@@ -35,7 +43,7 @@ namespace Cynthia.Card
         public bool IsSpying { get; set; } = false;//是否间谍 | 场地
         public bool IsResilience { get; set; } = false;//是否坚韧 | 场地
         public int Armor { get; set; } = 0;//护甲 | 场地
-        public int Strength { get; set; }//战力 | 手牌,场地,墓地
+        public int Strength { get=>_strength??GwentMap.CardMap[CardId].Strength; set=>_strength=value; }//战力 | 手牌,场地,墓地
         public int HealthStatus { get; set; } = 0;//增益减益 | 手牌,场地
         public bool IsLock { get; set; } = false;//是否锁定 | 场地,墓地
         public bool Conceal { get; set; } = false;//是否盖牌 | 场地
@@ -45,9 +53,9 @@ namespace Cynthia.Card
         //public bool IsGray { get; set; } = false;
         public bool IsCardBack { get; set; } = false;
         public Faction DeckFaction { get; set; } = Faction.Monsters;
-        public Faction Faction{get;set;}
+        public Faction Faction{get=>_faction??GwentMap.CardMap[CardId].Faction;set=>_faction=value;}
         //-----------------------------------------------------------------
-        public int Countdown = 0;
-        public bool IsCountdown = false;
+        public int Countdown{get=>_countDown??GwentMap.CardMap[CardId].Countdown;set=>_countDown=value;}
+        public bool IsCountdown{get=>_isCountDown??GwentMap.CardMap[CardId].IsCountdown;set=>_isCountDown=value;}
     }
 }
