22b09053a39a185d2bc7bbcdc5cd023a77334a10
DeusSeuca <1725877349@qq.com>
2018-11-20 21:00:14 +0800

    加个铁卫

--- test/Cynthia.Card.Test/Program.cs
+++ test/Cynthia.Card.Test/Program.cs
@@ -15,19 +15,8 @@ namespace Cynthia.Card.Test
     {
         public static async Task Main(string[] args)
         {
-            (IAsyncDataSender sender,IAsyncDataReceiver receiver) = AsyncDataEndPoint.CreateSimplex();
-            receiver.Receive+=async (x)=>
-            {
-                await Task.Delay(((int)x.Result)*500);
-                Console.WriteLine($"这是等待{((int)x.Result)}秒后输出的消息");
-            };
-            await sender.SendAsync<int>(5);
-            await sender.SendAsync<int>(4);
-            await sender.SendAsync<int>(3);
-            await sender.SendAsync<int>(2);
-            await sender.SendAsync<int>(3);
-            //Console.WriteLine($"接收到值:{await receiver.ReceiveAsync<int>()}");
-            //Console.WriteLine($"接收到值:{await receiver.ReceiveAsync<int>()}");
+
+            await Task.CompletedTask;
             Console.ReadLine();
         }
     }
