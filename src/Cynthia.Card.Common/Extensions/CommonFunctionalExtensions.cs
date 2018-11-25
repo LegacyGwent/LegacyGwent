834775fb38db343c7b2e894c1ca79871ab7a8bd1
DeusSeuca <1725877349@qq.com>
2018-11-25 16:42:16 +0800

    扩展方法

--- src/Cynthia.Card.Common/Extensions/CommonFunctionalExtensions.cs
+++ src/Cynthia.Card.Common/Extensions/CommonFunctionalExtensions.cs
@@ -5,7 +5,7 @@ namespace Cynthia.Card
 {
     public static class CommonFunctionalExtensions
     {
-        public static IEnumerable<(T,int)> Indexed<T>(this IEnumerable<T> source)
+        public static IEnumerable<(T item,int index)> Indexed<T>(this IEnumerable<T> source)
         {
             return source.Select((item,index)=>(item,index));
         }
