835d06a3629c9353047c71f9cc580d5c28274774
DeusSeuca <1725877349@qq.com>
2018-11-29 20:27:44 +0800

    add webclient

--- test/Cynthia.Card.Test/Program.cs
+++ test/Cynthia.Card.Test/Program.cs
@@ -9,6 +9,7 @@ using Newtonsoft.Json.Linq;
 using Alsein.Utilities.IO;
 using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.SignalR.Client;
+using System.Net;
 
 namespace Cynthia.Card.Test
 {
@@ -16,8 +17,9 @@ namespace Cynthia.Card.Test
     {
         static async Task Main(string[] args)
         {
+            var IP = Dns.GetHostEntry("cynthia.ovyno.com").AddressList[0];
             var client = new HubConnectionBuilder()
-                .WithUrl("http://us.ovyno.com:5000/hub/gwent").Build();
+                .WithUrl($"http://{IP}:5000/hub/gwent").Build();
             await client.StartAsync();
             Console.WriteLine($"注册结果为{await client.InvokeAsync<bool>("Register", "gezi", "123456", "格子")}");
             try
