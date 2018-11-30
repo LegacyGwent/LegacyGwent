12d471d2185611f1a721f9ab750038d0a29912d4
DeusSeuca <1725877349@qq.com>
2018-12-01 01:23:33 +0800

    cards

--- tools/Cynthia.Card.MakeCard/Program.cs
+++ tools/Cynthia.Card.MakeCard/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ExcelEi;
+using ExcelEi.Read;
+using OfficeOpenXml;
 
 namespace Cynthia.Card.MakeCard
 {
@@ -6,8 +11,21 @@ namespace Cynthia.Card.MakeCard
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var excelFilePath = "E:\\昆特效果\\中立效果.xlsx";
+            var package = new ExcelPackage(new FileInfo(excelFilePath));
+
+            var tableReader = ExcelTableReader.ReadContiguousTableWithHeader(package.Workbook.Worksheets[1], 1);
+
+            var pocoReader = new TableMappingReader<ExcelCard>();
+            //.Map(o => o.Name)
+            //.Map(o => o.Value);
+
+            IList<ExcelCard> pocoList = pocoReader.Read(tableReader);
             Console.ReadLine();
+        }
+        class ExcelCard
+        {
+
         }
     }
 }
