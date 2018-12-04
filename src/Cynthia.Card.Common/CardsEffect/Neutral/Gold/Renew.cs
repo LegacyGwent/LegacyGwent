1898ea2172d3ca334d0a59b532977afe9abbd048
DeusSeuca <1725877349@qq.com>
2018-12-04 13:44:14 +0800

    记得改回来复原

--- src/Cynthia.Card.Common/CardsEffect/Neutral/Gold/Renew.cs
+++ src/Cynthia.Card.Common/CardsEffect/Neutral/Gold/Renew.cs
@@ -10,7 +10,30 @@ namespace Cynthia.Card
 		public Renew(IGwentServerGame game, GameCard card) : base(game, card){}
 		public override async Task<int> CardUseEffect()
 		{
-			return 0;
+			//var damage = 4+(Game.PlayersCemetery[Card.PlayerIndex]);
+			var count = Game.PlayersDeck[Card.PlayerIndex].Count<3?Game.PlayersDeck[Card.PlayerIndex].Count:3;
+			var list = Game.PlayersDeck[Card.PlayerIndex].ToList();
+			for(var i = 0; i<count; i++)
+			{
+				await list[i].MoveToCardStayFirst();
+			}
+			return count;
+		}
+		public override async Task OnTurnStart(int playerIndex)
+		{
+			foreach(var card in Game.PlayersCemetery[Card.PlayerIndex])
+			{
+				await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck,0),card);
+			}
+			foreach(var card in Game.PlayersHandCard[Card.PlayerIndex])
+			{
+				if(card.Status.CardId!="14021")
+					await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck,0),card);
+			}
+			if(!Card.Status.CardRow.IsInHand())
+			{
+				await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck,0),Card);
+			}
 		}
 	}
 }
