6ef80106a26ed7b35509b31c791c80e0c52dbcbc
Cynthia <1725877349@qq.com>
2019-05-26 06:22:21 +0800

    bug?

--- src/Cynthia.Card.Server/Hubs/GwentHub.cs
+++ src/Cynthia.Card.Server/Hubs/GwentHub.cs
@@ -6,39 +6,40 @@ namespace Cynthia.Card.Server
 {
     public class GwentHub : Hub
     {
-        public GwentServerService GwentServerService { get; set; }
+        public GwentServerService _gwentServerService;
+        public GwentHub(GwentServerService gwentServerService) => _gwentServerService = gwentServerService;
 
         //注册
-        public bool Register(string username, string password, string playername) => GwentServerService.Register(username, password, playername);
+        public bool Register(string username, string password, string playername) => _gwentServerService.Register(username, password, playername);
 
         //登录
-        public async Task<UserInfo> Login(string username, string password) => await GwentServerService.Login(new User(username, Context.ConnectionId), password);
+        public async Task<UserInfo> Login(string username, string password) => await _gwentServerService.Login(new User(username, Context.ConnectionId), password);
 
         //上传卡组
-        public bool AddDeck(DeckModel deck) => GwentServerService.AddDeck(Context.ConnectionId, deck);
+        public bool AddDeck(DeckModel deck) => _gwentServerService.AddDeck(Context.ConnectionId, deck);
 
         //删除卡组
-        public bool RemoveDeck(string id) => GwentServerService.RemoveDeck(Context.ConnectionId, id);
+        public bool RemoveDeck(string id) => _gwentServerService.RemoveDeck(Context.ConnectionId, id);
 
         //修改卡组
-        public bool ModifyDeck(string id, DeckModel deck) => GwentServerService.ModifyDeck(Context.ConnectionId, id, deck);
+        public bool ModifyDeck(string id, DeckModel deck) => _gwentServerService.ModifyDeck(Context.ConnectionId, id, deck);
 
         //开始匹配
-        public bool Match(string deckId) => GwentServerService.Match(Context.ConnectionId, deckId);
+        public bool Match(string deckId) => _gwentServerService.Match(Context.ConnectionId, deckId);
 
         //停止匹配
-        public async Task<bool> StopMatch() => await GwentServerService.StopMatch(Context.ConnectionId);
+        public async Task<bool> StopMatch() => await _gwentServerService.StopMatch(Context.ConnectionId);
 
         //游戏内玩家操作
-        public Task GameOperation(Operation<UserOperationType> operation) => GwentServerService.GameOperation(operation, Context.ConnectionId);
+        public Task GameOperation(Operation<UserOperationType> operation) => _gwentServerService.GameOperation(operation, Context.ConnectionId);
 
         //重新连接
-        public async Task<bool> Reconnect(string username, string password)=> await GwentServerService.Reconnect(Context.ConnectionId,username,password);
+        public async Task<bool> Reconnect(string username, string password)=> await _gwentServerService.Reconnect(Context.ConnectionId,username,password);
         
         //连接中断
         public override Task OnDisconnectedAsync(Exception exception)
         {
-            return GwentServerService.Disconnect(Context.ConnectionId,exception,true);
+            return _gwentServerService.Disconnect(Context.ConnectionId,exception,true);
         }
     }
 }
--- src/Cynthia.Card.Server/Startup.cs
+++ src/Cynthia.Card.Server/Startup.cs
@@ -2,48 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Alsein.Utilities;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Cynthia.Card.MongoDB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Alsein.Utilities;
-using Autofac.Extensions.DependencyInjection;
-using Autofac;
+using Microsoft.Extensions.Hosting;
 using MongoDB.Driver;
 
-namespace Cynthia.Card.Server
-{
-    public class Startup
-    {
-        public IServiceProvider ConfigureServices(IServiceCollection services)
-        {
-            var container = default(IContainer);
-            var builder = new ContainerBuilder();
-            services.AddSignalR();
-            builder.Populate(services);
-            builder.RegisterType<MongoClient>()
-                .WithParameter("connectionString", "mongodb://cynthia.ovyno.com:27017/gwent")
-                .As<IMongoClient>()
-                .PropertiesAutowired()
-                .AsSelf();
-            builder.RegisterAll("Hub", option => option.ExternallyOwned());
-            builder.RegisterAllServices(option => option.PreserveExistingDefaults());
-            builder.Register(x => container).SingleInstance();
-            container = builder.Build();
-            //container.Resolve<InitializationService>().Start();
-            return new AutofacServiceProvider(container);
+namespace Cynthia.Card.Server {
+    public class Startup {
+        public void ConfigureServices (IServiceCollection services) {
+            //var container = default(IContainer);
+            //var builder = new ContainerBuilder();
+            services.AddSignalR ();
+            services.AddSingleton<GwentServerService> ();
+            services.AddSingleton<GwentDatabaseService> ();
+            services.AddSingleton<IDatabaseService, MongoDatabaseService> ();
+            services.AddSingleton<IMongoClient, MongoClient> ((ctx) => {
+                return new MongoClient ("mongodb://cynthia.ovyno.com:27017/gwent");
+            });
+            //services.AddAutofac(builder=> 
+            //{
+            //    builder.Populate(services);
+            //    builder.RegisterType<MongoClient>()
+            //        .WithParameter("connectionString", "mongodb://cynthia.ovyno.com:27017/gwent")
+            //        .As<IMongoClient>()
+            //        .PropertiesAutowired()
+            //        .AsSelf();
+            //    //builder.RegisterAll("Hub", option => option.ExternallyOwned());
+            //    builder.RegisterAllServices(option => option.PreserveExistingDefaults());
+            //    //builder.Register(x => container).SingleInstance();
+            //});
+            //container = builder.Build();
+            //return new AutofacServiceProvider(container);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<GwentHub>("/hub/gwent");
-                routes.MapHub<ChatHub>("/chathub");
+            app.UseSignalR (routes => {
+                routes.MapHub<GwentHub> ("/hub/gwent");
             });
         }
     }
