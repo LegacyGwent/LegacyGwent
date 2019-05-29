a5f690073fd8cedcc8ce18c41a3e1fee3d3fc854
Cynthia <1725877349@qq.com>
2019-05-30 03:34:26 +0800

    debug of Server

--- src/Cynthia.Card.Server/Program.cs
+++ src/Cynthia.Card.Server/Program.cs
@@ -11,16 +11,21 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Cynthia.Card.Server {
-    public class Program {
-        public static void Main (string[] args) {
-            CreateHostBuilder (args).Build ().Run ();
+namespace Cynthia.Card.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder (string[] args) =>
-            Host.CreateDefaultBuilder (args)
-            .ConfigureWebHostDefaults (webBuilder => {
-                webBuilder.UseStartup<Startup> ();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>()
+                .UseUrls("http://*:5000");
             });
     }
 }
