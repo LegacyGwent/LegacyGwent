339ed71a2f1b7f24d35f6b7c3819f5cdad67c883
Cynthia <1725877349@qq.com>
2019-06-12 06:35:46 +0800

    Adding Soldier Stream Empire-related Single Card

--- src/Cynthia.Card.Common/CardsEffect/Nilfgaard/Copper/Recruit.cs
+++ src/Cynthia.Card.Common/CardsEffect/Nilfgaard/Copper/Recruit.cs
@@ -4,13 +4,18 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("34032")]//新兵
-	public class Recruit : CardEffect
-	{//从牌组随机打出1张非同名“铜色”士兵牌。
-		public Recruit(IGwentServerGame game, GameCard card) : base(game, card){}
-		public override async Task<int> CardPlayEffect(bool isSpying)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("34032")]//新兵
+    public class Recruit : CardEffect
+    {//从牌组随机打出1张非同名“铜色”士兵牌。
+        public Recruit(IGwentServerGame game, GameCard card) : base(game, card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying)
+        {
+            var targets = Game.PlayersDeck[Card.PlayerIndex]
+                .Where(x => x.Status.Categories.Contains(Categorie.Soldier) && x.Status.Group == Group.Copper && x.Status.CardId != CardId.Recruit)
+                .Mess();
+            if (targets.Count() == 0) return 0;
+            await targets.First().MoveToCardStayFirst();
+            return 1;
+        }
+    }
 }
--- src/Cynthia.Card.Common/CardsEffect/Nilfgaard/Silver/Vreemde.cs
+++ src/Cynthia.Card.Common/CardsEffect/Nilfgaard/Silver/Vreemde.cs
@@ -4,13 +4,20 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("33016")]//弗林姆德
-	public class Vreemde : CardEffect
-	{//创造1个铜色尼弗迦德“士兵”单位。
-		public Vreemde(IGwentServerGame game, GameCard card) : base(game, card){}
-		public override async Task<int> CardPlayEffect(bool isSpying)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("33016")]//弗林姆德
+    public class Vreemde : CardEffect
+    {//创造1个铜色尼弗迦德“士兵”单位。
+        public Vreemde(IGwentServerGame game, GameCard card) : base(game, card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying)
+        {
+            return await Card.CreateAndMoveStay(
+                GwentMap.GetCreateCardsId(
+                    x => x.Faction == Faction.Nilfgaard &&
+                    (x.Group == Group.Copper) &&
+                    (x.Categories.Contains(Categorie.Soldier))
+                )
+                .ToList()
+            );
+        }
+    }
 }
