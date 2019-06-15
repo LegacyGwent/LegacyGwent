3d446e2476d92f35a11b7e9ce568e20eb8484e51
Jasper <450213487@qq.com>
2019-06-15 15:28:38 +0800

    HalfElfHunter

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/HalfElfHunter.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/HalfElfHunter.cs
@@ -4,13 +4,24 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("54017")]//半精灵猎人
-	public class HalfElfHunter : CardEffect
-	{//生成1张佚亡原始同名牌。
-		public HalfElfHunter(IGwentServerGame game, GameCard card) : base(game, card){}
-		public override async Task<int> CardPlayEffect(bool isSpying)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("54017")] //半精灵猎人
+    public class HalfElfHunter : CardEffect
+    {
+        //生成1张佚亡原始同名牌。
+        public HalfElfHunter(IGwentServerGame game, GameCard card) : base(game, card)
+        {
+        }
+
+        public override async Task<int> CardPlayEffect(bool isSpying)
+        {
+            var position = Card.GetLocation();
+            await Game.CreateCard(Card.Status.CardId, PlayerIndex, position, setting: ToDoomed);
+            return 0;
+        }
+
+        private void ToDoomed(CardStatus status)
+        {
+            status.IsDoomed = true;
+        }
+    }
 }
