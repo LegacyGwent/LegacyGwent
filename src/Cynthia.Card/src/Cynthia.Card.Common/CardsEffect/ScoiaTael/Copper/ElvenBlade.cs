c00792a899b2e868d0c0c79176ef2a63c06fdae1
Jasper <450213487@qq.com>
2019-06-15 15:27:09 +0800

    ElvenBlade

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/ElvenBlade.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/ElvenBlade.cs
@@ -4,13 +4,33 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("54031")]//精灵利剑
-	public class ElvenBlade : CardEffect
-	{//对1个非“精灵”单位造成10点伤害。
-		public ElvenBlade(IGwentServerGame game, GameCard card) : base(game, card){}
-		public override async Task<int> CardUseEffect()
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("54031")] //精灵利剑
+    public class ElvenBlade : CardEffect
+    {
+        //对1个非“精灵”单位造成10点伤害。
+        public ElvenBlade(IGwentServerGame game, GameCard card) : base(game, card)
+        {
+        }
+
+        public override async Task<int> CardUseEffect()
+        {
+            var list = await Game.GetSelectPlaceCards(Card, Sizer: isNotElf);
+            if (list.Count <= 0) return 0;
+            var card = list.Single();
+            await card.Effect.Damage(damage);
+            if (card.Status.CardRow.IsOnPlace())
+            {
+                await Card.Effect.Boost(damage);
+            }
+
+            return 0;
+        }
+
+        private bool isNotElf(GameCard card)
+        {
+            return !card.CardInfo().Categories.Contains(Categorie.Elf);
+        }
+
+        private const int damage = 10;
+    }
 }
