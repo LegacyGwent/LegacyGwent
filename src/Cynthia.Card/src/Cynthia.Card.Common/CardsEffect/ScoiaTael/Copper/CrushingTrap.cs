4138a71055910d96b6844874d80109d34de09bf6
Jasper <450213487@qq.com>
2019-06-15 10:28:15 +0800

    CrushingTrap

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/CrushingTrap.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/CrushingTrap.cs
@@ -9,8 +9,19 @@ namespace Cynthia.Card
 	{//使对方单排左右两侧末端的单位各受到6点伤害。
 		public CrushingTrap(IGwentServerGame game, GameCard card) : base(game, card){}
 		public override async Task<int> CardUseEffect()
-		{
-			return 0;
-		}
+        {
+            var rowIndex = await Game.GetSelectRow(Card.PlayerIndex,Card,
+				TurnType.Enemy.GetRow());
+            var row = Game.RowToList(Card.PlayerIndex, rowIndex).ToList();
+            if (row.Count <= 0)
+                return 0;
+            var cardLeft = row.First();
+            var cardRight = row.Last();
+            await cardLeft.Effect.Damage(damage);
+            if (cardLeft == cardRight) return 0;
+            await cardRight.Effect.Damage(damage);
+            return 0;
+        }
+        private const int damage = 6;
 	}
 }
