ced20977497c624b4eae555c7a22e3bd48a908f4
Jasper <450213487@qq.com>
2019-06-15 10:27:43 +0800

    BlueMountainElite

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/BlueMountainElite.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/BlueMountainElite.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Alsein.Extensions;
 
@@ -9,8 +10,28 @@ namespace Cynthia.Card
 	{//召唤所有同名牌。 自身移动时获得2点增益。
 		public BlueMountainElite(IGwentServerGame game, GameCard card) : base(game, card){}
 		public override async Task<int> CardPlayEffect(bool isSpying)
-		{
-			return 0;
+        {
+            var deck = Game.PlayersDeck[PlayerIndex];
+            var myId = Card.CardInfo().CardId;
+            var cardsToPlay = deck.Where(x => x.CardInfo().CardId == myId);
+            var list = cardsToPlay.ToList();
+            if (!list.Any()) return 0;
+            var position = Card.GetLocation();
+            foreach (var it in list)
+            {
+                await it.Effect.Summon(position, it);
+            }
+
+            return 0;
 		}
-	}
+
+        private const int boostCount = 2;
+        public override async Task OnCardMove(GameCard target, GameCard source = null)
+        {
+            if (target == Card)
+            {
+                await Card.Effect.Boost(boostCount);
+            }
+        }
+    }
 }
