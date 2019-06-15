bbae66cc338bf963291cd273b3cb56661ccb6ddc
neal2018 <neal.lion@icloud.com>
2019-06-15 22:24:05 +0800

    update cards. Note: Maerolorn and BrewessRitual need to be improve after category updated

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/Neutral/Gold/Phoenix.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/Neutral/Gold/Phoenix.cs
@@ -10,7 +10,23 @@ namespace Cynthia.Card
 		public Phoenix(IGwentServerGame game, GameCard card) : base(game, card){}
 		public override async Task<int> CardPlayEffect(bool isSpying)
 		{
-			return 0;
+			var list = Game.PlayersCemetery[PlayerIndex]
+	            .Where(x => (x.Categories.Contains(Categorie.Draconid))
+	            	&& (x.Status.Group == Group.Silver || x.Status.Group == Group.Silver)).Mess();
+
+            //让玩家选择一张卡
+            var result = await Game.GetSelectMenuCards
+                (Card.PlayerIndex, list.ToList(), 1, "选择复活一张牌");
+
+            //如果玩家一张卡都没选择,没有效果
+            if (result.Count() == 0){
+            	return 0;
+            } 
+
+            //打出
+            await result.First().Effect.Resurrect(new CardLocation() { RowPosition = RowPosition.MyStay, CardIndex = 0 }, Card);
+			
+			return 1;
 		}
 	}
 }
