5755d83a3a1468675bf9e78207435504340bddb4
AlseinX <xyh951115@live.com>
2019-06-16 06:26:11 +0800

    Add new project files

---
+++ src/LegacyGwent/test/LegacyGwent.ConsoleTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegacyGwent.ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
