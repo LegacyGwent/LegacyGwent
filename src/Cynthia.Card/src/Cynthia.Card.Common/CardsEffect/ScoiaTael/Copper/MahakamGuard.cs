f6c365926c4efc3423a648c937d8432fad3a2395
Jasper <450213487@qq.com>
2019-06-15 15:30:15 +0800

    MahakamGuard

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/MahakamGuard.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardsEffect/ScoiaTael/Copper/MahakamGuard.cs
@@ -4,13 +4,24 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("54022")]//玛哈坎守卫
-	public class MahakamGuard : CardEffect
-	{//使1个友军单位获得7点增益。
-		public MahakamGuard(IGwentServerGame game, GameCard card) : base(game, card){}
-		public override async Task<int> CardPlayEffect(bool isSpying)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("54022")] //玛哈坎守卫
+    public class MahakamGuard : CardEffect
+    {
+        //使1个友军单位获得7点增益。
+        public MahakamGuard(IGwentServerGame game, GameCard card) : base(game, card)
+        {
+        }
+
+        public override async Task<int> CardPlayEffect(bool isSpying)
+        {
+            var cards = await Game.GetSelectPlaceCards(Card, selectMode: SelectModeType.MyRow);
+            if (cards.Count <= 0) return 0;
+            var card = cards.Single();
+            await card.Effect.Boost(boost);
+
+            return 0;
+        }
+
+        private int boost = 7;
+    }
 }
