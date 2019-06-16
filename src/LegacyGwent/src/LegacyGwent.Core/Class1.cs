e27f99f4a0b242646f6866bcb8792898848a4dc9
AlseinX <xyh951115@live.com>
2019-06-16 08:24:20 +0800

    Add effect pool and demo

--- src/LegacyGwent/src/LegacyGwent.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LegacyGwent
-{
-    public class Class1
-    {
-    }
-}
--- src/LegacyGwent/test/LegacyGwent.ConsoleTester/Program.cs
+++ src/LegacyGwent/test/LegacyGwent.ConsoleTester/Program.cs
@@ -1,12 +1,92 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace LegacyGwent.ConsoleTester
 {
+    class BeforeDeath : Event
+    {
+        public bool IsCancelled { get; set; } = false;
+    }
+
+    class AfterDeath : Event
+    {
+
+    }
+
+    abstract class Unit
+    {
+        public abstract string Name { get; }
+
+        public IList<Effect> Effects { get; set; } = new List<Effect>();
+
+        public int Health { get; set; }
+
+        public async Task GetDamaged(int damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                var beforeDeath = new BeforeDeath();
+                await Effects.RaiseEvent(beforeDeath);
+                if (beforeDeath.IsCancelled)
+                {
+                    Console.WriteLine($"{Name} has escaped from death.");
+                    Health = 1;
+                }
+                else
+                {
+                    Console.WriteLine($"{Name} is dead.");
+                    await Effects.RaiseEvent<AfterDeath>();
+                }
+            }
+        }
+    }
+
+    class Tryndamere : Unit
+    {
+        public override string Name => "Tryndamere";
+
+        public Tryndamere()
+        {
+            Health = 10;
+            Effects.Add(new UndyingRage(this));
+        }
+    }
+
+    class UndyingRage : Effect, IHandlesEvent<BeforeDeath>
+    {
+        private const int _maxCount = 5;
+
+        private int count = _maxCount;
+
+        private readonly Tryndamere _target;
+
+        public UndyingRage(Tryndamere target)
+        {
+            _target = target;
+        }
+
+        public Task HandleEvent(BeforeDeath @event)
+        {
+            if (count > 0)
+            {
+                @event.IsCancelled = true;
+                count--;
+            }
+            return Task.CompletedTask;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var trynd = new Tryndamere();
+            while (trynd.Health > 0)
+            {
+                _ = trynd.GetDamaged(5);
+            }
         }
     }
 }
