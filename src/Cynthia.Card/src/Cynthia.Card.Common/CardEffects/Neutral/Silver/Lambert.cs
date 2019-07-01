ade0ed79a2408d5f8ca526161bcc01cf3d1cc475
Cynthia <1725877349@qq.com>
2019-07-01 18:23:55 +0800

    修正了艾斯卡尔和兰伯特贴图反的问题

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/Lambert.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/Lambert.cs
@@ -4,13 +4,24 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("13017")]//兰伯特
-	public class Lambert : CardEffect
-	{//召唤“维瑟米尔”和“艾斯卡尔”。
-		public Lambert(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("13017")]//兰伯特
+    public class Lambert : CardEffect
+    {//召唤“维瑟米尔”和“艾斯卡尔”。
+        public Lambert(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying)
+        {
+            var myDeck = Game.PlayersDeck[PlayerIndex].ToList();
+            var eskels = myDeck.Where(x => x.Status.CardId == CardId.Eskel).ToList();
+            var vesemirs = myDeck.Where(x => x.Status.CardId == CardId.Vesemir).ToList();
+            foreach (var eskel in eskels)
+            {
+                await eskel.Effect.Summon(Card.GetLocation() + 1, Card);
+            }
+            foreach (var vesemir in vesemirs)
+            {
+                await vesemir.Effect.Summon(Card.GetLocation() + 1, Card);
+            }
+            return 0;
+        }
+    }
 }
