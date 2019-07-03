2d87028220c395d13f2f19c1dc2c6670cb0e5aad
neal2018 <neal.lion@icloud.com>
2019-07-03 13:48:46 +0800

    Brewess

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Gold/BrewessRitual.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Gold/BrewessRitual.cs
@@ -4,13 +4,33 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("22014")]//煮婆：仪式
-	public class BrewessRitual : CardEffect
-	{//复活2个铜色遗愿单位。
-		public BrewessRitual(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("22014")]//煮婆：仪式
+    public class BrewessRitual : CardEffect
+    {//复活2个铜色遗愿单位。
+        public BrewessRitual(GameCard card) : base(card){ }
+        public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
+        {
+            var list = Game.PlayersCemetery[PlayerIndex]
+                .Where(x => x.CardInfo().Info.Contains("遗愿：")//<这个位置等待category更新后改进>
+                    && x.Status.Group == Group.Copper);
+                        
+            //让玩家选择两张铜色遗愿单位
+            var result = await Game.GetSelectMenuCards
+                (Card.PlayerIndex, list.ToList(), 2,isCanOver:true);
+
+            //如果玩家一张卡都没选择,没有效果
+            if (result.Count() == 0)
+            {
+                return 0;
+            }
+
+            //复活铜色遗愿单位
+            foreach(var card in result)
+            {
+                await card.Effect.Resurrect(new CardLocation() { RowPosition = RowPosition.MyStay, CardIndex = 0 }, Card);
+            }
+                  
+            return 2;
+        }
+    }
 }
