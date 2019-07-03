b74ceaa73addf7e1ab3df1a00f5b2a8ad8bfd216
neal2018 <neal.lion@icloud.com>
2019-07-03 13:07:17 +0800

    nekkerwarrior

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/NekkerWarrior.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/NekkerWarrior.cs
@@ -10,7 +10,17 @@ namespace Cynthia.Card
 		public NekkerWarrior(GameCard card) : base(card){}
 		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
 		{
-			return 0;
+			//选择1个友军铜色单位
+            var result = (await Game.GetSelectPlaceCards(Card, selectMode:SelectModeType.MyRow));
+
+            //将2张它的同名牌加入牌组底部
+            
+            for(var i = 0; i < 2; i++)
+            {
+            	await Game.CreateCard(result.Single().CardId, Card.PlayerIndex, 
+                    new CardLocation(RowPosition.MyDeck, Game.PlayersDeck[playerIndex].Count));
+            }
+            return 0;
 		}
 	}
 }
