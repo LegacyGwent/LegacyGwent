7c62d157d736134f0c6122c209805e92c37876dd
neal2018 <neal.lion@icloud.com>
2019-07-03 15:01:03 +0800

    Barbegazi

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/Barbegazi.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/Barbegazi.cs
@@ -7,9 +7,17 @@ namespace Cynthia.Card
 	[CardEffectId("24025")]//须岩怪
 	public class Barbegazi : CardEffect
 	{//坚韧。 吞噬1个友军单位，获得其战力作为增益。
-		public Barbegazi(GameCard card) : base(card){}
+		public Barbegazi(GameCard card) : base(card){ }
+
 		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
 		{
+            await Card.Effect.Resilience(Card);
+            
+            var card = (await Game.GetSelectPlaceCards(Card, selectMode: SelectModeType.MyRow)).SingleOrDefault();
+            if (card != default)
+            {
+                await Card.Effect.Consume(card);
+            }
 			return 0;
 		}
 	}
