ca7b50783c87c0888b206c2da74b2fb0d40d6ff3
neal2018 <neal.lion@icloud.com>
2019-07-03 15:14:55 +0800

    fix bug

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Gold/Phoenix.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Gold/Phoenix.cs
@@ -7,19 +7,21 @@ namespace Cynthia.Card
     [CardEffectId("12028")]//凤凰
     public class Phoenix : CardEffect
     {//复活1个铜色/银色“龙兽”单位。
-        public Phoenix(GameCard card) : base(card){}
+        public Phoenix(GameCard card) : base(card){ }
+        
         public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
         {
             var list = Game.PlayersCemetery[PlayerIndex]
                 .Where(x => (x.Categories.Contains(Categorie.Draconid))
-                    && (x.Status.Group == Group.Silver || x.Status.Group == Group.Silver)).Mess();
+                    && (x.Status.Group == Group.Silver || x.Status.Group == Group.Silver));
 
             //让玩家选择一张卡
             var result = await Game.GetSelectMenuCards
                 (Card.PlayerIndex, list.ToList(), 1, "选择复活一张牌");
 
             //如果玩家一张卡都没选择,没有效果
-            if (result.Count() == 0){
+            if (result.Count() == 0)
+            {
                 return 0;
             } 
 
