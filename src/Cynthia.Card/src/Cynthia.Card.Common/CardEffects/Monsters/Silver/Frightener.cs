629ee88f8e4c81b2507a747d839e99c9877772a8
neal2018 <neal.lion@icloud.com>
2019-07-03 15:17:09 +0800

    fix bug

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Silver/Frightener.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Silver/Frightener.cs
@@ -4,25 +4,25 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("23001")]//畏惧者
-	public class Frightener : CardEffect
-	{//间谍、力竭。 将1个敌军单位移至自身所在排，然后抽1张牌。
-		public Frightener(GameCard card) : base(card){ }
+    [CardEffectId("23001")]//畏惧者
+    public class Frightener : CardEffect
+    {//间谍、力竭。 将1个敌军单位移至自身所在排，然后抽1张牌。
+        public Frightener(GameCard card) : base(card){ }
         public bool IsUse = false;
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
+        public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
+        {
             if (IsUse) return 0;
             IsUse = true;
 
             var card = await Game.GetSelectPlaceCards(Card, x => x.Status.CardRow != Card.Status.CardRow, SelectModeType.MyRow);
-			if(card.Count<=0) 
+            if(card.Count<=0) 
             {
                 return 0;
             }
 
-            target = card.Single()
-            row = Card.Status.CardRow
-            population = Game.RowToList(target.PlayerIndex, row).Count
+            var target = card.Single();
+            var row = Card.Status.CardRow;
+            var population = Game.RowToList(target.PlayerIndex, row).Count;
 
             //满了的话
             if (population >= 9) 
@@ -35,6 +35,6 @@ namespace Cynthia.Card
             await Game.PlayerDrawCard(Game.AnotherPlayer(Card.PlayerIndex));//抽卡
             
             return 0;
-		}
-	}
+        }
+    }
 }
