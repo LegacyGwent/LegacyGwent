54a9f7d7d4ce82175c24fdd5b4ef440dbe2dc8d0
Cynthia <1725877349@qq.com>
2019-07-05 04:42:45 +0800

    添加'交换'事件与指令

--- src/Cynthia.Card/src/Cynthia.Card.Common/GameEvent/AfterCardSwap.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GameEvent/AfterCardSwap.cs
@@ -3,13 +3,11 @@ namespace Cynthia.Card
     //进行了一次交换之后
     public class AfterCardSwap : Event
     {
-        public GameCard Target { get; set; }
-        public GameCard Source { get; set; }
+        public GameCard HandCard { get; set; }
 
-        public AfterCardSwap(GameCard target, GameCard source)
+        public AfterCardSwap(GameCard handCard)
         {
-            Target = target;
-            Source = source;
+            HandCard = handCard;
         }
     }
 }
