3a1ed56234b6ca764eb4bf4945d9d7ea821ab487
neal2018 <neal.lion@icloud.com>
2019-07-05 15:23:24 +0800

    reslove conflicts

---
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Gold/GeEls.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alsein.Extensions;
+
+namespace Cynthia.Card
+{
+    [CardEffectId("22013")]//盖尔
+    public class GeEls : CardEffect
+    {//检视牌组中1张金色牌和银色牌，打出1张，将另1张置于牌组顶端。
+        public GeEls(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            return 0;
+        }
+    }
+}
