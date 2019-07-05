aac46ad5fdc6391ab67773686e4de205f0c18819
neal2018 <neal.lion@icloud.com>
2019-07-05 09:20:33 +0800

    同步凤凰

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Gold/Phoenix.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Gold/Phoenix.cs
@@ -7,27 +7,20 @@ namespace Cynthia.Card
     [CardEffectId("12028")]//凤凰
     public class Phoenix : CardEffect
     {//复活1个铜色/银色“龙兽”单位。
-        public Phoenix(GameCard card) : base(card){ }
-        
-        public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
+        public Phoenix(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
-            var list = Game.PlayersCemetery[PlayerIndex]
-                .Where(x => (x.Categories.Contains(Categorie.Draconid))
-                    && (x.Status.Group == Group.Silver || x.Status.Group == Group.Silver));
+            var cards = Game.PlayersCemetery[PlayerIndex].Where(x =>
+                x.Is(filter: x => x.IsAnyGroup(Group.Copper, Group.Silver) &&
+                x.HasAllCategorie(Categorie.Draconid),
+                type: CardType.Unit)).ToList();
 
-            //让玩家选择一张卡
-            var result = await Game.GetSelectMenuCards
-                (Card.PlayerIndex, list.ToList(), 1, "选择复活一张牌");
-
-            //如果玩家一张卡都没选择,没有效果
-            if (result.Count() == 0)
+            var selectCard = await Game.GetSelectMenuCards(PlayerIndex, cards);
+            if (!selectCard.TrySingle(out var target))
             {
                 return 0;
-            } 
-
-            //打出
-            await result.First().Effect.Resurrect(new CardLocation() { RowPosition = RowPosition.MyStay, CardIndex = 0 }, Card);
-            
+            }
+            await target.Effect.Resurrect(new CardLocation(RowPosition.MyStay, 0), Card);
             return 1;
         }
     }
