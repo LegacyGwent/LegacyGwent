14828683dd27c113d2787d32a286e26cd5891813
Cynthia <1725877349@qq.com>
2019-07-29 20:16:20 +0800

    添加了一些怪物阵营银卡

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Silver/Mourntart.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Silver/Mourntart.cs
@@ -4,13 +4,18 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("23017")]//莫恩塔特
-	public class Mourntart : CardEffect
-	{//吞噬己方墓场所有铜色/银色单位。每吞噬1个单位，便获得1点增益。
-		public Mourntart(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("23017")]//莫恩塔特
+    public class Mourntart : CardEffect
+    {//吞噬己方墓场所有铜色/银色单位。每吞噬1个单位，便获得1点增益。
+        public Mourntart(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            var cardlist = Game.PlayersCemetery[PlayerIndex].Where(x => x.IsAnyGroup(Group.Copper, Group.Silver)).ToList();
+            foreach (var target in cardlist)
+            {
+                await Consume(target, x => 1);
+            }
+            return 0;
+        }
+    }
 }
