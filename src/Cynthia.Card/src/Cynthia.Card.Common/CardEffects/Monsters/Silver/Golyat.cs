5c0baf988ee23eef4a8925db3e8624ac7171da29
Cynthia <1725877349@qq.com>
2019-07-29 20:16:20 +0800

    添加了一些怪物阵营银卡

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Silver/Golyat.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Silver/Golyat.cs
@@ -4,13 +4,18 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("23003")]//哥亚特
-	public class Golyat : CardEffect
-	{//获得7点增益。 每次被伤害时，额外承受2点伤害。
-		public Golyat(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("23003")]//哥亚特
+    public class Golyat : CardEffect, IHandlesEvent<AfterCardHeal>
+    {//获得7点增益。 每次被伤害时，额外承受2点伤害。
+        public Golyat(GameCard card) : base(card) { }
+
+        public async Task HandleEvent(AfterCardHeal @event)
+        {
+            if (@event.Target != Card || @event.Source == Card)
+            {
+                return;
+            }
+            await Card.Effect.Damage(2, Card);
+        }
+    }
 }
