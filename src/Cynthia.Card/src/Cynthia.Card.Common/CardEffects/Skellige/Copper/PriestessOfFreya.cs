270a3adfe015872c52a3e9b38b5c3a7d7b8395c1
shanzhoupeng <fff@pku.edu.cn>
2019-08-01 05:09:11 +0800

    0801joeshyn

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/PriestessOfFreya.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/PriestessOfFreya.cs
@@ -4,13 +4,26 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("64032")]//弗蕾雅女祭司
-	public class PriestessOfFreya : CardEffect
-	{//复活1个铜色“士兵”单位。
-		public PriestessOfFreya(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("64032")]//弗蕾雅女祭司
+    public class PriestessOfFreya : CardEffect
+    {//复活1个铜色“士兵”单位。
+        public PriestessOfFreya(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            //从我方墓地列出铜色“士兵单位”
+            var list = Game.PlayersCemetery[PlayerIndex].Where(x => x.Status.Group == Group.Copper && x.CardInfo().CardType == CardType.Unit && x.HasAllCategorie(Categorie.Soldier)).Mess();
+            if (list.Count() == 0)
+            {
+                return 0;
+            }
+
+            //让玩家选择一张卡
+            var result = await Game.GetSelectMenuCards
+            (Card.PlayerIndex, list.ToList(), 1, "选择复活一张牌");
+            //如果玩家一张卡都没选择,没有效果
+            if (result.Count() == 0) return 0;
+            await result.First().Effect.Resurrect(new CardLocation() { RowPosition = RowPosition.EnemyStay, CardIndex = 0 }, Card);
+            return 1;
+        }
+    }
 }
