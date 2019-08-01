e82de08fe659a141588008818b65914d09e46cd7
shanzhoupeng <fff@pku.edu.cn>
2019-08-01 22:50:10 +0800

    coral&wildboarofthesea&dimunlightlongship

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/DimunLightLongship.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/DimunLightLongship.cs
@@ -4,13 +4,27 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("64017")]//迪门家族轻型长船
-	public class DimunLightLongship : CardEffect
-	{//回合结束时，对右侧的单位造成1点伤害，自身获得2点增益。
-		public DimunLightLongship(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
+    [CardEffectId("64017")]//迪门家族轻型长船
+    public class DimunLightLongship : CardEffect, IHandlesEvent<AfterTurnOver>
+    {//回合结束时，对右侧的单位造成1点伤害，自身获得2点增益。
+        public DimunLightLongship(GameCard card) : base(card) { }
+
+
+        public async Task HandleEvent(AfterTurnOver @event)
+        {
+            var taget = Card.GetRangeCard(1, GetRangeType.HollowRight);
+			//如果右侧没有单位或者是伏击卡，不触发效果
+            if (taget.Count() == 0||taget.Single().Status.Conceal = true)
+            {
+                return 0;
+            }
+
+            await taget.Single().Effect.Damage(1,Card);
+			await Card.Effect.Boost(2, Card);
 			return 0;
-		}
-	}
+        }
+
+
+    }
+}
 }
