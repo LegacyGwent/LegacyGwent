d799e87553d9f152a430af17dcf174b7296d317b
shanzhoupeng <fff@pku.edu.cn>
2019-08-02 22:24:19 +0800

    EistTuirseach_debug&&OrnamentalSword

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Leader/EistTuirseach.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Leader/EistTuirseach.cs
@@ -4,13 +4,19 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("61003")]//埃斯特·图尔赛克
-	public class EistTuirseach : CardEffect
-	{//生成1个铜色“图尔赛克家族”的“士兵”单位。
-		public EistTuirseach(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{	//图尔赛克家族，士兵，（铜单位）
-            return await Card.CreateAndMoveStay(GwentMap.GetCreateCardsId(x => x.Group == Group.Copper&&x.CardInfo().CardType == CardType.Unit&&x.HasAllCategorie(Categorie.ClanTuirseach,Categorie.Soldier),RNG).ToList()
-		}
-	}
+    [CardEffectId("61003")]//埃斯特·图尔赛克
+    public class EistTuirseach : CardEffect
+    {//生成1个铜色“图尔赛克家族”的“士兵”单位。
+        public EistTuirseach(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {   //图尔赛克家族，士兵，铜单位
+            var target = GwentMap.GetCards().Where(x => x.Is(Group.Copper, CardType.Unit, x => x.HasAllCategorie(Categorie.ClanTuirseach, Categorie.Soldier)).Select(x => x.CardId));
+            //创造,如果没选择,什么都不做
+            var count = (await Game.CreateAndMoveStay(PlayerIndex, target.ToArray()));
+            if (count == 0)
+            {
+                return 0;
+            }
+        }
+    }
 }
