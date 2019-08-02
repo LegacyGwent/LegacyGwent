300629d43c8f9622d40f0b82d1aca5401335474c
shanzhoupeng <fff@pku.edu.cn>
2019-08-02 19:47:41 +0800

    EistTuirseach

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Leader/EistTuirseach.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Leader/EistTuirseach.cs
@@ -9,8 +9,8 @@ namespace Cynthia.Card
 	{//生成1个铜色“图尔赛克家族”的“士兵”单位。
 		public EistTuirseach(GameCard card) : base(card){}
 		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
+		{	//图尔赛克家族，士兵，（铜单位）
+            return await Card.CreateAndMoveStay(GwentMap.GetCreateCardsId(x => x.Group == Group.Copper&&x.CardInfo().CardType == CardType.Unit&&x.HasAllCategorie(Categorie.ClanTuirseach,Categorie.Soldier),RNG).ToList()
 		}
 	}
 }
