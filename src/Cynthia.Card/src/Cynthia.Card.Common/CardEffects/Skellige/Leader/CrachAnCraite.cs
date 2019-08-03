6ad9f2261b0cc41d3444fc530ec566a91508a717
shanzhoupeng <fff@pku.edu.cn>
2019-08-04 02:46:32 +0800

    debug:unity

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Leader/CrachAnCraite.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Leader/CrachAnCraite.cs
@@ -9,9 +9,15 @@ namespace Cynthia.Card
     {//使牌组中最强的非间谍铜色/银色单位牌获得2点强化，随后打出。
         public CrachAnCraite(GameCard card) : base(card) { }
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
-        {
-            //卡组中所有最强非间谍铜色/银色单位牌
-            var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.CardInfo().CardUseInfo == CardUseInfo.MyRow && (x.Status.Group == Group.Silver || x.Status.Group == Group.Copper) && (x.CardInfo().CardType == CardType.Unit)).WhereAllHighest();
+        {   
+            //计算最强的时候算绿字
+            // //卡组中所有最强非间谍铜色/银色单位牌
+            // var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.CardInfo().CardUseInfo == CardUseInfo.MyRow && (x.Status.Group == Group.Silver || x.Status.Group == Group.Copper) && (x.CardInfo().CardType == CardType.Unit)).WhereAllHighest();
+            
+
+            //计算最强的时候不算绿字
+            //列出全部非间谍单位
+            var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.CardInfo().CardUseInfo == CardUseInfo.MyRow && (x.Status.Group == Group.Silver || x.Status.Group == Group.Copper) && (x.CardInfo().CardType == CardType.Unit));
             //选一张
             if (!list.TryMessOne(out var target, Game.RNG))
             {
--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Silver/HolgerBlackhand.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Silver/HolgerBlackhand.cs
@@ -4,13 +4,26 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("63011")]//“黑手”霍格
-	public class HolgerBlackhand : CardEffect
-	{//造成6点伤害。若摧毁目标，则使己方墓场中最强的单位获得3点强化。
-		public HolgerBlackhand(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("63011")]//“黑手”霍格
+    public class HolgerBlackhand : CardEffect
+    {//造成6点伤害。若摧毁目标，则使己方墓场中最强的单位获得3点强化。
+        public HolgerBlackhand(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+			//以下代码基于：如果有多个最强单位，随机强化其中一个
+			//选择一张场上的卡
+            var selectList = await Game.GetSelectPlaceCards(Card, selectMode: SelectModeType.AllRow);
+            if (!selectList.TrySingle(out var target))
+            {
+                return 0;
+            }
+            await target.Effect.Damage(6, Card);
+            if (target.IsDead)
+            {	
+				//列出我方最强墓地单位
+				 var list = Game.PlayersCemetery[Card.PlayerIndex].Where(x => x.CardInfo().CardType == CardType.Unit).WhereAllHighest();	
+            }
+            return 0;
+        }
+    }
 }
