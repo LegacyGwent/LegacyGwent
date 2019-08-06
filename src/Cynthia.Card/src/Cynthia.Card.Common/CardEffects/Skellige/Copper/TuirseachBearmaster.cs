f4b451ad4bf2b44340ddfbd05a3e75617f865090
shanzhoupeng <fff@pku.edu.cn>
2019-08-06 13:45:14 +0800

    coppers

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/TuirseachBearmaster.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/TuirseachBearmaster.cs
@@ -4,13 +4,14 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("64033")]//图尔赛克家族驯兽师
-	public class TuirseachBearmaster : CardEffect
-	{//生成1头“熊”。
-		public TuirseachBearmaster(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("64033")]//图尔赛克家族驯兽师
+    public class TuirseachBearmaster : CardEffect
+    {//生成1头“熊”。
+        public TuirseachBearmaster(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            await Game.CreateCard("15010", Card.PlayerIndex, new CardLocation(RowPosition.MyStay, 0));
+            return 1;
+        }
+    }
 }
--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Wilmar.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Wilmar.cs
@@ -5,40 +5,50 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("65007")]//威尔玛
-    public class Wilmar : CardEffect, IHandlesEvent<AfterCardDeath>, IHandlesEvent<AfterTurnStart>
+    public class Wilmar : CardEffect, IHandlesEvent<AfterCardDeath>
+    //, IHandlesEvent<AfterTurnStart>
     {//遗愿：若为对方回合，则在对面此排生成1头“熊”。 间谍。
         public Wilmar(GameCard card) : base(card) { }
 
         //一个记录现在是谁的回合的flag
-        private bool _summonflag = true;
-        public async Task HandleEvent(AfterTurnStart @event)
-        {
-            //在回合交替时改变flag
-            if (@event.PlayerIndex == Card.PlayerIndex)
-            {
-                _summonflag = false;
-            }
-            else
-            {
-                _summonflag = true;
-            }
-            await Task.CompletedTask;
-        }
+        // private bool _summonflag = true;
+        // public async Task HandleEvent(AfterTurnStart @event)
+        // {
+        //     //在回合交替时改变flag
+        //     if (@event.PlayerIndex == Card.PlayerIndex)
+        //     {
+        //         _summonflag = false;
+        //     }
+        //     else
+        //     {
+        //         _summonflag = true;
+        //     }
+        //     await Task.CompletedTask;
+        // }
+        // public async Task HandleEvent(AfterCardDeath @event)
+        // {
+        //     //如果死的不是本卡或flag为false
+        //     if (@event.Target != Card || !_summonflag)
+        //     {
+        //         return;
+        //     }
+        //     if (Game.RowToList(AnotherPlayer, Card.Status.CardRow).Count >= 9)
+        //     {
+        //         return;
+        //     }
+
+        //     await Game.CreateCardAtEnd("15010", PlayerIndex, Card.Status.CardRow);
+        //     return;
+
+        // }
+
+
+        //尝试使用羊角魔代码，失效
         public async Task HandleEvent(AfterCardDeath @event)
         {
-            //如果死的不是本卡或flag为false
-            if (@event.Target != Card || !_summonflag)
-            {
-                return;
-            }
-            if (Game.RowToList(AnotherPlayer, Card.Status.CardRow).Count >= 9)
-            {
-                return;
-            }
-
-            await Game.CreateCardAtEnd("15010", PlayerIndex, Card.Status.CardRow);
+            if (@event.Target != Card) return;
+            await Game.CreateCard(CardId.Chort, PlayerIndex, @event.DeathLocation);
             return;
-
         }
     }
 }
