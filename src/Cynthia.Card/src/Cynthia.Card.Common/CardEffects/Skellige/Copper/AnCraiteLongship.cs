ca7469bc13dbb24b1171f4ec9530792d760ac36d
shanzhoupeng <fff@pku.edu.cn>
2019-08-07 07:10:35 +0800

    Fixbugcoppers

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/AnCraiteLongship.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/AnCraiteLongship.cs
@@ -5,7 +5,7 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("64018")]//奎特家族作战长船
-    public class AnCraiteLongship : CardEffect
+    public class AnCraiteLongship : CardEffect, IHandlesEvent<AfterCardDiscard>
     {//对1个敌军随机单位造成2点伤害。己方每丢弃1张牌，便触发此能力一次。
         public AnCraiteLongship(GameCard card) : base(card) { }
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
@@ -14,10 +14,14 @@ namespace Cynthia.Card
             return 0;
         }
 
-        public async Task HandleEvent(AfterCardReveal @event)
+        public async Task HandleEvent(AfterCardDiscard @event)
         {
-            if (@event.Source == null || @event.Source.PlayerIndex != Card.PlayerIndex || !Card.Status.CardRow.IsOnPlace()) return;
-            await DamageRandomEnemy();
+            if (@event.Source.PlayerIndex == Card.PlayerIndex && Card.Status.CardRow.IsOnPlace())
+            {
+                await DamageRandomEnemy();
+            }
+            return;
+
         }
 
         private async Task DamageRandomEnemy()
