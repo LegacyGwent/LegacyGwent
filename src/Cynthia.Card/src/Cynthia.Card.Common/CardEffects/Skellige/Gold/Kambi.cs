2270817817a387ad361684072461fa0e27776280
shanzhoupeng <fff@pku.edu.cn>
2019-08-06 22:03:02 +0800

    Gold1

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Gold/Kambi.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Gold/Kambi.cs
@@ -4,13 +4,20 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("62013")]//坎比
-	public class Kambi : CardEffect
-	{//间谍。遗愿：生成“汉姆多尔”。
-		public Kambi(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("62013")]//坎比
+    public class Kambi : CardEffect, IHandlesEvent<AfterCardDeath>
+    {//间谍。遗愿：生成“汉姆多尔”。
+        public Kambi(GameCard card) : base(card) { }
+        public async Task HandleEvent(AfterCardDeath @event)
+        {
+            //如果死的不是这样卡，或者死亡位置不是场上，什么事情都不做
+            if (@event.Target != Card || !@event.DeathLocation.RowPosition.IsOnPlace())
+            {
+                return;
+            }
+            var row = @event.DeathLocation.RowPosition;
+            //最左生成汉姆多尔
+            await Game.CreateCard(CardId.Hemdall, Card.PlayerIndex, new CardLocation(row, 0));
+        }
+    }
 }
--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Gold/MadmanLugos.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Gold/MadmanLugos.cs
@@ -4,13 +4,39 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("62008")]//“疯子”卢戈
-	public class MadmanLugos : CardEffect
-	{//从牌组丢弃1张铜色单位牌，对1个敌军单位造成等同于被丢弃单位基础战力的伤害。
-		public MadmanLugos(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("62008")]//“疯子”卢戈
+    public class MadmanLugos : CardEffect
+    {//从牌组丢弃1张铜色单位牌，对1个敌军单位造成等同于被丢弃单位基础战力的伤害。
+        public MadmanLugos(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            //乱序列出牌库中铜色单位
+            var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.Group == Group.Copper).Mess(Game.RNG);
+            if (list.Count() == 0)
+            {
+                return 0;
+            }
+            var result = await Game.GetSelectMenuCards(Card.PlayerIndex, list.ToList(), 1, "选择丢弃一张牌");
+
+            if (result.Count() == 0)
+            {
+                return 0;
+            }
+            //先记录
+            int damagenum = result.First().Status.Strength;
+            //后丢弃
+            await result.First().Effect.Discard(Card);
+
+            //选择一张场上的卡
+            var selectList = await Game.GetSelectPlaceCards(Card, selectMode: SelectModeType.EnemyRow);
+            if (!selectList.TrySingle(out var target))
+            {
+                return 0;
+            }
+            await target.Effect.Damage(damagenum, Card);
+
+
+            return 0;
+        }
+    }
 }
