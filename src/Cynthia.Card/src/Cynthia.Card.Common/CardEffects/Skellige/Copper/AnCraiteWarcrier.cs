0d41cf7c8d536c9724b6496f81a03e7785897e4f
shanzhoupeng <fff@pku.edu.cn>
2019-08-06 19:37:44 +0800

    coppers2

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/AnCraiteWarcrier.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/AnCraiteWarcrier.cs
@@ -4,13 +4,22 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("64023")]//奎特家族战吼者
-	public class AnCraiteWarcrier : CardEffect
-	{//使1个友军单位获得自身一半战力的增益。
-		public AnCraiteWarcrier(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("64023")]//奎特家族战吼者
+    public class AnCraiteWarcrier : CardEffect
+    {//使1个友军单位获得自身一半战力的增益。
+        public AnCraiteWarcrier(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+
+        {
+            var selectList = await Game.GetSelectPlaceCards(Card, selectMode: SelectModeType.MyRow);
+            if (!selectList.TrySingle(out var target))
+            {
+                return 0;
+            }
+            //增益值向下取等
+            int boostnum = (target.Status.Strength + target.Status.HealthStatus - 1) / 2;
+            await target.Effect.Boost(boostnum, Card);
+            return 0;
+        }
+    }
 }
