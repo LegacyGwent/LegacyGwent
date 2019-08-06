163735dd5d75f1524893a06c5d6125682d2bfe4d
shanzhoupeng <fff@pku.edu.cn>
2019-08-07 04:42:59 +0800

    SkelligeAllCards

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Hemdall.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Hemdall.cs
@@ -1,16 +1,37 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Alsein.Extensions;
+using System.Collections.Generic;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("65001")]//汉姆多尔
-	public class Hemdall : CardEffect
-	{//摧毁场上所有单位，并移除所有恩泽和灾厄。
-		public Hemdall(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("65001")]//汉姆多尔
+    public class Hemdall : CardEffect
+    {//摧毁场上所有单位，并移除所有恩泽和灾厄。
+        public Hemdall(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            var cards = Game.GetAllCard(Card.PlayerIndex).Where(x => x.Status.CardRow.IsOnPlace() && x != Card).ToList();
+            if (cards.Count() == 0)
+            {
+                return 0;
+            }
+            foreach (var card in cards)
+            {
+                await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
+            }
+
+
+
+            var allrow = new List<int>() { 0, 1, 2, 3, 4, 5 };
+            foreach (var rowIndex in allrow)
+            {
+                //将所有行设置为无效果
+                await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<NoneStatus>();
+            }
+
+            await Task.CompletedTask;
+            return 0;
+        }
+    }
 }
--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Gold/CerysFearless.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Gold/CerysFearless.cs
@@ -4,13 +4,35 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("62010")]//凯瑞丝：无所畏惧
-	public class CerysFearless : CardEffect
-	{//复活己方下张丢弃的单位牌。
-		public CerysFearless(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("62010")]//凯瑞丝：无所畏惧
+    public class CerysFearless : CardEffect, IHandlesEvent<AfterCardDiscard>
+    {//复活己方下张丢弃的单位牌。
+        public CerysFearless(GameCard card) : base(card) { }
+        //以下代码基于：自己丢弃自己的牌的时候才可以复活。
+
+
+
+        //打出后获得复活计数
+
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            await Card.Effect.SetCountdown(value: 1);
+            return 0;
+
+        }
+
+
+        public async Task HandleEvent(AfterCardDiscard @event)
+        {
+
+            if (@event.Target.PlayerIndex == Card.PlayerIndex && @event.Source.PlayerIndex == Card.PlayerIndex && Card.Status.CardRow.IsOnPlace() && Card.Status.Countdown == 1)
+            {
+                await Card.Effect.SetCountdown(value: 0);
+                await @event.Target.Effect.Resurrect(new CardLocation() { RowPosition = RowPosition.MyStay, CardIndex = 0 }, Card);
+
+            }
+            return;
+        }
+
+    }
 }
