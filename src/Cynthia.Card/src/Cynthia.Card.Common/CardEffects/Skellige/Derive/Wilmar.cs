cc0b03ce1f82ce5fba395b42375919ae351da500
shanzhoupeng <fff@pku.edu.cn>
2019-08-09 09:16:32 +0800

    finalversion

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Wilmar.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Wilmar.cs
@@ -5,40 +5,41 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("65007")]//威尔玛
-    public class Wilmar : CardEffect, IHandlesEvent<AfterTurnStart>, IHandlesEvent<AfterCardDeath>
-    {//遗愿：若为对方回合，则在对面此排生成1头“熊”。 间谍。
+    public class Wilmar : CardEffect
+    //IHandlesEvent<AfterCardDeath>
+    {//遗愿：若为对方回合，则在对面此排生成1头“熊”。 间谍。IHandlesEvent<AfterTurnStart>,
         public Wilmar(GameCard card) : base(card) { }
 
         // 一个记录现在是谁的回合的flag
         private bool _summonflag = true;
-        public async Task HandleEvent(AfterTurnStart @event)
-        {
-            //在回合交替时改变flag
-            if (@event.PlayerIndex == Card.PlayerIndex)
-            {
-                _summonflag = false;
-            }
-            else
-            {
-                _summonflag = true;
-            }
-            await Task.CompletedTask;
-        }
-        public async Task HandleEvent(AfterCardDeath @event)
-        {
-            //如果死的不是本卡或flag为false
-            if (@event.Target != Card || !_summonflag)
-            {
-                return;
-            }
-            if (Game.RowToList(AnotherPlayer, Card.Status.CardRow).Count >= Game.RowMaxCount)
-            {
-                return;
-            }
+        // public async Task HandleEvent(AfterTurnStart @event)
+        // {
+        //     //在回合交替时改变flag
+        //     if (@event.PlayerIndex == Card.PlayerIndex)
+        //     {
+        //         _summonflag = false;
+        //     }
+        //     else
+        //     {
+        //         _summonflag = true;
+        //     }
+        //     await Task.CompletedTask;
+        // }
+        // public async Task HandleEvent(AfterCardDeath @event)
+        // {
+        //     //如果死的不是本卡或flag为false
+        //     if (@event.Target != Card || !_summonflag)
+        //     {
+        //         return;
+        //     }
+        //     if (Game.RowToList(AnotherPlayer, Card.Status.CardRow).Count >= Game.RowMaxCount)
+        //     {
+        //         return;
+        //     }
 
-            await Game.CreateCardAtEnd("15010", AnotherPlayer, Card.Status.CardRow);
-            return;
-        }
+        //     await Game.CreateCardAtEnd("15010", PlayerIndex, Card.Status.CardRow.Mirror());
+        //     return;
+        // }
 
 
     }
