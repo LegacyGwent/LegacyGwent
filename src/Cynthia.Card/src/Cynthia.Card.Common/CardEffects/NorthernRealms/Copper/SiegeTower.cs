7596888ac7a28ccedec5928be462b04da08a88e2
shanzhoupeng <fff@pku.edu.cn>
2019-08-10 07:49:50 +0800

    coppers2

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/SiegeTower.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/SiegeTower.cs
@@ -4,13 +4,14 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("44007")]//攻城塔
-	public class SiegeTower : CardEffect
-	{//获得2点增益。 驱动：再次触发此能力。
-		public SiegeTower(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("44007")]//攻城塔
+    public class SiegeTower : CardEffect
+    {//获得2点增益。 驱动：再次触发此能力。
+        public SiegeTower(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            await Card.Effect.Boost(2, Card);
+            return 0;
+        }
+    }
 }
