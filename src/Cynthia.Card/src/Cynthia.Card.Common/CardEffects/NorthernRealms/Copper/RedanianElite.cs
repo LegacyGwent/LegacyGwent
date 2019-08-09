b89178045dd9acf0f30f7f7e285134fc7c049d10
shanzhoupeng <fff@pku.edu.cn>
2019-08-10 07:14:07 +0800

    coppers

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/RedanianElite.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/RedanianElite.cs
@@ -4,13 +4,23 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("44006")]//瑞达尼亚精锐
-	public class RedanianElite : CardEffect
-	{//每当护甲归0，获得5点增益。 4点护甲。
-		public RedanianElite(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("44006")]//瑞达尼亚精锐
+    public class RedanianElite : CardEffect, IHandlesEvent<AfterCardArmorBreak>
+    {//每当护甲归0，获得5点增益。 4点护甲。
+        public RedanianElite(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            await Card.Effect.Armor(4, Card);
+            return 0;
+        }
+        public async Task HandleEvent(AfterCardArmorBreak @event)
+        {
+            if (@event.Target == Card && Card.Status.CardRow.IsOnPlace())
+            {
+                await Card.Effect.Boost(4, Card);
+            }
+            return;
+        }
+
+    }
 }
