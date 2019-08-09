ed0e9a076c414b0252e2d6c77d545e2b68a687ed
shanzhoupeng <fff@pku.edu.cn>
2019-08-09 09:16:32 +0800

    finalversion

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Hemdall.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Hemdall.cs
@@ -9,44 +9,30 @@ namespace Cynthia.Card
 
 
     //妥协的写法，回合结束生效
-    public class Hemdall : CardEffect, IHandlesEvent<AfterTurnOver>
+    public class Hemdall : CardEffect
     {//摧毁场上所有单位，并移除所有恩泽和灾厄。
         public Hemdall(GameCard card) : base(card) { }
 
-        private bool _isuse = false;
-        public override async Task CardDownEffect(bool isSpying, bool isReveal)
-        {
-            _isuse = false;
-            await Task.CompletedTask;
+        // public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        // {
+        //     var cards = Game.GetAllCard(Card.PlayerIndex).Where(x => x.Status.CardRow.IsOnPlace() && x != Card).ToList();
+        //     if (cards.Count() != 0)
+        //     {
+        //         foreach (var card in cards)
+        //         {
+        //             await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
+        //         }
+        //     }
+        //     var allrow = new List<int>() { 0, 1, 2, 3, 4, 5 };
+        //     foreach (var rowIndex in allrow)
+        //     {
+        //         //将所有行设置为无效果
+        //         await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<NoneStatus>();
+        //     }
+        //     return 0;
+        // }
 
-        }
 
-
-
-        public async Task HandleEvent(AfterTurnOver @event)
-        {
-
-            if (_isuse == true)
-            {
-                return;
-            }
-            _isuse = true;
-            //列出场上其他卡
-            var cards = Game.GetAllCard(Card.PlayerIndex).Where(x => x.Status.CardRow.IsOnPlace() && x != Card).ToList();
-            if (cards.Count() != 0)
-            {
-                foreach (var card in cards)
-                {
-                    await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
-                }
-            }
-            var allrow = new List<int>() { 0, 1, 2, 3, 4, 5 };
-            foreach (var rowIndex in allrow)
-            {
-                //将所有行设置为无效果
-                await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<NoneStatus>();
-            }
-        }
     }
 }
 
