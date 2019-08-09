d568d18e491eaa76b10aa7ae9a8f25117d5d0bf9
shanzhoupeng <fff@pku.edu.cn>
2019-08-09 08:46:03 +0800

    copperbugs2

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Hemdall.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Derive/Hemdall.cs
@@ -6,30 +6,68 @@ using System.Collections.Generic;
 namespace Cynthia.Card
 {
     [CardEffectId("65001")]//汉姆多尔
-    public class Hemdall : CardEffect
+
+
+    //妥协的写法，回合结束生效
+    public class Hemdall : CardEffect, IHandlesEvent<AfterTurnOver>
     {//摧毁场上所有单位，并移除所有恩泽和灾厄。
         public Hemdall(GameCard card) : base(card) { }
-        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+
+        private bool _isuse = false;
+        public override async Task CardDownEffect(bool isSpying, bool isReveal)
         {
+            _isuse = false;
+            await Task.CompletedTask;
+
+        }
+
+
+
+        public async Task HandleEvent(AfterTurnOver @event)
+        {
+
+            if (_isuse == true)
+            {
+                return;
+            }
+            _isuse = true;
+            //列出场上其他卡
             var cards = Game.GetAllCard(Card.PlayerIndex).Where(x => x.Status.CardRow.IsOnPlace() && x != Card).ToList();
-            if (cards.Count() == 0)
+            if (cards.Count() != 0)
             {
-                return 0;
+                foreach (var card in cards)
+                {
+                    await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
+                }
             }
-            foreach (var card in cards)
-            {
-                await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
-            }
-
-
-
             var allrow = new List<int>() { 0, 1, 2, 3, 4, 5 };
             foreach (var rowIndex in allrow)
             {
                 //将所有行设置为无效果
                 await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<NoneStatus>();
             }
-            return 1;
         }
     }
 }
+
+
+
+// var cards = Game.GetAllCard(Card.PlayerIndex).Where(x => x.Status.CardRow.IsOnPlace() && x != Card).ToList();
+// if (cards.Count() == 0)
+// {
+//     return;
+// }
+// foreach (var card in cards)
+// {
+//     await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
+// }
+
+
+
+// var allrow = new List<int>() { 0, 1, 2, 3, 4, 5 };
+// foreach (var rowIndex in allrow)
+// {
+//     //将所有行设置为无效果
+//     await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<NoneStatus>();
+// }
+// return;
