5e7fb8da45fa30f271c5a8d658e3ccaf1a5c5131
shanzhoupeng <fff@pku.edu.cn>
2019-08-10 19:39:28 +0800

    coppers

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/Foglet.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/Foglet.cs
@@ -4,13 +4,29 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("24028")]//小雾妖
-	public class Foglet : CardEffect
-	{//每当在对方半场降下“蔽日浓雾”，便召唤1张同名牌至己方同排。
-		public Foglet(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("24028")]//小雾妖
+    public class Foglet : CardEffect, IHandlesEvent<AfterWeatherApply>
+    {//每当在对方半场降下“蔽日浓雾”，便召唤1张同名牌至己方同排。
+        public Foglet(GameCard card) : base(card) { }
+        public async Task HandleEvent(AfterWeatherApply @event)
+        {
+
+            if ((@event.PlayerIndex == Card.PlayerIndex && @event.Type == RowStatus.BitingFrost)
+
+            {
+                //列出所有可以打出卡
+                var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == Card.Status.CardId);
+                if (list.Count() == 0)
+                {
+                    return;
+                }
+                //只召唤第一个
+                if (Card == list.First())
+                {
+                    await Card.Effect.Summon(Game.GetRandomCanPlayLocation(Card.PlayerIndex), Card);
+                }
+            }
+            return;
+        }
+    }
 }
--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/BlueStripesCommando.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/BlueStripesCommando.cs
@@ -4,13 +4,34 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("44027")]//蓝衣铁卫突击队
-	public class BlueStripesCommando : CardEffect
-	{//有战力与自身相同的非同名“泰莫利亚”友军单位被打出时，从牌组召唤1张它的同名牌。
-		public BlueStripesCommando(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("44027")]//蓝衣铁卫突击队
+    public class BlueStripesCommando : CardEffect, IHandlesEvent<AfterUnitPlay>
+    {//有战力与自身相同的非同名“泰莫利亚”友军单位被打出时，从牌组召唤1张它的同名牌。
+        public BlueStripesCommando(GameCard card) : base(card) { }
+        public async Task HandleEvent(AfterUnitPlay @event)
+        {
+
+            var target = @event.PlayedCard;
+            if (target.Status.CardId == Card.Status.CardId)
+            {
+                return;
+            }
+            int phealth = target.ToHealth().health;
+            //获取可触发效果的本卡同名卡列表
+            var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == Card.Status.CardId && x.ToHealth().health == phealth);
+            if (list.Count() == 0)
+            {
+                return;
+            }
+            //只召唤第一个
+            if (Card == list.First())
+            {
+                await Card.Effect.Summon(Game.GetRandomCanPlayLocation(Card.PlayerIndex), Card);
+            }
+
+            return;
+
+        }
+
+    }
 }
