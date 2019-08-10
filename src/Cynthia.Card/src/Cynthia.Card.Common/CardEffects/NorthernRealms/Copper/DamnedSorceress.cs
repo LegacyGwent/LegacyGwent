df65f18c0ef8d225aca1688054e44523f6e3326b
shanzhoupeng <fff@pku.edu.cn>
2019-08-11 04:57:16 +0800

    coppers3

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/DamnedSorceress.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/DamnedSorceress.cs
@@ -4,14 +4,23 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("44025")]//中邪的女术士
-	public class DamnedSorceress : CardEffect
-	{//若同排有1个“诅咒生物”单位，则造成7点伤害。
-		public DamnedSorceress(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			
-			return 0;
-		}
-	}
+    [CardEffectId("44025")]//中邪的女术士
+    public class DamnedSorceress : CardEffect
+    {//若同排有1个“诅咒生物”单位，则造成7点伤害。
+        public DamnedSorceress(GameCard card) : base(card) { }
+        public override async Task CardDownEffect(bool isSpying, bool isReveal)
+        {
+            var list = Game.RowToList(PlayerIndex, Card.Status.CardRow).Where(x => x.HasAnyCategorie(Categorie.Cursed));
+            if (list.Count() >= 0)
+            {
+                var selectList = await Game.GetSelectPlaceCards(Card, selectMode: SelectModeType.AllRow);
+                if (!selectList.TrySingle(out var target))
+                {
+                    return;
+                }
+                await target.Effect.Damage(7, Card);
+            }
+            return;
+        }
+    }
 }
