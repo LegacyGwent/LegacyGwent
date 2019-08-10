4dc25ba391dce92462eb469ab45e6444dd4ec9dc
Cynthia <1725877349@qq.com>
2019-08-11 07:05:45 +0800

    解决了一些bug

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/Drowner.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/Drowner.cs
@@ -4,12 +4,16 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("24014")]//水鬼
-	public class Drowner : CardEffect
-	{//将1个敌军单位拖至对方同排，对其造成2点伤害，若目标排处于灾厄之下，则伤害提高至4点。
-		public Drowner(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
+    [CardEffectId("24014")]//水鬼
+    public class Drowner : CardEffect
+    {//将1个敌军单位拖至对方同排，对其造成2点伤害，若目标排处于灾厄之下，则伤害提高至4点。
+        public Drowner(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            if (Game.PlayersPlace[AnotherPlayer].Count() >= Game.RowMaxCount)
+            {
+                return 0;
+            }
             if (!(await Game.GetSelectPlaceCards(Card, selectMode: SelectModeType.EnemyRow, filter: x => x.Status.CardRow != Card.Status.CardRow)).TrySingle(out var target))
             {
                 return 0;
@@ -18,6 +22,6 @@ namespace Cynthia.Card
             var damagePoint = Game.GameRowEffect[target.PlayerIndex][target.Status.CardRow.MyRowToIndex()].RowStatus.IsHazard() ? 4 : 2;
             await target.Effect.Damage(damagePoint, Card);
             return 0;
-		}
-	}
+        }
+    }
 }
