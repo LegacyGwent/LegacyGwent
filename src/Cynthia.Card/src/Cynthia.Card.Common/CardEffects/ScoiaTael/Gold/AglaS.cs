6385f5afe39d9d2141fcc08f411b5d3f99fb05b5
Jasper <450213487@qq.com>
2019-08-12 20:08:17 +0800

    And Some Card For Shupe

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/ScoiaTael/Gold/AglaS.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/ScoiaTael/Gold/AglaS.cs
@@ -4,13 +4,28 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("52004")]//艾格莱丝
-	public class AglaS : CardEffect
-	{//从对方墓场复活1张铜色/银色“特殊”牌，随后将其放逐。
-		public AglaS(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("52004")] //艾格莱丝
+    public class AglaS : CardEffect
+    {
+        //从对方墓场复活1张铜色/银色“特殊”牌，随后将其放逐。
+        public AglaS(GameCard card) : base(card)
+        {
+        }
+
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            var cards = Game.PlayersCemetery[AnotherPlayer].FilterCards(type: CardType.Special,
+                filter: x => x.IsAnyGroup(Group.Copper, Group.Silver));
+
+            if (!(await Game.GetSelectMenuCards(PlayerIndex, cards.ToList())).TrySingle(out var target))
+            {
+                return 0;
+            }
+
+            target.Status.IsDoomed = true;
+
+            await target.Effect.Resurrect(new CardLocation(RowPosition.EnemyStay, 0), Card);
+            return 1;
+        }
+    }
 }
