c521cade0b42c65c0a475565a0b67d337290d072
shanzhoupeng <fff@pku.edu.cn>
2019-08-12 09:32:46 +0800

    silvers2

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Silver/PrincessPavetta.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Silver/PrincessPavetta.cs
@@ -4,13 +4,28 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("43015")]//帕薇塔公主
-	public class PrincessPavetta : CardEffect
-	{//将双方最弱的铜色/银色单位放回各自牌组。
-		public PrincessPavetta(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("43015")]//帕薇塔公主
+    public class PrincessPavetta : CardEffect
+    {//将双方最弱的铜色/银色单位放回各自牌组。
+        public PrincessPavetta(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            //最弱定义为点数最弱（包括绿字）
+            var mylist = Game.GetPlaceCards(Card.PlayerIndex).Where(x => x.Status.Group == Group.Silver || x.Status.Group == Group.Copper).WhereAllLowest();
+            var enemylist = Game.GetPlaceCards(Game.AnotherPlayer(Card.PlayerIndex)).Where(x => x.Status.Group == Group.Silver || x.Status.Group == Group.Copper).WhereAllLowest();
+            if (mylist.TryMessOne(out var mycard, Game.RNG))
+            {
+                await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck, RNG.Next(0, Game.PlayersDeck[Card.PlayerIndex].Count)), mycard);
+
+            }
+            if (enemylist.TryMessOne(out var enemycard, Game.RNG))
+            {
+                await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck, RNG.Next(0, Game.PlayersDeck[Card.PlayerIndex].Count)), enemycard);
+
+
+            }
+
+            return 0;
+        }
+    }
 }
--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Silver/SabrinaGlevissig.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Silver/SabrinaGlevissig.cs
@@ -4,13 +4,28 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("43017")]//萨宾娜·葛丽维希格
-	public class SabrinaGlevissig : CardEffect
-	{//间谍。 遗愿：将该排最弱单位的战力应用于全排单位。
-		public SabrinaGlevissig(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("43017")]//萨宾娜·葛丽维希格
+    public class SabrinaGlevissig : CardEffect, IHandlesEvent<AfterCardDeath>
+    {//间谍。 遗愿：将该排最弱单位的战力应用于全排单位。
+        public SabrinaGlevissig(GameCard card) : base(card) { }
+        public async Task HandleEvent(AfterCardDeath @event)
+        {
+            var rowlist = Game.RowToList(Card.PlayerIndex, Card.GetLocation().RowPosition).Where(x => x.GetLocation().RowPosition.IsOnPlace() && x != Card).ToList();
+            if (rowlist.Count() <= 1)
+            {
+                return;
+            }
+            int minhealth = rowlist.WhereAllLowest().First().CardPoint();
+            foreach (var card in rowlist)
+            {
+                int offnum = card.CardPoint() - minhealth;
+                if (offnum > 0)
+                {
+                    await card.Effect.Damage(offnum, Card);
+                }
+            }
+            return;
+        }
+
+    }
 }
