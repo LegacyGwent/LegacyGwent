fe26da657677846c41e0e70950c5ea3813ad9dc6
shanzhoupeng <fff@pku.edu.cn>
2019-08-12 09:32:46 +0800

    silvers2

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Silver/SLeDeTansarville.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Silver/SLeDeTansarville.cs
@@ -4,14 +4,26 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("43014")]//席儿·德·坦沙维耶
-	public class SLeDeTansarville : CardEffect
-	{//从手牌打出1张铜色/银色“特殊”牌，随后抽1张牌。
-		public SLeDeTansarville(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			
-			return 0;
-		}
-	}
+    [CardEffectId("43014")]//席儿·德·坦沙维耶
+    public class SLeDeTansarville : CardEffect
+    {//从手牌打出1张铜色/银色“特殊”牌，随后抽1张牌。
+        public SLeDeTansarville(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+
+            var handlist = Game.PlayersHandCard[Card.PlayerIndex].Where(x => (x.Status.Group == Group.Silver || x.Status.Group == Group.Copper) && x.HasAnyCategorie(Categorie.Special));
+            if (handlist.Count() == 0)
+            {
+                return 0;
+            }
+            var result = await Game.GetSelectMenuCards(Card.PlayerIndex, handlist.ToList(), 1);
+            if (result.Count() == 0)
+            {
+                return 0;
+            }
+            await result.Single().MoveToCardStayFirst();
+            await Game.PlayerDrawCard(Card.PlayerIndex, 1);
+            return 0;
+        }
+    }
 }
