fe78cd31b256ae0367ea728c8bc3dcf4c7f3cdd6
shanzhoupeng <fff@pku.edu.cn>
2019-08-14 04:56:37 +0800

    debug

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/SiegeSupport.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Copper/SiegeSupport.cs
@@ -5,18 +5,18 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("44011")]//攻城后援
-    public class SiegeSupport : CardEffect, IHandlesEvent<AfterUnitPlay>
+    public class SiegeSupport : CardEffect, IHandlesEvent<AfterUnitDown>
     {//使后续打出的友军单位获得1点增益，“机械”单位额外获得1点护甲。 操控。
         public SiegeSupport(GameCard card) : base(card) { }
-        public async Task HandleEvent(AfterUnitPlay @event)
+        public async Task HandleEvent(AfterUnitDown @event)
         {
             //以下代码基于 打入我方半场的间谍单位也会被buff
-            if (@event.PlayedCard.PlayerIndex == Card.PlayerIndex&&Card.GetLocation().RowPosition.IsOnPlace()&&@event.PlayedCard!=Card)
+            if (@event.Target.PlayerIndex == Card.PlayerIndex && Card.GetLocation().RowPosition.IsOnPlace() && @event.Target != Card)
             {
-                await @event.PlayedCard.Effect.Boost(1, Card);
-                if (@event.PlayedCard.HasAnyCategorie(Categorie.Machine))
+                await @event.Target.Effect.Boost(1, Card);
+                if (@event.Target.HasAnyCategorie(Categorie.Machine))
                 {
-                    await @event.PlayedCard.Effect.Armor(1, Card);
+                    await @event.Target.Effect.Armor(1, Card);
                 }
 
             }
