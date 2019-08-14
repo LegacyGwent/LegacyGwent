9d22420583cac6e31819a6f7a08e2d3a3a56cc62
Jasper <450213487@qq.com>
2019-08-14 21:57:53 +0800

    Add ElvenScout VriheddOfficer Wardancer reformat and fix bug

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/ScoiaTael/Copper/VriheddNeophyte.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/ScoiaTael/Copper/VriheddNeophyte.cs
@@ -4,14 +4,18 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("54003")]//维里赫德旅新兵
-	public class VriheddNeophyte : CardEffect
-	{//随机使手牌中2个单位获得1点增益。
-		public VriheddNeophyte(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
+    [CardEffectId("54003")] //维里赫德旅新兵
+    public class VriheddNeophyte : CardEffect
+    {
+        //随机使手牌中2个单位获得1点增益。
+        public VriheddNeophyte(GameCard card) : base(card)
+        {
+        }
+
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
             var cards = Game.PlayersHandCard[Card.PlayerIndex];
-            var list = cards.ToList().Take(2);
+            var list = cards.Where(x => x.CardInfo().CardType == CardType.Unit).ToList().Take(2);
             if (!list.Any()) return 0;
 
             foreach (var it in list)
@@ -20,7 +24,7 @@ namespace Cynthia.Card
             }
 
             return 0;
-		}
+        }
 
         private const int boost = 1;
     }
--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/ScoiaTael/Silver/IbhearHattori.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/ScoiaTael/Silver/IbhearHattori.cs
@@ -4,14 +4,19 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("53015")]//哈托利
-	public class IbhearHattori : CardEffect
-	{//复活1个战力不高于自身的铜色/银色“松鼠党”单位。
-		public IbhearHattori(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-            var cards = Game.PlayersCemetery[AnotherPlayer].FilterCards(type: CardType.Unit, filter: 
-                x => x.IsAnyGroup(Group.Copper, Group.Silver) && x.Status.Faction == Faction.ScoiaTael && x.Status.Strength <= Card.Status.Strength);
+    [CardEffectId("53015")] //哈托利
+    public class IbhearHattori : CardEffect
+    {
+        //复活1个战力不高于自身的铜色/银色“松鼠党”单位。
+        public IbhearHattori(GameCard card) : base(card)
+        {
+        }
+
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            var cards = Game.PlayersCemetery[PlayerIndex].FilterCards(type: CardType.Unit, filter:
+                x => x.IsAnyGroup(Group.Copper, Group.Silver) && x.Status.Faction == Faction.ScoiaTael &&
+                     (x.CardPoint()) <= (Card.CardPoint()));
 
             if (!(await Game.GetSelectMenuCards(PlayerIndex, cards.ToList())).TrySingle(out var target))
             {
@@ -20,7 +25,6 @@ namespace Cynthia.Card
 
             await target.Effect.Resurrect(new CardLocation(RowPosition.MyStay, 0), Card);
             return 1;
-
         }
-	}
+    }
 }
