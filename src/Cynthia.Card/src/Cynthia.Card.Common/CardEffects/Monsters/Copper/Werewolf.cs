d23187607878afabb72d9f6c6ffd292ba9f5d844
shanzhoupeng <fff@pku.edu.cn>
2019-08-16 06:33:59 +0800

    monstercoppers

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/Werewolf.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Monsters/Copper/Werewolf.cs
@@ -4,13 +4,56 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("24017")]//狼人
-	public class Werewolf : CardEffect
-	{//接触“满月”后获得7点增益。 免疫。
-		public Werewolf(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("24017")]//狼人
+    public class Werewolf : CardEffect, IHandlesEvent<AfterWeatherApply>
+    {//接触“满月”后获得7点增益。 免疫。
+        public Werewolf(GameCard card) : base(card) { }
+
+        private bool isfullmoon = false;
+        public override async Task CardDownEffect(bool isSpying, bool isReveal)
+        {
+            if (Game.GameRowEffect[Card.PlayerIndex][Card.GetLocation().RowPosition.MyRowToIndex()].RowStatus == RowStatus.FullMoon)
+            {
+                isfullmoon = true;
+                await Card.Effect.Boost(7, Card);
+            }
+            return;
+        }
+
+        public async Task HandleEvent(AfterWeatherApply @event)
+        {
+            //以下代码基于 向满月上打出满月不会buff
+
+
+            //如果不在场上
+            if (!Card.GetLocation().RowPosition.IsOnPlace())
+            {
+                return;
+            }
+            //如果特效没有放置到狼人排 什么事情都不做
+            if (!(@event.PlayerIndex == Card.PlayerIndex && @event.Row == Card.GetLocation().RowPosition) && !(@event.PlayerIndex != Card.PlayerIndex && @event.Row == Card.GetLocation().RowPosition.Mirror()))
+            {
+                return;
+            }
+            //如果特效不是满月,记录isfullmoon
+            if (@event.Type != RowStatus.FullMoon)
+            {
+
+                isfullmoon = false;
+                await Task.CompletedTask;
+                return;
+            }
+            //如果特效是满月 且之前的特效不是满月
+            if (@event.Type == RowStatus.FullMoon && !isfullmoon)
+            {
+                isfullmoon = true;
+                await Card.Effect.Boost(7, Card);
+                return;
+            }
+            return;
+        }
+
+
+
+    }
 }
