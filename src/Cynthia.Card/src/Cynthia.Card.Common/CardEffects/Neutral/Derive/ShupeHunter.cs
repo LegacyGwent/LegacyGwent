c8b96ee794fe303cc072c6d3a945d1d8c422d8a6
Jasper <450213487@qq.com>
2019-08-17 00:34:12 +0800

    Add ShupeKnight ShupeMage

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Derive/ShupeHunter.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Derive/ShupeHunter.cs
@@ -3,33 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Alsein.Extensions;
+using Cynthia.Card.Common.CardEffects.Neutral.Derive;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("15002")]//店店：猎人
-	public class ShupeHunter : CardEffect
-	{//派“店店”去多尔·布雷坦纳的森林。 造成15点伤害；对一个敌军随机单位造成2点伤害，连续8次；重新打出1个铜色/银色单位，并使它获得5点增益；从牌组打出1张铜色/银色单位牌；移除己方半场的所有“灾厄”效果，并使友军单位获得1点增益。
-		public ShupeHunter(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
+    [CardEffectId("15002")] //店店：猎人
+    public class ShupeHunter : AbstractShupe
+    {
+        //派“店店”去多尔·布雷坦纳的森林。 造成15点伤害；对一个敌军随机单位造成2点伤害，连续8次；重新打出1个铜色/银色单位，并使它获得5点增益；从牌组打出1张铜色/银色单位牌；移除己方半场的所有“灾厄”效果，并使友军单位获得1点增益。
+        public ShupeHunter(GameCard card) : base(card)
         {
-            InitDict();
-            var list = baseChoiceList.Mess(RNG).Take(3).ToList();
-            var switchCard = await Card.GetMenuSwitch
-            (
-                (string.Empty, methodDesDict[list[0]]),
-                (string.Empty, methodDesDict[list[1]]),
-                (string.Empty, methodDesDict[list[2]])
-            );
-            return await UseMethodByChoice(list[switchCard]);
-            
-		}
+        }
 
-        private async Task<int> UseMethodByChoice(int switchCard)
+        protected override async Task<int> UseMethodByChoice(int switchCard)
         {
             switch (switchCard)
             {
                 case 1:
-                    return await Damage();
+                    return await DamageEnemy();
                 case 2:
                     return await RandomDamage();
                 case 3:
@@ -43,27 +34,19 @@ namespace Cynthia.Card
             return 0;
         }
 
-        private List<int> baseChoiceList = new List<int>{1,2,3,4,5};
 
-        private Dictionary<int, Action<Task<int>>> methodDict;
-        private Dictionary<int, string> methodDesDict;
-
-        private void InitDict()
+        protected override void RealInitDict()
         {
-
-            if (methodDesDict == null)
+            methodDesDict = new Dictionary<int, string>()
             {
-                methodDesDict = new Dictionary<int, string>()
-                {
-                    {1, "造成15点伤害"},
-                    {2, "对一个敌军随机单位造成2点伤害，连续8次"},
-                    {3, "重新打出1个铜色/银色单位，并使它获得5点增益"},
-                    {4, "从牌组打出1张铜色/银色单位牌"},
-                    {5, "移除己方半场的所有“灾厄”效果，并使友军单位获得1点增益"}
-                };
-            }
-            
+                {1, "造成15点伤害"},
+                {2, "对一个敌军随机单位造成2点伤害，连续8次"},
+                {3, "重新打出1个铜色/银色单位，并使它获得5点增益"},
+                {4, "从牌组打出1张铜色/银色单位牌"},
+                {5, "移除己方半场的所有“灾厄”效果，并使友军单位获得1点增益"}
+            };
         }
+
 
         private async Task<int> RemoveHazard()
         {
@@ -76,12 +59,13 @@ namespace Cynthia.Card
             }
 
 
-            var list = Game.GetPlaceCards(PlayerIndex).Where(x=>x.Status.Type == CardType.Unit).ToList();
+            var list = Game.GetPlaceCards(PlayerIndex).ToList();
 
             foreach (var it in list)
             {
                 await it.Effect.Boost(1, Card);
             }
+
             return 0;
         }
 
@@ -90,19 +74,21 @@ namespace Cynthia.Card
             //己方卡组乱序呈现
             var list = Game.PlayersDeck[PlayerIndex]
                 .Where(x => (x.Status.Group == Group.Copper || x.Status.Group == Group.Silver) &&
-                       x.Status.Type == CardType.Unit)
+                            x.Status.Type == CardType.Unit)
                 .Mess(RNG)
                 .ToList();
             //让玩家选择一张卡,不能不选
             var result = await Game.GetSelectMenuCards(PlayerIndex, list);
-            if (result.Count == 0) return 0;//如果没有任何符合标准的牌,返回
+            if (result.Count == 0) return 0; //如果没有任何符合标准的牌,返回
             await result.Single().MoveToCardStayFirst();
             return 1;
         }
 
         private async Task<int> PlayUnitAgain()
         {
-            var result = await Game.GetSelectPlaceCards(Card, filter: (x => x.Status.Group == Group.Copper || x.Status.Group == Group.Silver), selectMode: SelectModeType.MyRow);
+            var result = await Game.GetSelectPlaceCards(Card,
+                filter: (x => x.Status.Group == Group.Copper || x.Status.Group == Group.Silver),
+                selectMode: SelectModeType.MyRow);
             if (!result.Any()) return 0;
             var targetCard = result.Single();
             targetCard.Effect.Repair(true);
@@ -122,10 +108,11 @@ namespace Cynthia.Card
                 var card = realList.Mess(RNG).First();
                 await card.Effect.Damage(2, Card);
             }
+
             return 0;
         }
 
-        private async Task<int> Damage()
+        private async Task<int> DamageEnemy()
         {
             var result = await Game.GetSelectPlaceCards(Card);
             if (result.Count <= 0) return 0;
--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Derive/ShupeMage.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Derive/ShupeMage.cs
@@ -1,16 +1,112 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Alsein.Extensions;
+using Cynthia.Card.Common.CardEffects.Neutral.Derive;
 
 namespace Cynthia.Card
 {
 	[CardEffectId("15003")]//店店：法师
-	public class ShupeMage : CardEffect
+	public class ShupeMage : AbstractShupe
 	{//派“店店”去班·阿德学院，见见那里的小伙子们。 抽1张牌；随机魅惑1个敌军单位；在对方三排随机生成一种灾厄；对1个敌军造成10点伤害，再对其相邻单位造成5点；从牌组打出1张铜色/银色“特殊”牌。
 		public ShupeMage(GameCard card) : base(card){}
 		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
 		{
 			return 0;
 		}
-	}
+
+        protected override async Task<int> UseMethodByChoice(int switchCard)
+        {
+            switch (switchCard)
+            {
+                case 1:
+                    return await DrawOneCard();
+                case 2:
+                    return await CharmRNG();
+                case 3:
+                    return await HazardRNG();
+                case 4:
+                    return await DamageThreeEnemy();
+                case 5:
+                    return await PlaySpecialFromDeck();
+            }
+
+            return 0;
+        }
+
+        private async Task<int> PlaySpecialFromDeck()
+        {
+            //己方卡组乱序呈现
+            var list = Game.PlayersDeck[PlayerIndex]
+                .Where(x => (x.Status.Group == Group.Copper || x.Status.Group == Group.Silver) &&
+                            x.Status.Type == CardType.Special)
+                .Mess(RNG)
+                .ToList();
+            //让玩家选择一张卡,不能不选
+            var result = await Game.GetSelectMenuCards(PlayerIndex, list);
+            if (result.Count == 0) return 0; //如果没有任何符合标准的牌,返回
+            await result.Single().MoveToCardStayFirst();
+            return 1;
+        }
+
+        private async Task<int> DamageThreeEnemy()
+        {
+            var result = await Game.GetSelectPlaceCards(Card, range: 1);
+            if (!result.Any()) return 0;
+            int center = result.Count() / 2;
+
+            await result[center].Effect.Damage(10, Card);
+            if(center - 1 != 0) await result[center - 1].Effect.Damage(5, Card);
+            if(center + 1 != result.Count()) await result[center + 1].Effect.Damage(5, Card);
+            return 0;
+        }
+
+        private async Task<int> HazardRNG()
+        {
+            var list = Enum.GetValues(typeof(RowStatus));
+            var realList = new List<RowStatus>();
+            foreach (RowStatus it in list)
+            {
+                if (it.IsHazard())
+                {
+                    realList.Add(it);
+                }
+            }
+            var allenemylist = new List<RowPosition>() { RowPosition.EnemyRow1, RowPosition.EnemyRow2, RowPosition.EnemyRow3 };
+
+            return 0;
+
+        }
+
+        private async Task<int> CharmRNG()
+        {
+
+            var list = Game.GetPlaceCards(AnotherPlayer).ToList();
+            if (!list.Any()) return 0;
+            var target = list.Mess(RNG).First();
+
+            await target.Effect.Charm(Card);
+            return 0;
+
+        }
+
+        private async Task<int> DrawOneCard()
+        {
+            await Game.PlayerDrawCard(PlayerIndex);//抽卡
+            return 0;
+        }
+
+        protected override void RealInitDict()
+        {
+            methodDesDict = new Dictionary<int, string>()
+            {
+                {1, "抽1张牌"},
+                {2, "随机魅惑1个敌军单位"},
+                {3, "在对方三排随机生成一种灾厄"},
+                {4, "再对其相邻单位造成5点"},
+                {5, "从牌组打出1张铜色/银色“特殊”牌"}
+            };
+        }
+    }
 }
