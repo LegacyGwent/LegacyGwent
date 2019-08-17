3157e7915fbfa9282fa26c85c6baa615c2dd1000
shanzhoupeng <fff@pku.edu.cn>
2019-08-17 12:26:23 +0800

    gold1

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Gold/Aguara.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Gold/Aguara.cs
@@ -10,7 +10,30 @@ namespace Cynthia.Card
         public Aguara(GameCard card) : base(card) { }
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
+            var switchCard = await Card.GetMenuSwitch(2, ("超然之力", "使最弱的友军单位获得5点增益。"), ("狂热攻势", "对最强的1个敌军单位造成5点伤害。"), ("幻象", "使手牌中的1个随机单位获得5点增益。"), ("诱拐", "魅惑1个战力不高于5点的敌军“精灵”单位"));
+            foreach (var i in switchCard)
+            {
+
+            }
             return 0;
+
         }
+
+        private async Task<int> buffplace0()
+        {
+        }
+        
+        private async Task<int> damageplace1()
+        {
+        }
+
+                private async Task<int> buffhand1()
+        {
+        }
+
+                        private async Task<int> charm11()
+        {
+        }
+
     }
 }
--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Gold/Sihil.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Gold/Sihil.cs
@@ -4,13 +4,13 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("12042")]//矮人符文剑
-	public class Sihil : CardEffect
-	{//择一：对所有战力为“奇数”的敌军单位造成3点伤害；对所有战力为“偶数”的敌军单位造成3点伤害；或从牌组随机打出1个铜色/银色单位。
-		public Sihil(GameCard card) : base(card){}
-		public override async Task<int> CardUseEffect()
-		{
-			return 0;
-		}
-	}
+    [CardEffectId("12042")]//矮人符文剑
+    public class Sihil : CardEffect
+    {//择一：对所有战力为“奇数”的敌军单位造成3点伤害；对所有战力为“偶数”的敌军单位造成3点伤害；或从牌组随机打出1个铜色/银色单位。均衡分裂装备https://www.bilibili.com/video/av17561142?from=search&seid=13434231812588843367 2215
+        public Sihil(GameCard card) : base(card) { }
+        public override async Task<int> CardUseEffect()
+        {
+            var switchCard = await Card.GetMenuSwitch(("寄生之缚", "对所有战力为“奇数”的敌军单位造成3点伤害。"), ("寄生之缚", "对所有战力为“偶数”的敌军单位造成3点伤害。"), ("低语", "从牌组随机打出1个铜色/银色单位。"));
+        }
+    }
 }
