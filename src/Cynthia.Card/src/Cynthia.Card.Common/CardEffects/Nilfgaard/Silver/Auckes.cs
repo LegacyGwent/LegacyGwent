034fe6f258bb7a241c49b6b5a7bee0c46257111f
Cynthia <1725877349@qq.com>
2019-08-19 01:07:29 +0800

    解决了奥克斯的异步调用隐患问题

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Nilfgaard/Silver/Auckes.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Nilfgaard/Silver/Auckes.cs
@@ -8,10 +8,13 @@ namespace Cynthia.Card
     public class Auckes : CardEffect
     {//改变2个单位的锁定状态。
         public Auckes(GameCard card) : base(card) { }
-        public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
             var result = await Game.GetSelectPlaceCards(Card, 2);
-            result.ForAll(x => x.Effect.Lock(Card));
+            foreach (var card in result)
+            {
+                await card.Effect.Lock(Card)
+            }
             return 0;
         }
     }
