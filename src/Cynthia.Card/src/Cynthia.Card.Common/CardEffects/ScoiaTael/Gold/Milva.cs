f5ce263e05b454d0367033b9ab999cffc1c8b202
Cynthia <1725877349@qq.com>
2019-08-27 10:19:24 +0800

    修复射手和米尔瓦的bug

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/ScoiaTael/Gold/Milva.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/ScoiaTael/Gold/Milva.cs
@@ -4,26 +4,28 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-	[CardEffectId("52008")]//米尔瓦
-	public class Milva : CardEffect
-	{//将双方最强的铜色/银色单位收回各自牌组。
-		public Milva(GameCard card) : base(card){}
-		public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-		{
-			//最强定义为点数最强（包括绿字）
+    [CardEffectId("52008")]//米尔瓦
+    public class Milva : CardEffect
+    {//将双方最强的铜色/银色单位收回各自牌组。
+        public Milva(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            //最强定义为点数最强（包括绿字）
             var mylist = Game.GetPlaceCards(Card.PlayerIndex).Where(x => x.Status.Group == Group.Silver || x.Status.Group == Group.Copper).WhereAllHighest();
             var enemylist = Game.GetPlaceCards(Game.AnotherPlayer(Card.PlayerIndex)).Where(x => x.Status.Group == Group.Silver || x.Status.Group == Group.Copper).WhereAllHighest();
             if (mylist.TryMessOne(out var mycard, Game.RNG))
             {
-                await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck, RNG.Next(0, Game.PlayersDeck[Card.PlayerIndex].Count)), mycard,refreshPoint:true);
+                mycard.Effect.Repair(true);
+                await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck, RNG.Next(0, Game.PlayersDeck[Card.PlayerIndex].Count)), mycard, refreshPoint: true);
 
             }
             if (enemylist.TryMessOne(out var enemycard, Game.RNG))
             {
-                await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck, RNG.Next(0, Game.PlayersDeck[Card.PlayerIndex].Count)), enemycard,refreshPoint:true);
+                enemycard.Effect.Repair(true);
+                await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck, RNG.Next(0, Game.PlayersDeck[Card.PlayerIndex].Count)), enemycard, refreshPoint: true);
             }
 
             return 0;
-		}
-	}
+        }
+    }
 }
