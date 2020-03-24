98de698ecf46a00e89e5e8a95a55b1812436989f
Cynthia <1725877349@qq.com>
2020-03-25 01:47:30 +0800

    添加伤害前事件

--- src/Cynthia.Card/src/Cynthia.Card.Common/GameEvent/BeforeCardHurt.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GameEvent/BeforeCardHurt.cs
@@ -1,14 +1,15 @@
 namespace Cynthia.Card
 {
     //发生"受伤"后
-    public class AfterCardHurt : Event
+    public class BeforeCardHurt : Event
     {
         public GameCard Target { get; set; }
         public GameCard Source { get; set; }
         public int Num { get; set; }
+        public bool IsCancel { get; set; } = false;
         public DamageType DamageType { get; set; }
 
-        public AfterCardHurt(GameCard target, int num, GameCard source, DamageType damageType)
+        public BeforeCardHurt(GameCard target, int num, GameCard source, DamageType damageType)
         {
             Target = target;
             Num = num;
