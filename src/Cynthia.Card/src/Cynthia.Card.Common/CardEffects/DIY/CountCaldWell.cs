34a2cf5c262e501665812448b8c1871c18da6c00
neal2018 <neal.lion@icloud.com>
2020-03-26 00:28:44 +0800

    增加两张卡

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/CountCaldWell.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/CountCaldWell.cs
@@ -5,12 +5,57 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("70002")]//考德威尔伯爵
-    public class CountCaldWell : CardEffect
+    public class CountCaldWell : CardEffect, IHandlesEvent<AfterTurnOver>
     {//择一：从牌库中打出一张战力不高于自身的铜色单位，在回合结束把它送进墓地；或吞噬牌库中一张战力高于自身的铜色单位牌，将它的战力作为自身的增益。
         public CountCaldWell(GameCard card) : base(card) { }
+
+        private bool _needKill = false;
+        private GameCard _target = null;
         public override async Task<int> CardUseEffect()
         {
+            //选择
+            var switchCard = await Card.GetMenuSwitch
+            (
+                ("借刀", "从牌库中打出一张战力不高于自身的铜色单位，在回合结束把它送进墓地。"),
+                ("弃子", "吞噬牌库中一张战力高于自身的铜色单位牌，将它的战力作为自身的增益。")
+            );
+            if (switchCard == 0)
+            {
+                _target = null;
+                var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => (x.CardPoint() <= Card.CardPoint()) && x.Is(Group.Copper)).Mess(Game.RNG).ToList();
+                if (list.Count() == 0) { return 0; }
+                //选一张
+                var cards = await Game.GetSelectMenuCards(Card.PlayerIndex, list, 1);
+
+                if (cards.Count() == 0) { return 0; }
+
+                //打出
+                _needKill = true;
+                _target = cards.Single();
+                await cards.Single().MoveToCardStayFirst();
+                return 1;
+            }
+            else if (switchCard == 1)
+            {
+                Card.CardPoint();
+                var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => (x.CardPoint() > Card.CardPoint()) && x.Is(Group.Copper)).ToList();
+
+                //让玩家选择一张卡
+                var result = await Game.GetSelectMenuCards(Card.PlayerIndex, list, 1);
+                //如果玩家一张卡都没选择,没有效果
+                if (result.Count() == 0) { return 0; }
+
+                await Card.Effect.Consume(result.Single());
+            }
             return 0;
+        }
+        public async Task HandleEvent(AfterTurnOver @event)
+        {
+            if (_needKill)
+            {
+                await _target.Effect.ToCemetery(CardBreakEffectType.ToCemetery);
+                _needKill=false;
+            }
         }
     }
 }
