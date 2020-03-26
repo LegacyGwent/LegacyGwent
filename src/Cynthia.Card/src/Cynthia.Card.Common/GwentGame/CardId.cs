e1d85e8e51a0f87a2c05c30ee5106afc78512e5b
neal2018 <neal.lion@icloud.com>
2020-03-26 19:07:31 +0800

    加入湖中仙女（70006）

--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/CardId.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/CardId.cs
@@ -523,6 +523,7 @@ namespace Cynthia.Card
         public const string Hammond = "70003";
         public const string GlynnisAepLoernach = "70004";
         public const string VysogotaOfCorvo = "70005";
+        public const string LadyOfTheLake = "70006";
         public const string ProphetLebioda = "70007";
     }
 }
