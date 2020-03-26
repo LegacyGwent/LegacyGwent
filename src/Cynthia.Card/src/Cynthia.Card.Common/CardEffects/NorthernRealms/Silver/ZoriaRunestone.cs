0364b73f13b930dc2ec2c2ca93bbb7775cdf86a3
Cynthia <1725877349@qq.com>
2020-03-26 08:10:34 +0800

    修复符文石和战灵bug

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Silver/ZoriaRunestone.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/NorthernRealms/Silver/ZoriaRunestone.cs
@@ -10,7 +10,7 @@ namespace Cynthia.Card
         public ZoriaRunestone(GameCard card) : base(card) { }
         public override async Task<int> CardUseEffect()
         {
-            return await Card.CreateAndMoveStay(GwentMap.GetCreateCardsId(x => x.Faction == Faction.NorthernRealms && (x.Group == Group.Copper || x.Group == Group.Silver) &&
+            return await Card.CreateAndMoveStay(GwentMap.GetCreateCardsId(x => x.Faction == Faction.NorthernRealms && x.CardId != Card.Status.CardId && (x.Group == Group.Copper || x.Group == Group.Silver) &&
                     !x.HasAnyCategorie(Categorie.Agent), RNG).ToList());
         }
     }
