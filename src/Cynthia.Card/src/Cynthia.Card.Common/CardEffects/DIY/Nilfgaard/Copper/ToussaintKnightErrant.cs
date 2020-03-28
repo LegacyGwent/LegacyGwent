973e4d2ae99f51f1cdc65883b44115731c550c6b
neal2018 <neal.lion@icloud.com>
2020-03-28 16:38:35 +0800

    【铜卡】陶森特游侠骑士 ToussaintKnightErrant 帝国 单位卡 士兵 8战力 效果：部署：己方手牌每比对手少1张，便获得一次4点增益。

---
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Nilfgaard/Copper/ToussaintKnightErrant.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Alsein.Extensions;
+
+namespace Cynthia.Card
+{
+    [CardEffectId("70012")]//陶森特游侠骑士
+
+    public class ToussaintKnightErrant : CardEffect
+    {//部署：己方手牌每比对手少1张，便获得一次4点增益。
+        public ToussaintKnightErrant(GameCard card) : base(card) { }
+        private const int boostPoint = 4;
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            int myHandCardCount = Game.PlayersHandCard[PlayerIndex].Count();
+            int opponentHandCardCount = Game.PlayersDeck[Game.AnotherPlayer(Card.PlayerIndex)].Count();
+            
+            int boostTimes = 0;
+            if (opponentHandCardCount > myHandCardCount)
+            {
+                boostTimes = opponentHandCardCount - myHandCardCount;
+            }
+
+            for (int i = 0; i < boostTimes; i++)
+            {
+                await Card.Effect.Boost(boostPoint, Card);
+            }
+            return 0;
+        }
+    }
+}
