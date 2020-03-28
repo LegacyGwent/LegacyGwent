87742c602dfb9570215664831a21d333b4ffcabc
neal2018 <neal.lion@icloud.com>
2020-03-28 16:44:06 +0800

    米尔加塔布雷克【新效果】8 战力，部署：造成 3、2、1 点伤害。

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/Myrgtabrakke.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/Myrgtabrakke.cs
@@ -9,7 +9,7 @@ namespace Cynthia.Card
     {//造成2点伤害，再重复2次。
         public Myrgtabrakke(GameCard card) : base(card) { }
 
-        private int[] damageValue = { 2, 2, 1 };
+        private int[] damageValue = { 3, 2, 1 };
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
             for (var i = 0; i < 3; i++)
