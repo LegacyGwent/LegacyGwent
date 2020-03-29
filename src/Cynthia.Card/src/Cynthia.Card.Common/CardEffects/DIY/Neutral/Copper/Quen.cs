c24bdc73f3498c3b15d98195a01c83ce2077ab5f
neal2018 <neal.lion@icloud.com>
2020-03-29 19:49:08 +0800

    增加昆恩护盾的效果

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Copper/Quen.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Copper/Quen.cs
@@ -8,18 +8,25 @@ namespace Cynthia.Card
     public class Quen : CardEffect
     {//给予手牌中一个铜色单位及其牌组中的同名卡一层护盾，阻挡一次削弱/伤害/重置效果。
         public Quen(GameCard card) : base(card) { }
-        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        public override async Task<int> CardUseEffect()
         {
-            //打乱己方卡组,并且取2张铜色卡
-            var list = Game.PlayersDeck[Card.PlayerIndex]
-            .Where(x => x.Status.Group == Group.Copper && x.CardInfo().CardType == CardType.Unit).Mess(RNG).Take(2);
-            //让玩家选择一张卡
-            var result = await Game.GetSelectMenuCards
-            (Card.PlayerIndex, list.ToList(), 1, "选择打出一张牌");
-            //如果玩家一张卡都没选择,没有效果
-            if (result.Count() == 0) return 0;
-            await result.First().MoveToCardStayFirst();
-            return 1;
+            var unitHandCard = Game.PlayersHandCard[PlayerIndex].Where(x => x.Status.Type == CardType.Unit).ToList();
+            var targetcards = await Game.GetSelectMenuCards(PlayerIndex, unitHandCard, isCanOver: false);
+            if (!targetcards.TrySingle(out var target)) { return 0; }
+
+            var deckCardList = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.CardInfo().CardId == target.CardInfo().CardId);
+            var handCardList = Game.PlayersHandCard[Card.PlayerIndex].Where(x => x.CardInfo().CardId == target.CardInfo().CardId);
+
+            foreach (var card in handCardList)
+            {
+                card.Status.IsShield = true;
+            }
+
+            foreach (var card in deckCardList)
+            {
+                card.Status.IsShield = true;
+            }
+            return 0;
         }
     }
 }
