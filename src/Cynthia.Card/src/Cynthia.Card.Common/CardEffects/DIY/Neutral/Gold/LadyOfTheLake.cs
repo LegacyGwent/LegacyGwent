8c65d240b29c33d3500ffb8bd95b8d49366984e9
neal2018 <neal.lion@icloud.com>
2020-03-30 14:37:24 +0800

    削弱湖中仙女：部署：对自身造成削弱，削弱数值等同于手牌和牌库剩余卡牌之和的两倍。

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/LadyOfTheLake.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/LadyOfTheLake.cs
@@ -5,13 +5,14 @@ namespace Cynthia.Card
 {
     [CardEffectId("70006")]//湖中仙女
     public class LadyOfTheLake : CardEffect
-    {//部署：对自身造成等同于己方牌组中剩余牌数量与手牌数量之和的削弱。
+    {//部署：对自身造成削弱，削弱数值等同于手牌和牌库剩余卡牌之和的两倍。
         public LadyOfTheLake(GameCard card) : base(card) { }
+        private const int mutipleFactor = 2;
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
             int weakenPointFromHand = Game.PlayersHandCard[Card.PlayerIndex].Count();
             int weakenPointFromDeck = Game.PlayersDeck[Card.PlayerIndex].Count();
-            int totalWeakenPoint = weakenPointFromHand+ weakenPointFromDeck;
+            int totalWeakenPoint = (weakenPointFromHand + weakenPointFromDeck) * mutipleFactor;
 
             await Card.Effect.Weaken(totalWeakenPoint, Card);
 
