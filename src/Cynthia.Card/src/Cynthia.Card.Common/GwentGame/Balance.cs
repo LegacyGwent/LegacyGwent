4eebedd23601504cee71820b57fa6835becbbda5
neal2018 <neal.lion@icloud.com>
2020-04-02 22:22:21 +0800

    将拼点范围调整为-5~30点

--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/Balance.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/Balance.cs
@@ -6,8 +6,8 @@ namespace Cynthia.Card.Common.GwentGame
 {
     public class Balance
     {
-        public const int ComparePointMin = 1;
-        public const int ComparePointMax = 10;
+        public const int ComparePointMin = -5;
+        public const int ComparePointMax = 30;
 
         public const string ShowCardId = "12004";
     }
