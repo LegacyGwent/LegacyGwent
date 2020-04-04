b87531888035b1cd65a89a4935429c3f76ee78d8
Cynthia <1725877349@qq.com>
2020-04-05 00:00:30 +0800

    2新卡

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Silver/Sukrus.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Silver/Sukrus.cs
@@ -7,9 +7,37 @@ namespace Cynthia.Card
 {
     [CardEffectId("70003")]//苏克鲁斯
     public class Sukrus : CardEffect
-    {//部署：选择手牌中的一张同名牌，丢弃所有牌组中该牌的同名牌。
+    {//部署：选择手牌中的一张铜卡，丢弃所有牌组中该牌的同名牌。
         public Sukrus(GameCard card) : base(card)
         {
+        }
+
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            var unitHandCard = Game.PlayersHandCard[PlayerIndex]
+                .Where(x =>
+                    x.Status.Type == CardType.Unit &&
+                    x.IsAnyGroup(Group.Copper)
+                )
+                .ToList();
+            if (unitHandCard.Count() == 0)
+            {
+                return 0;
+            }
+
+            var targetcards = await Game.GetSelectMenuCards(PlayerIndex, unitHandCard, isCanOver: false);
+            if (!targetcards.TrySingle(out var target))
+            {
+                return 0;
+            }
+
+            var result = Game.PlayersDeck[PlayerIndex].Where(x => x.Status.CardId == target.Status.CardId).ToList();
+
+            foreach (var card in result)
+            {
+                await card.Effect.Discard(Card);
+            }
+            return 0;
         }
     }
 }
