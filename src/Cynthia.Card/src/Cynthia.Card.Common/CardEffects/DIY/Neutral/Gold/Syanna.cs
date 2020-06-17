4ec9072eb17d485a897c0c110e2afda9f4f55abf
neal2018 <neal.lion@icloud.com>
2020-06-18 02:03:09 +0800

    席安娜效果更改：11点战力 -> 6战力，4护甲。

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/Syanna.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/Syanna.cs
@@ -7,7 +7,7 @@ namespace Cynthia.Card
 {
     [CardEffectId("70025")]//席安娜
     public class Syanna : CardEffect, IHandlesEvent<AfterUnitPlay>, IHandlesEvent<AfterUnitDown>
-    {//力竭。使你的下一张银色/铜色忠诚单位卡额外触发一次部署效果。
+    {//4护甲。力竭。使你的下一张银色/铜色忠诚单位卡额外触发一次部署效果。
 
         private bool _isUse = false;
 
@@ -15,6 +15,7 @@ namespace Cynthia.Card
 
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
+            await Card.Effect.Armor(4, Card);
             if (_isUse == false)
             {
                 await Card.Effect.SetCountdown(1);
