76a4d5890b0fca39acee71532842541ae4b57092
Cynthia <38712063+DeusSeuca@users.noreply.github.com>
2020-08-19 05:05:43 +0800

    修复卡图

--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
@@ -8,7 +8,7 @@ namespace Cynthia.Card
     public static class GwentMap
     {
         //更新CardMap内容请务必将CardMapVersion更新
-        public static Version CardMapVersion { get; } = new Version(1, 0, 0, 15);
+        public static Version CardMapVersion { get; } = new Version(1, 0, 0, 16);
         public static IDictionary<string, int> CardIdMap { get; set; }
         public static string[] CardIdIndexMap { get; set; }
 
@@ -11168,7 +11168,7 @@ namespace Cynthia.Card
                     Categories = new Categorie[] { Categorie.Soldier},
                     Flavor = "",
                     Info = "对一个敌军随机单位造成3点伤害。被丢弃时，再次触发此能力，并将2张恐狼持斧者加入牌组。",
-                    CardArtsId = "d19640000",
+                    CardArtsId = "d19620000",
                 }
             },
             {
@@ -11188,7 +11188,7 @@ namespace Cynthia.Card
                     Categories = new Categorie[] { Categorie.Soldier},
                     Flavor = "",
                     Info = "对一个敌军随机单位造成3点伤害。被丢弃时，再次触发此能力，并将1张恐狼持斧者加入牌组底部。",
-                    CardArtsId = "d19650000",
+                    CardArtsId = "d19640000",
                 }
             },
             {
