ceb014d80b573cf38ded46ad804064d12108b85f
lwr51 <1146498931@qq.com>
2021-06-19 11:35:12 +0800

    上次上传假死问题的修复

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Expantion01/ScoiaTael/FeignDeath.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Expantion01/ScoiaTael/FeignDeath.cs
@@ -6,21 +6,20 @@ namespace Cynthia.Card
 {
     [CardEffectId("70054")]//假死 FeignDeath
     public class FeignDeath : CardEffect
-    {//复活2个战力高于5点的铜色单位，并对它们各造成5点伤害。
+    {//复活2个战力高于4点的铜色单位，并对它们各造成4点伤害。
         public FeignDeath(GameCard card) : base(card) { }
         public override async Task<int> CardUseEffect()
         {
             var list = Game.PlayersCemetery[Card.PlayerIndex]
-            .Concat(Game.PlayersCemetery[Game.AnotherPlayer(Card.PlayerIndex)])
             .Where(x => x.Status.Group == Group.Copper 
             && x.CardInfo().CardType == CardType.Unit 
-            && x.Status.Strength > 5).ToList();
+            && x.Status.Strength > 4).ToList();
             //让玩家选择
             var result = await Game.GetSelectMenuCards(Card.PlayerIndex, list, 2, isCanOver: true);
             foreach (var x in result.ToList())
             {
                 await x.Effect.Resurrect(new CardLocation() { RowPosition = RowPosition.MyStay, CardIndex = 0 }, Card);
-                await x.Effect.Damage(5, Card);
+                await x.Effect.Damage(4, Card);
             }
             return result.Count();
         }
