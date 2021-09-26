08d6e7db262b64318cdac995d9b0bd3e9cff7e83
newhandapo <bpo2008@live.cn>
2021-09-27 01:26:30 +0900

    format code

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Nilfgaard/Silver/ArtoriusVigo.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Nilfgaard/Silver/ArtoriusVigo.cs
@@ -6,19 +6,19 @@ using System;
 namespace Cynthia.Card
 {
     [CardEffectId("70103")]//亚托列司·薇歌 ArtoriusVigo
-    public class ArtoriusVigo: CardEffect
+    public class ArtoriusVigo : CardEffect
     {//丢弃1张手牌，并在手牌中添加1张己方起始牌组中铜色单位牌，随后将其揭示。
         public ArtoriusVigo(GameCard card) : base(card) { }
-        public override async Task<int> CardPlayEffect(bool isSpying,bool isReveal)
-        {   
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
             //丢弃1张手牌
             var cards = await Game.GetSelectMenuCards(PlayerIndex, Game.PlayersHandCard[PlayerIndex]);
-            if(!cards.TrySingle(out var target))
+            if (!cards.TrySingle(out var target))
             {
                 return 0;
             }
             var targetGroup = target.Status.Group;
-            if(!Game.PlayerBaseDeck[PlayerIndex].Deck.Where(x=>x.Group==targetGroup).Select(x=>x.CardId).TryMessOne(out var targetId,Game.RNG))
+            if (!Game.PlayerBaseDeck[PlayerIndex].Deck.Where(x => x.Group == targetGroup).Select(x => x.CardId).TryMessOne(out var targetId, Game.RNG))
             {
                 return 0;
             }
@@ -26,7 +26,7 @@ namespace Cynthia.Card
 
             //并在手牌中添加1张己方起始牌组中铜色单位牌
             var list = Game.PlayerBaseDeck[PlayerIndex].Deck.Where(x => x.Is(Group.Copper, CardType.Unit));
-            var selectList = list.Select(x=>new CardStatus(x.CardId)).ToList();
+            var selectList = list.Select(x => new CardStatus(x.CardId)).ToList();
             var result = (await Game.GetSelectMenuCards(PlayerIndex, selectList, isCanOver: false, title: "选择一张牌"));
             if (!(result).TrySingle(out var targetIndex))
             {
@@ -37,7 +37,7 @@ namespace Cynthia.Card
 
             //随后将其揭示
             await createCard.Effect.Reveal(Card);
-            
+
             return 0;
         }
     }
