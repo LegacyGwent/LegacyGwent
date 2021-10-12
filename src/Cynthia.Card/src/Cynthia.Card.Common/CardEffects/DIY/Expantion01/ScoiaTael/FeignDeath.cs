5097617597f2918c814c334a59d832fe03cdeb4b
C-lab-1 <shunfany@sfu.ca>
2021-10-12 00:47:39 -0700

    黑名单更正：现在能够禁止的范围为所有金卡 平衡性修改：弗妮希尔的突击队，齐齐摩战士，狄拉夫：猩红诅咒，假死，洞穴巨魔，劫掠者。bug修复：恩希尔不召唤萝卜

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Expantion01/ScoiaTael/FeignDeath.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Expantion01/ScoiaTael/FeignDeath.cs
@@ -6,17 +6,17 @@ namespace Cynthia.Card
 {
     [CardEffectId("70054")]//假死 FeignDeath
     public class FeignDeath : CardEffect
-    {//复活2个战力高于4点的铜色单位，并对它们各造成4点伤害。
+    {//复活2个战力高于5点的铜色单位，并对它们各造成4点伤害。
         public FeignDeath(GameCard card) : base(card) { }
         public override async Task<int> CardUseEffect()
         {
             var list = Game.PlayersCemetery[Card.PlayerIndex]
-            .Where(x => x.Status.Group == Group.Copper 
-            && x.CardInfo().CardType == CardType.Unit 
-            && x.Status.Strength > 4).ToList();
+            .Where(x => x.Status.Group == Group.Copper
+            && x.CardInfo().CardType == CardType.Unit
+            && x.Status.Strength > 5).ToList();
             //让玩家选择
             var result = await Game.GetSelectMenuCards(Card.PlayerIndex, list, 2, isCanOver: true);
-            for(int i=result.Count()-1;i>=0;i--)
+            for (int i = result.Count() - 1; i >= 0; i--)
             {
                 await result[i].Effect.Resurrect(new CardLocation() { RowPosition = RowPosition.MyStay, CardIndex = 0 }, Card);
                 await result[i].Effect.Damage(4, Card);
