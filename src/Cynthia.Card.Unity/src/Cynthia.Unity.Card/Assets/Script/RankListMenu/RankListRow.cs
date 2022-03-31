58dc1b98cb71021228e793f7f8f48ed754dcdb0b
neal <neal.lion@icloud.com>
2022-03-31 01:03:10 -0400

    feat: add self on the board

--- src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Script/RankListMenu/RankListRow.cs
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Script/RankListMenu/RankListRow.cs
@@ -8,11 +8,27 @@ public class RankListRow : MonoBehaviour
     public Text RankNumber;
     public Text Playername;
     public Text MMR;
-    
+
     public void SetRankListRow(int rankNumber, string playername, int mmr)
     {
         RankNumber.text = "Rank " + rankNumber;
         Playername.text = playername;
         MMR.text = mmr.ToString();
+    }
+    public void SetRankListRowString(string rankNumber, string playername, int mmr)
+    {
+        RankNumber.text = "Rank " + rankNumber;
+        Playername.text = playername;
+        MMR.text = mmr.ToString();
+    }
+    public void SetHighlight()
+    {
+        Playername.color = RankNumber.color;
+    }
+    public void SetTransparency()
+    {
+        RankNumber.color = Color.clear;
+        Playername.color = Color.clear;
+        MMR.color = Color.clear;
     }
 }
