be101ba41d8a0c301b739d6def256477dd16617c
fraloow <50948805+fraloow@users.noreply.github.com>
2023-03-11 21:47:12 +0800

    Update Keltullis.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Monsters/Gold/Keltullis.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Monsters/Gold/Keltullis.cs
@@ -5,22 +5,22 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("70113")]//克尔图里斯
-    public class Keltullis : CardEffect, IHandlesEvent<AfterTurnStart>
+    public class Keltullis : CardEffect, IHandlesEvent<AfterTurnOver>
     {//6护甲，回合结束时随机摧毁1个场上战力最低且低于自身基础战力的单位。
         public Keltullis(GameCard card) : base(card) { }
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
-            await Armor(6, Card);
+            await Armor(2, Card);
             return 0;
         }
 
-        public async Task HandleEvent(AfterTurnStart @event)
+        public async Task HandleEvent(AfterTurnOver @event)
         {
             if (@event.PlayerIndex != PlayerIndex || !Card.Status.CardRow.IsOnPlace())
             {
                 return;
             }
-            if (!Game.GetAllCard(Game.AnotherPlayer(Card.PlayerIndex)).FilterCards(filter: x => x.CardPoint() < Card.Status.Strength).Where(x => x.Status.CardRow.IsOnPlace()).WhereAllLowest().TryMessOne(out var target, Game.RNG))
+            if (!Game.GetAllCard(Game.AnotherPlayer(Card.PlayerIndex)).FilterCards(filter: x => x.CardPoint() < 6 && x != Card).Where(x => x.Status.CardRow.IsOnPlace()).WhereAllLowest().TryMessOne(out var target, Game.RNG))
             {
                 return;
             }
