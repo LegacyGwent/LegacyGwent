5e40c43e014a50962b9de0485451041e46222e0b
Azazel Wang <68633685+azazelw@users.noreply.github.com>
2023-06-26 21:18:30 +0800

    DIY

---
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/SyannagoldenmilleTDream.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Alsein.Extensions;
+
+namespace Cynthia.Card
+{
+    [CardEffectId("72043")]//
+    public class SyannagoldenmilleTDream : CardEffect
+    {//
+        public SyannagoldenmilleTDream(GameCard card) : base(card) { }
+        public override async Task<int> CardUseEffect()
+        {
+            var cards = Game.GetAllCard(Game.AnotherPlayer(Card.PlayerIndex)).Where(x => x.Status.CardRow.IsOnPlace()).WhereAllHighest().ToList();
+            foreach (var card in cards)
+            {
+                //if (card.Status.HealthStatus > 0) 
+                    await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
+                card.Status.IsDoomed = true;
+            }
+            return 0;
+
+        }
+    }
+}
