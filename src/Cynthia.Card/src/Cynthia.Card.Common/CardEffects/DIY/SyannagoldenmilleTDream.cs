57e4c828ef9b45e49335e519f673de176eef5d80
Azazel Wang <68633685+azazelw@users.noreply.github.com>
2023-06-30 09:05:14 +0800

    Update SyannagoldenmilleTDream.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/SyannagoldenmilleTDream.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/SyannagoldenmilleTDream.cs
@@ -4,18 +4,21 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-    [CardEffectId("72043")]//
+    [CardEffectId("72043")]//席安娜，“黄粱一梦”
     public class SyannagoldenmilleTDream : CardEffect
-    {//
+    {//摧毁场上战力最高的牌。若其已被增益，则使其被放逐。
         public SyannagoldenmilleTDream(GameCard card) : base(card) { }
         public override async Task<int> CardUseEffect()
         {
             var cards = Game.GetAllCard(Game.AnotherPlayer(Card.PlayerIndex)).Where(x => x.Status.CardRow.IsOnPlace()).WhereAllHighest().ToList();
             foreach (var card in cards)
             {
-                //if (card.Status.HealthStatus > 0) 
-                    await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
+                if (card.Status.HealthStatus > 0) 
+                {await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
                 card.Status.IsDoomed = true;
+                }
+            else
+            await card.Effect.ToCemetery(CardBreakEffectType.Scorch);
             }
             return 0;
 
