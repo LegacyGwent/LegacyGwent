1dd7de377443489f26ba47664b532db89b44a967
neal <46561493+neal2018@users.noreply.github.com>
2023-06-30 14:04:40 +0800

    Update SyannagoldenmilleTDream.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/SyannagoldenmilleTDream.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/SyannagoldenmilleTDream.cs
@@ -5,24 +5,24 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
      [CardEffectId("70082")]//席安娜:黄粱一梦
-      public class SyannagoldenmilleTDream : CardEffect
-      {//使场上战力最高的牌获得“佚亡”效果。若其已被增益，则使其直接被放逐。
-      public SyannagoldenmilleTDream(GameCard card) : base(card) { }
-      public override async Task<int> CardUseEffect()
+     public class SyannagoldenmilleTDream : CardEffect
+     {//使场上战力最高的牌获得“佚亡”效果。若其已被增益，则使其直接被放逐。
+          public SyannagoldenmilleTDream(GameCard card) : base(card) { }
+          public override async Task<int> CardUseEffect()
           {
-             var cards = Game.GetAllCard(Game.AnotherPlayer(Card.PlayerIndex)).Where(x =>x.Status.CardRow.IsOnPlace()).WhereAllHighest().ToList();
-             foreach (var card in cards)
-             {
-               if (Card.Status.HealthStatus > 0 )
-                {
-                  await card.Effect.Banish();
-                }
-               else
-                {
-                  card.Status.IsDoomed = true;
-                }
-             }
-            return 0;
-           }
-       }
+               var cards = Game.GetAllCard(Game.AnotherPlayer(Card.PlayerIndex)).Where(x =>x.Status.CardRow.IsOnPlace()).WhereAllHighest().ToList();
+               foreach (var card in cards)
+               {
+                    if (Card.Status.HealthStatus > 0 )
+                    {
+                         await card.Effect.Banish();
+                    }
+                    else
+                    {
+                         card.Status.IsDoomed = true;
+                    }
+               }
+               return 0;
+          }
+     }
 }
