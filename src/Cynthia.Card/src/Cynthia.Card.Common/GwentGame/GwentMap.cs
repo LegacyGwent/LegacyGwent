6c247ea071d694d3d96960a2dd0943c42fb95c70
Azazel Wang <68633685+azazelw@users.noreply.github.com>
2023-06-30 15:08:45 +0800

    Update GwentMap.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
@@ -12577,7 +12577,7 @@ namespace Cynthia.Card
                     IsDerive = false,
                     Categories = new Categorie[]{ Categorie.Officer},
                     Flavor = "席安娜的命运不像她的名字一样美丽，也不像她的出身一样高贵。一切都因为她出生在错误的时刻。",
-                    Info = "使场上战力最高的牌获得“佚亡”效果。若其已被增益，则使其直接被放逐。",
+                    Info = "使所有最强的单位获得“佚亡”，然后放逐其中具有增益的单位。",
                     CardArtsId = "c10001500",
                 }
             },
