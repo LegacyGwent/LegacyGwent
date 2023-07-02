f28a6839faef165708b089f0a6a7ad6c048381d6
neal <46561493+neal2018@users.noreply.github.com>
2023-07-03 01:44:07 +0800

    Update GwentMap.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
@@ -8,7 +8,7 @@ namespace Cynthia.Card
     public static class GwentMap
     {
         //更新CardMap内容请务必将CardMapVersion更新
-        public static Version CardMapVersion { get; } = new Version(1, 0, 0, 64);
+        public static Version CardMapVersion { get; } = new Version(1, 0, 0, 65);
         public static IDictionary<string, int> CardIdMap { get; set; }
         public static string[] CardIdIndexMap { get; set; }
 
