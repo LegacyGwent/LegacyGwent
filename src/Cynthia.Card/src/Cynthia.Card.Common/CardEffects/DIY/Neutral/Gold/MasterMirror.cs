2c5671d84ff0ed978859868d34a56a461a5ae258
Azazel Wang <68633685+azazelw@users.noreply.github.com>
2023-07-02 11:10:50 +0800

    Update and rename SyannagoldenmilleTDream.cs to MasterMirror.cs
    change name to MasterMirror

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/MasterMirror.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/MasterMirror.cs
@@ -4,10 +4,10 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-     [CardEffectId("70082")]//席安娜:黄粱一梦
-     public class SyannagoldenmilleTDream : CardEffect
+     [CardEffectId("70082")]//镜子大师
+     public class MasterMirror : CardEffect
      {//使所有最强的单位获得“佚亡”，然后摧毁其中具有增益的单位。
-          public SyannagoldenmilleTDream(GameCard card) : base(card) { }
+          public MasterMirror(GameCard card) : base(card) { }
           public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
           {
                var cards = Game.GetAllCard(Game.AnotherPlayer(Card.PlayerIndex)).Where(x =>x.Status.CardRow.IsOnPlace()).WhereAllHighest().ToList();
