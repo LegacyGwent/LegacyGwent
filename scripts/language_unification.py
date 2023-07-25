bb2a5f7dbd087cda6033246c98c7f185506e526a
kajtek14102 <kajtek14102@gmail.com>
2023-07-25 15:12:06 +0200

    script

--- scripts/language_unification.py
+++ scripts/language_unification.py
@@ -10,20 +10,50 @@
 """
 import os
 import shutil
+import json
+
+
+def unify_language(path,languages):
+    for language in languages:
+        from_path = os.path.join(path, "src", "Cynthia.Card", "src", "Cynthia.Card.Server","Locales", language+'.json')
+        
+        to_path = os.path.join(path, "src", "Cynthia.Card.Unity", "src", "Cynthia.Unity.Card", "Assets", "Resources", "Locales", language+'.json')
+        shutil.copyfile(from_path, to_path)
+        
+        to_path = os.path.join(path, "src", "Cynthia.Card.Unity", "src", "Cynthia.Unity.Card", "Assets", "StreamingFile", "Locales", language+'.json')
+        shutil.copyfile(from_path, to_path)
+        
+        to_path = os.path.join(path, "src", "Cynthia.Card", "src", "Cynthia.Card.Server", "bin", "Debug", "netcoreapp3.0","locales", language+'.json')
+        shutil.copyfile(from_path, to_path)
+        print(language +' unified')
+    
+
+
+def sort(path,languages):
+    for language in languages:
+        full_path= os.path.join(path, "src", "Cynthia.Card", "src", "Cynthia.Card.Server","Locales", language+'.json')
+        with open(full_path,encoding="utf-8") as file:
+            source=json.load(file)
+        cards=source['CardLocales']
+        ids = [int(x) for x in list(cards.keys())]
+        ids.sort()
+        ids = [x for x in ids if x<130000]
+        ids = [ str(x) for x in ids]
+        sorted_cards = {i: cards[i] for i in ids}
+        source['CardLocales']=sorted_cards
+        with open(full_path, 'w',encoding="utf-8") as outfile:
+            json.dump(source, outfile,indent=4, ensure_ascii=False)
+        print(language +' sorted')
+        
+def main():
+    languages=['cn','pl','ru','en']
+    path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sort(path,languages)
+    unify_language(path,languages)
+
+
+if __name__ == "__main__":
+    main()
 
 
 
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-languages=['cn','en','pl','ru']
-for language in languages:
-    print(language)
-    dll_path = os.path.join(path, "src", "Cynthia.Card", "src", "Cynthia.Card.Server","Locales", language+'.json')
-    
-    to_path = os.path.join(path, "src", "Cynthia.Card.Unity", "src", "Cynthia.Unity.Card", "Assets", "Resources", "Locales", language+'.json')
-    shutil.copyfile(dll_path, to_path)
-    
-    to_path = os.path.join(path, "src", "Cynthia.Card.Unity", "src", "Cynthia.Unity.Card", "Assets", "StreamingFile", "Locales", language+'.json')
-    shutil.copyfile(dll_path, to_path)
-    
-    to_path = os.path.join(path, "src", "Cynthia.Card", "src", "Cynthia.Card.Server", "bin", "Debug", "netcoreapp3.0","locales", language+'.json')
-    shutil.copyfile(dll_path, to_path)
