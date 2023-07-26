c70097f39868265dbf4af31e5ec3c0abd2be7be8
iceshoesss <iceshoesss@gmail.com>
2023-07-26 17:22:16 +0800

    fix roach

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/Roach.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/Roach.cs
@@ -4,16 +4,16 @@ using System.Threading.Tasks;
 namespace Cynthia.Card
 {
     [CardEffectId("13001")]//萝卜
-    public class Roach : CardEffect, IHandlesEvent<AfterUnitDown>
+    public class Roach : CardEffect, IHandlesEvent<AfterUnitPlay>
     {
         public Roach(GameCard card) : base(card) { }
 
-        public async Task HandleEvent(AfterUnitDown @event)
+        public async Task HandleEvent(AfterUnitPlay @event)
         {
-            if (@event.Target.Status.Group == Group.Gold && @event.Target.PlayerIndex == Card.PlayerIndex
-                && Card.Status.CardRow.IsInDeck() && !@event.IsSpying && !@event.IsMoveInfo.isMove)
+            if (@event.PlayedCard.Status.Group == Group.Gold && @event.PlayedCard.PlayerIndex == Card.PlayerIndex
+                && Card.Status.CardRow.IsInDeck() && !@event.IsSpying)
             {
-                await Card.Effect.Summon(Game.GetRandomCanPlayLocation(Card.PlayerIndex, true), @event.Target);
+                await Card.Effect.Summon(Game.GetRandomCanPlayLocation(Card.PlayerIndex, true), @event.PlayedCard);
             }
         }
     }
