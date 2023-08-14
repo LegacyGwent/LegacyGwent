5a459c86ae4ea3e00d211d76ccd80eac004288af
iceshoesss <46239483+iceshoesss@users.noreply.github.com>
2023-08-14 17:25:59 +0800

    fix(roach):summon only when playing the gold card (#494)

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/Roach.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/Roach.cs
@@ -4,14 +4,28 @@ using System.Threading.Tasks;
 namespace Cynthia.Card
 {
     [CardEffectId("13001")]//萝卜
-    public class Roach : CardEffect, IHandlesEvent<AfterUnitDown>
+    public class Roach : CardEffect, IHandlesEvent<AfterUnitDown>, IHandlesEvent<AfterUnitPlay>
     {
         public Roach(GameCard card) : base(card) { }
+
+        private bool canSummon = false;
+
+        public async Task HandleEvent(AfterUnitPlay @event)
+        {
+            if (@event.PlayedCard.Status.Group == Group.Gold && @event.PlayedCard.PlayerIndex == Card.PlayerIndex
+                && Card.Status.CardRow.IsInDeck() && !@event.IsSpying)
+            {
+                canSummon = true;
+                return;
+            }
+            await Task.CompletedTask;
+            return;
+        }
 
         public async Task HandleEvent(AfterUnitDown @event)
         {
             if (@event.Target.Status.Group == Group.Gold && @event.Target.PlayerIndex == Card.PlayerIndex
-                && Card.Status.CardRow.IsInDeck() && !@event.IsSpying && !@event.IsMoveInfo.isMove)
+                && Card.Status.CardRow.IsInDeck() && !@event.IsSpying && !@event.IsMoveInfo.isMove && canSummon)
             {
                 await Card.Effect.Summon(Game.GetRandomCanPlayLocation(Card.PlayerIndex, true), @event.Target);
             }
