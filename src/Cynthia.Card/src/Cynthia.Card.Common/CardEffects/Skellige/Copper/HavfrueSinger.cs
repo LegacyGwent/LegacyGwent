dbe0e2faa2a3c565cbf53a580e729eeb5437bd21
NPC-YFY <124620892+NPC-YFY@users.noreply.github.com>
2023-08-15 21:48:07 +0800

    Update HavfrueSinger.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/HavfrueSinger.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Skellige/Copper/HavfrueSinger.cs
@@ -4,9 +4,20 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-if (Game.GameRowEffect[Card.PlayerIndex][Card.Status.CardRow.MyRowToIndex()].RowStatus == RowStatus.SkelligeStorm)|| (Game.GameRowEffect[Card.PlayerIndex][Card.Status.CardRow.MyRowToIndex()].RowStatus == RowStatus.TorrentialRain)
+    [CardEffectId("70091")]//海之歌者
+    pubic class HavfrueSinger : CardEffect
+    {
+        public HavfrueSinger(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            if (Game.GameRowEffect[Card.PlayerIndex][Card.Status.CardRow.MyRowToIndex()].RowStatus == RowStatus.SkelligeStorm)|| (Game.GameRowEffect[Card.PlayerIndex][Card.Status.CardRow.MyRowToIndex()].RowStatus == RowStatus.TorrentialRain)
             {var selectCard = await Game.GetSelectPlaceCards(Card, 2, selectMode: SelectModeType.EnemyRow, filter: x => x.Status.CardRow != Card.Status.CardRow);
             foreach (var target in selectCard)
             {
                 await target.Effect.Move(new CardLocation(Card.Status.CardRow, int.MaxValue), Card);
             }
+            return 0;
+            }
+        }
+    }
+}
