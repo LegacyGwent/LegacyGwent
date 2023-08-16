d9e72283ddb5a306a82ab1bf79839c5a7bf10afd
NPC-YFY <124620892+NPC-YFY@users.noreply.github.com>
2023-08-17 02:28:19 +0800

    Update TearsofSiren.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Silver/TearsofSiren.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Silver/TearsofSiren.cs
@@ -5,18 +5,26 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("70093")]//女海妖之泪
-    public class ClearSkies : CardEffect
+    public class TearsofSiren : CardEffect
     {//使灾厄下的所有受伤友军单位获得2点增益，并清除己方半场所有灾厄。
         public TearsofSiren(GameCard card) : base(card) { }
         public override async Task<int> CardUseEffect()
         {
             var tagetRows = Game.GameRowEffect[Card.PlayerIndex].Indexed()
-                .Where(x => x.Value.RowStatus.IsHazard())
+                .Where(x => x.Value.RowStatus.TorrentialRain())
                 .Select(x => x.Key);
             foreach (var rowIndex in tagetRows)
             {
                 // await Game.ApplyWeather(Card.PlayerIndex, rowIndex.IndexToMyRow(), RowStatus.None);
-                await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<NoneStatus>();
+                await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<SkelligeStormStatus>();
+            }
+            var tagetRows = Game.GameRowEffect[Card.PlayerIndex].Indexed()
+                .Where(x => !x.Value.RowStatus.SkelligeStormStatus())
+                .Select(x => x.Key);
+            foreach (var rowIndex in tagetRows)
+            {
+                // await Game.ApplyWeather(Card.PlayerIndex, rowIndex.IndexToMyRow(), RowStatus.None);
+                await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<TorrentialRain>();
             }
             return 0;
         }
