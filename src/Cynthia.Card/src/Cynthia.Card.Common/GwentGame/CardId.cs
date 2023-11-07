b48cafa9dbd359d255052256e9561de2ce932b30
fraloow <50948805+fraloow@users.noreply.github.com>
2023-11-07 11:48:43 +0800

    Update CardId.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/CardId.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/CardId.cs
@@ -582,6 +582,8 @@ namespace Cynthia.Card
         public const string CultistOblation = "70090";
         public const string HavfrueSinger = "70092";
         public const string Tempest ="70093";
+        public const string LyrianCavalry = "70094";
+        public const string LyrianArbalest = "70095";
         public const string AuberonKing = "80001";
         public const string AuberonInvader = "80002";
         public const string AuberonConqueror = "80003";
@@ -589,7 +591,6 @@ namespace Cynthia.Card
         public const string IronFalconInfantry = "89005";
         public const string IronFalconTroubadour = "89006";
         public const string IronFalconKnifeJuggler = "89007";
-        public const string LyrianCavalry = "70094";
-        public const string LyrianArbalest = "70095";
+        public const string SoldierTrain = "89008";
     }
 }
