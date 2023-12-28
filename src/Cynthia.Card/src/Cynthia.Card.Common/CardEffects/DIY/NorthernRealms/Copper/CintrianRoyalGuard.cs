01af987ee5d33c7c06cc6a3593ba9e349838949d
fraloow <50948805+fraloow@users.noreply.github.com>
2023-12-28 17:04:53 +0800

    Create CintrianRoyalGuard.cs

---
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/NorthernRealms/Copper/CintrianRoyalGuard.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Alsein.Extensions;
+using System;
+
+namespace Cynthia.Card
+{
+    [CardEffectId("70109")]//辛特拉皇家护卫 CintrianRoyalGuard
+    public class CintrianRoyalGuard : CardEffect
+    {//己方总点数落后时，使同名牌获得3点增益。
+        public CintrianRoyalGuard(GameCard card) : base(card) { }
+        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
+        {
+            if ((Game.GetPlayersPoint(PlayerIndex) > Game.GetPlayersPoint(AnotherPlayer)))
+            {
+                return 0;
+            }
+            
+            var cards = Game.GetPlaceCards(PlayerIndex).FilterCards(filter: x => x.Status.CardId == Card.Status.CardId).ToList();
+            if (cards.Count() == 0)
+            {
+                return 0;
+            }
+            foreach (var card in cards)
+            {
+                await card.Effect.Boost(3, Card);
+            }
+            return 0;
+        }
+    }
+}
