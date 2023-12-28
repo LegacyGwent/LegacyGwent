3b73fcbb3ecb08a49942eb96c66e924b8e288798
fraloow <50948805+fraloow@users.noreply.github.com>
2023-12-28 17:03:30 +0800

    Update BekkerSDarkMirror.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/BekkerSDarkMirror.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/Neutral/Silver/BekkerSDarkMirror.cs
@@ -6,7 +6,7 @@ namespace Cynthia.Card
 {
     [CardEffectId("13026")]//贝克尔的黑暗之镜
     public class BekkerSDarkMirror : CardEffect, IHandlesEvent<AfterCardHurt>
-    {//对场上最强的单位造成最多13点伤害（无视护甲），并使场上最弱的单位获得相同数值的增益。
+    {//对场上最强的单位造成最多11点伤害（无视护甲），并使场上最弱的单位获得相同数值的增益。
         public BekkerSDarkMirror(GameCard card) : base(card) { }
         private GameCard boostCard;
         public override async Task<int> CardUseEffect()
@@ -16,7 +16,7 @@ namespace Cynthia.Card
                 return 0;
             var damageCard = list.WhereAllHighest().Mess(RNG).First();
             boostCard = list.WhereAllLowest().Mess(RNG).First();
-            await damageCard.Effect.Damage(13, Card, BulletType.RedLight, true);//穿透伤害
+            await damageCard.Effect.Damage(11, Card, BulletType.RedLight, true);//穿透伤害
             return 0;
         }
 
