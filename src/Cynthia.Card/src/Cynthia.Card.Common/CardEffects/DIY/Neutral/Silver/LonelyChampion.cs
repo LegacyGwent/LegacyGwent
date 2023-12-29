581c2af83530514b49ea30f6ce0768c12ebf10af
fraloow <50948805+fraloow@users.noreply.github.com>
2023-12-29 13:04:01 +0800

    Update LonelyChampion.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Silver/LonelyChampion.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Silver/LonelyChampion.cs
@@ -6,11 +6,9 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("70120")] //孤独的勇士 LonelyChampion
-    public class LonelyChampion : CardEffect, IHandlesEvent<AfterTurnOver>
-    {
-        //回合结束时，若场上没有其它友军单位则获得4点增益。
-        public LonelyChampion(GameCard card) : base(card){}
-
+    public class LonelyChampion: CardEffect, IHandlesEvent<AfterTurnOver>
+    {//回合结束时，若场上没有其它友军单位则获得4点增益。
+        public LonelyChampion(GameCard card) : base(card) { }
         public async Task HandleEvent(AfterTurnOver @event)
         {
             var targets = Game.GetPlaceCards(PlayerIndex).Where(x => x != Card).ToList();
