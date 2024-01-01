ce5e1746cf3107df8be586ca2e9849881b3e9c36
fraloow <50948805+fraloow@users.noreply.github.com>
2024-01-01 16:13:50 +0800

    Update TorrentialRainStatus.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/RowEffect/TorrentialRainStatus.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/RowEffect/TorrentialRainStatus.cs
@@ -10,24 +10,28 @@ namespace Cynthia.Card
 
         public async Task HandleEvent(AfterTurnStart @event)
         {
-            /*原本的效果
-            var cards = AliveNotConceal.Mess(Game.RNG).Take(2);
-            foreach (var card in cards)
-            {
-                await card.Effect.Damage(1, null, damageType: DamageType.TorrentialRain);
-            }
-            */
-
             if (@event.PlayerIndex != PlayerIndex) return;
+            if (AliveNotConceal.Count() == 0) return;
+            for(var i = 0; i < 2 ;i++)
+            {
+                var cards = AliveNotConceal.Mess(Game.RNG).Take(1);
+                foreach (var card in cards)
+                {
+                    int exDamage = 0;
+                    int exDamage1 = Game.GetPlaceCards(Game.AnotherPlayer(PlayerIndex)).Where(x => x.Status.CardId == CardId.Otkell && !x.Status.IsLock && x.IsAliveOnPlance()).Count();
+                    if (exDamage1 != 0){exDamage = exDamage1;}
+                    await card.Effect.Damage(1 + exDamage, null, damageType: DamageType.TorrentialRain);
+                }
+            }
+
+            /*原本的效果
             var LowestCard = AliveNotConceal.WhereAllLowest();
             var HighestCard = AliveNotConceal.WhereAllHighest();
-            if (AliveNotConceal.Count() == 0) return;
-            var exDamage1 = Game.GetPlaceCards(Game.AnotherPlayer(PlayerIndex)).Where(x => x.Status.CardId == CardId.Otkell && !x.Status.IsLock && x.IsAliveOnPlance()).Count();
-            var exDamage2 = Game.GetPlaceCards(Game.AnotherPlayer(PlayerIndex)).Where(x => x.Status.CardId == CardId.Otkell && x.Status.HealthStatus < 0  && !x.Status.IsLock && x.IsAliveOnPlance()).Count();
-            var exDamage = exDamage1 + exDamage2;
-            await LowestCard.Mess(Game.RNG).First().Effect.Damage(1 + exDamage, null, damageType: DamageType.TorrentialRain);
-            await HighestCard.Mess(Game.RNG).First().Effect.Damage(1 + exDamage, null, damageType: DamageType.TorrentialRain);
-            
+            var exDamage1 = Game.GetPlaceCards(Game.AnotherPlayer(PlayerIndex), RowPosition).Where(x => x.Status.CardId == CardId.liaogen && !x.Status.IsLock && x.IsAliveOnPlance()).Count();
+            var exDamage2 = Game.GetPlaceCards(Game.AnotherPlayer(PlayerIndex)).Where(x => x.Status.CardId == CardId.liaogen && x.Status.HealthStatus < 0  && !x.Status.IsLock && x.IsAliveOnPlance()).Count();
+            await LowestCard.Mess(Game.RNG).First().Effect.Damage(1 + exDamage1 + exDamage2, null, damageType: DamageType.TorrentialRain);
+            await HighestCard.Mess(Game.RNG).First().Effect.Damage(1 + exDamage1 + exDamage2, null, damageType: DamageType.TorrentialRain);
+            */
         }
     }
 }
