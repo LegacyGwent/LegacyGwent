2455315865bc23ee079ca620e2f7743ea929ac6a
fraloow <50948805+fraloow@users.noreply.github.com>
2024-02-18 16:16:11 +0800

    Create Alp.cs

---
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Monsters/Copper/Alp.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Alsein.Extensions;
+
+namespace Cynthia.Card
+{
+    [CardEffectId("80033")] //吸血鬼女 Alp
+    public class Alp : CardEffect, IHandlesEvent<AfterTurnOver>
+    {
+        public Alp(GameCard card) : base(card){}
+        public async Task HandleEvent(AfterTurnOver @event)
+        {
+            if (@event.PlayerIndex != Card.PlayerIndex || !Card.Status.CardRow.IsOnPlace())
+            {
+                return;
+            }
+            var row = Game.RowToList(PlayerIndex, Card.Status.CardRow.Mirror()).IgnoreConcealAndDead();
+            if(row.Count == 0)
+            {
+                var cards = Game.GetPlaceCards(AnotherPlayer);
+                if (cards.Count() == 0)
+                {
+                    return;
+                }
+                await Card.Effect.Drain(1, cards.Mess().First());
+            }
+            return;
+        }
+    }
+}
