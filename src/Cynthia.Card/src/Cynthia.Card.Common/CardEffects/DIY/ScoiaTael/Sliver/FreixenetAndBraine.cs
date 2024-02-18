603a3f73a21e973c24d8099cb28600eed4daa27e
fraloow <50948805+fraloow@users.noreply.github.com>
2024-02-18 10:41:57 +0800

    Update FreixenetAndBraine.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/ScoiaTael/Sliver/FreixenetAndBraine.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/ScoiaTael/Sliver/FreixenetAndBraine.cs
@@ -4,27 +4,33 @@ using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-    [CardEffectId("70105")]//菲斯奈特与布雷恩 FreixenetAndBraine
+    [CardEffectId("70105")] // 菲斯奈特与布雷恩 FreixenetAndBraine
     public class FreixenetAndBraine : CardEffect
-    {//使一个单位的战力等同于自身战力。
-        public FreixenetAndBraine(GameCard card) : base(card) { }
+    {//随机打出1张铜色士兵牌，若牌组数量低于自身战力，则改为打出1张铜色牌。
+        public FreixenetAndBraine(GameCard card) : base(card){}
+
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
-            
-            var selectList = await Game.GetSelectPlaceCards(Card, selectMode: SelectModeType.AllRow);
-            if (!selectList.TrySingle(out var target))
+            int DeckCount = Game.PlayersDeck[Card.PlayerIndex].Count();
+            if (DeckCount >= Card.CardPoint())
             {
-                return 0;
+                var list = Game.PlayersDeck[PlayerIndex].Where(x => ((x.Status.Group == Group.Copper) && x.Status.Categories.Contains(Categorie.Soldier) && x.CardInfo().CardType == CardType.Unit)).ToList();
+                if (list.Count() == 0) return 0;
+                var moveCard = list.Mess(RNG).First();
+                await moveCard.MoveToCardStayFirst();
+                return 1;
             }
-            if(target.CardPoint()>Card.CardPoint())
+            else
             {
-                await target.Effect.Damage(target.CardPoint()-Card.CardPoint(), Card, BulletType.RedLight, true);//造成穿透伤害
+                var list = Game.PlayersDeck[Card.PlayerIndex]
+                .Where(x => (x.Status.Group == Group.Copper || x.Status.Group == Group.Silver)).Mess(RNG);
+                var result = await Game.GetSelectMenuCards
+                (Card.PlayerIndex, list.ToList(), 1, "选择打出一张牌");
+                if (result.Count() == 0) return 0;
+                await result.First().MoveToCardStayFirst();
+                return 1;
             }
-            else if(target.CardPoint()<Card.CardPoint())
-            {
-                await target.Effect.Boost(Card.CardPoint()-target.CardPoint(), Card);//增益
-            }
-            return 0;
-        }
+         }
     }
 }
+
