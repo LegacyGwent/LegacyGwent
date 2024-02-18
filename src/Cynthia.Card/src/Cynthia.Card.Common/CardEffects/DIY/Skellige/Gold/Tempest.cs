3f1187aa428c1909b00232f86026c0e7f2508324
fraloow <50948805+fraloow@users.noreply.github.com>
2024-02-18 13:24:24 +0800

    Update Tempest.cs

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Gold/Tempest.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Gold/Tempest.cs
@@ -1,47 +1,55 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Alsein.Extensions;
+using System;
+using System.Collections.Generic;
 
 namespace Cynthia.Card
 {
-    [CardEffectId("70093")]//暴风雨
+    [CardEffectId("70093")]//暴风雨Tempest
     public class Tempest : CardEffect
-    {//使灾厄下的所有受伤友军单位获得2点增益，并清除己方半场所有灾厄。
+    {
         public Tempest(GameCard card) : base(card) { }
         public override async Task<int> CardUseEffect()
         {
-            var tagetRows = Game.GameRowEffect[Card.PlayerIndex].Indexed()
-                .Where(x => x.Value.RowStatus == RowStatus.TorrentialRain)
+            var rowlist = new List<RowPosition>() { RowPosition.EnemyRow1, RowPosition.EnemyRow2, RowPosition.EnemyRow3, RowPosition.MyRow1, RowPosition.MyRow2, RowPosition.MyRow3 };
+            var rainlist = Game.GameRowEffect[Card.PlayerIndex].Indexed()
+                .Where(x => x.Value.RowStatus.IsHazard())
                 .Select(x => x.Key);
-            foreach (var rowIndex in tagetRows)
+
+            for(var i = 0; i<4;i++)
             {
-                // await Game.ApplyWeather(Card.PlayerIndex, rowIndex.IndexToMyRow(), RowStatus.None);
-                await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<SkelligeStormStatus>();
+                
+                var result = await Game.GetSelectRow(Card.PlayerIndex, Card, rowlist);
+               
+                if(result == RowPosition.EnemyRow1 || result == RowPosition.EnemyRow2 || result == RowPosition.EnemyRow3)
+                {
+                    var rows = result.Mirror().MyRowToIndex();
+                    if (Game.GameRowEffect[AnotherPlayer][rows].RowStatus == RowStatus.TorrentialRain)
+                    {
+                        await Game.GameRowEffect[AnotherPlayer][rows].SetStatus<SkelligeStormStatus>();
+                    }
+                    else
+                    {
+                        await Game.GameRowEffect[AnotherPlayer][rows].SetStatus<TorrentialRainStatus>();
+                    }
+                    
+                }
+                if(result == RowPosition.MyRow1 || result == RowPosition.MyRow2 || result == RowPosition.MyRow3)
+                {
+                    var rows = result.MyRowToIndex();
+                    if(Game.GameRowEffect[PlayerIndex][rows].RowStatus == RowStatus.TorrentialRain)
+                    {
+                        await Game.GameRowEffect[PlayerIndex][rows].SetStatus<SkelligeStormStatus>();
+                    }
+                    else
+                    {
+                        await Game.GameRowEffect[PlayerIndex][rows].SetStatus<TorrentialRainStatus>();
+                    }
+                }
+                rowlist.Remove(result);
             }
-            var tagetRow = Game.GameRowEffect[Game.AnotherPlayer(Card.PlayerIndex)].Indexed()
-                .Where(x => x.Value.RowStatus == RowStatus.TorrentialRain)
-                .Select(x => x.Key);
-            foreach (var rowIndex in tagetRow)
-            {
-                // await Game.ApplyWeather(Card.PlayerIndex, rowIndex.IndexToMyRow(), RowStatus.None);
-                await Game.GameRowEffect[AnotherPlayer][rowIndex].SetStatus<SkelligeStormStatus>();
-            }            
-            var targetRows = Game.GameRowEffect[Card.PlayerIndex].Indexed()
-                .Where(x => x.Value.RowStatus != RowStatus.SkelligeStorm)
-                .Select(x => x.Key);
-            foreach (var rowIndex in targetRows)
-            {
-                // await Game.ApplyWeather(Card.PlayerIndex, rowIndex.IndexToMyRow(), RowStatus.None);
-                await Game.GameRowEffect[PlayerIndex][rowIndex].SetStatus<TorrentialRainStatus>();
-            }
-            var targetRow = Game.GameRowEffect[Game.AnotherPlayer(Card.PlayerIndex)].Indexed()
-                .Where(x => x.Value.RowStatus != RowStatus.SkelligeStorm)
-                .Select(x => x.Key);
-            foreach (var rowIndex in targetRow)
-            {
-                // await Game.ApplyWeather(Card.PlayerIndex, rowIndex.IndexToMyRow(), RowStatus.None);
-                await Game.GameRowEffect[AnotherPlayer][rowIndex].SetStatus<TorrentialRainStatus>();
-            }
+            
             return 0;
         }
     }
