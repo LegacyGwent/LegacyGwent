4efb95f5e43fd6055ccaf8143a2d2774726c0ed3
Pippow <filippodealfaro@hotmail.fr>
2024-05-30 21:16:58 +0200

    first comit - corrupted flaminica - no tranlsations yet - for feedback purposes

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Copper/CorruptedFlaminca.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Copper/CorruptedFlaminca.cs
@@ -11,17 +11,34 @@ namespace Cynthia.Card
         public CorruptedFlaminca(GameCard card) : base(card) { }
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
-            var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == CardId.TorrentialRain).Mess(Game.RNG);
-            if (list.Count() == 0) { return 0; }
+            var switchCard = await Card.GetMenuSwitch
+            (
+                ("Discard Torrential Rain from your deck, then Spawn Torrential Rain in this and the opposite row"),
+                ("Damage self by 4 and Spawn Torrential Rain in this and the opposite row.")
+            );
+            if (switchCard == 0)
+            // start old effect
+            {
+                var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == CardId.TorrentialRain).Mess(Game.RNG);
+                if (list.Count() == 0) { return 0; }
 
-            var result = await Game.GetSelectMenuCards(Card.PlayerIndex, list.ToList(), 1, "选择丢弃一张牌");
+                var result = await Game.GetSelectMenuCards(Card.PlayerIndex, list.ToList(), 1, "选择丢弃一张牌");
 
-            if (result.Count() == 0) { return 0; }
+                if (result.Count() == 0) { return 0; }
 
-            await result.First().Effect.Discard(Card);
-            await Game.GameRowEffect[PlayerIndex][Card.Status.CardRow.MyRowToIndex()].SetStatus<TorrentialRainStatus>();
-            await Game.GameRowEffect[AnotherPlayer][Card.Status.CardRow.MyRowToIndex()].SetStatus<TorrentialRainStatus>();
-            return 0;
+                await result.First().Effect.Discard(Card);
+                await Game.GameRowEffect[PlayerIndex][Card.Status.CardRow.MyRowToIndex()].SetStatus<TorrentialRainStatus>();
+                await Game.GameRowEffect[AnotherPlayer][Card.Status.CardRow.MyRowToIndex()].SetStatus<TorrentialRainStatus>();
+                return 0;
+            }
+            // end old effect
+            // new effect
+            else if (switchCard == 1)
+            {
+                await Game.GameRowEffect[PlayerIndex][Card.Status.CardRow.MyRowToIndex()].SetStatus<TorrentialRainStatus>();
+                await Game.GameRowEffect[AnotherPlayer][Card.Status.CardRow.MyRowToIndex()].SetStatus<TorrentialRainStatus>();
+                await Card.Effect.Damage(4, Card);
+            }
         }
     }
 }
