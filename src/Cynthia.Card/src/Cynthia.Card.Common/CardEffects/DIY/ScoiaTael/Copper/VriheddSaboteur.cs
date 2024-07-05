59aca653a98378dd4b5d7ca1609276c891c27e18
Pippow <filippodealfaro@hotmail.fr>
2024-07-05 21:57:41 +0200

    removed useless lines of code from previous versions

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/ScoiaTael/Copper/VriheddSaboteur.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/ScoiaTael/Copper/VriheddSaboteur.cs
@@ -11,8 +11,6 @@ namespace Cynthia.Card
     public class VriheddSaboteur : Choose
     {//Play a random item from your deck or choose and play a scoiatael item from your deck. Boost self by 3 if the item kills a card
         public VriheddSaboteur(GameCard card) : base(card){}
-        private bool isplayturn = false;
-        private bool isneedboost = false;
         // private bool isneedboost = false;
         protected override async Task<int> UseMethodByChoice(int switchCard)
         {
@@ -37,20 +35,16 @@ namespace Cynthia.Card
         }
         // play a random item from your deck
         private async Task<int> FUNCTION1()
-        {
-            isplayturn = true;    
+        { 
             var list = Game.PlayersDeck[PlayerIndex].Where(x => ((x.Status.Group == Group.Copper) && x.Status.Categories.Contains(Categorie.Item) && x.CardInfo().CardType == CardType.Special)).ToList();
             if (list.Count() == 0) return 0;
             var item = list.Mess(RNG).First();
             await item.MoveToCardStayFirst();
-            while (isneedboost)
-                await Boost(3, Card);
             return 1;
         }
             // choose and play a scoiatael item from your deck
         private async Task<int> FUNCTION2()
         {
-            isplayturn = true; 
             var list = Game.PlayersDeck[PlayerIndex].Where(x => ((x.Status.Group == Group.Copper) && x.Status.Categories.Contains(Categorie.Item) && x.CardInfo().CardType == CardType.Special && x.CardInfo().Faction == Faction.ScoiaTael)).ToList();
             var result = await Game.GetSelectMenuCards(Card.PlayerIndex, list.ToList(), 1, "");
             if (result.Count() == 0) return 0;
