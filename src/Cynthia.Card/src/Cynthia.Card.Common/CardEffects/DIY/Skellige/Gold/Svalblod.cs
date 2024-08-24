055296bee49bd1b29c6feac79d75fbe15186b9bc
Pippow <filippodealfaro@hotmail.fr>
2024-08-24 12:28:58 +0200

    syntax error fixed

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Gold/Svalblod.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Skellige/Gold/Svalblod.cs
@@ -29,7 +29,7 @@ namespace Cynthia.Card
                     if (card.CardPoint() >=3)
                         await card.Effect.Damage(2, Card);
                         await card.Effect.Strengthen(2, Card);
-                    await card.Effect.Strengthen(2, Card)
+                    await card.Effect.Strengthen(2, Card);
                     await card.Effect.Damage(2, Card);
                 } 
             return 0;
