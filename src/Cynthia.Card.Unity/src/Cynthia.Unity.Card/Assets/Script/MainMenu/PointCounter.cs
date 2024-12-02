0bb42ea21ab2d71a287879c1f13b90544bd117a2
Pippow <filippodealfaro@hotmail.fr>
2024-12-02 10:53:44 +0100

    sauvegarde, le counter donne 2 en permanence

---
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Script/MainMenu/PointCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cynthia.Card.Client;
+using Assets.Script.Localization;
+
+
+
+public class PointCounter : MonoBehaviour
+{
+    
+    [SerializeField] counterHUD counterHUD;
+    public Text PlayerCounttNum;
+    //-----------------------------------
+    private LocalizationService _translator;
+    private GwentClientService server;
+    
+    
+    private void Start ()
+    {
+        int timer = 0;
+        StartCoroutine (CountPoints() );
+    }
+    private IEnumerator CountPoints()
+    {
+        while (true)
+        {
+            counterHUD.Points = 2;
+        yield return new WaitForSeconds(2);
+        }
+    }
+}
---
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Script/MainMenu/counterHUD.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class counterHUD : MonoBehaviour
+{
+    [SerializeField] Text pointText;
+    int points = 0;
+    private void Awake ()
+    {
+        UpdateHUD();
+    }
+    public int Points
+    {
+        get {return points;}
+        set {
+            points = value;
+            UpdateHUD();
+            }
+    }
+    private void UpdateHUD()
+    {
+        pointText.text = points.ToString ();
+    }
+    
+
+}
