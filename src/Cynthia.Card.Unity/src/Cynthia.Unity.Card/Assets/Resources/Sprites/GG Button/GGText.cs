2ae409f254f3983c90f500bf14c33bf67e14d77f
Pippow <filippodealfaro@hotmail.fr>
2024-12-06 14:17:13 +0100

    GG message always displays - Correct Opponent Name

---
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Resources/Sprites/GG Button/GGText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GGText : MonoBehaviour
+{
+    [SerializeField] Text OpponentText;        
+    
+    string opponent = "";
+
+    private void Awake ()
+    {
+        UpdateHUD();
+    }
+    public string Opponent
+    {
+        get {return opponent;}
+        set {
+            opponent = value;
+            UpdateHUD();
+            }
+    }
+    
+    private void UpdateHUD()
+    {
+        OpponentText.text = opponent.ToString ();
+    }
+    
+
+}
