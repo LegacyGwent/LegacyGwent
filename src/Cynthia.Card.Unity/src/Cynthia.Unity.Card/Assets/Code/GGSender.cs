6a0f6b4c13010506ca1c98c1daaf1caa34d7d09d
Pippow <48692951+Pippow@users.noreply.github.com>
2024-12-21 03:51:11 +0100

    redo checks

--- src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Code/GGSender.cs
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Code/GGSender.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class GGSender
+public static class GGSender // record the name of the person that sent gg
 {
     public static string ggsender { get; set; }
 }
