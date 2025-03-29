47a2ad8d343a65cef39ed99d3397336789b22f0f
Pippow <filippodealfaro@hotmail.fr>
2025-03-29 21:17:27 +0100

    typo fix

--- src/Cynthia.Card/src/Cynthia.Card.Server/Services/GwentGameService/GwentServerService.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Server/Services/GwentGameService/GwentServerService.cs
@@ -151,7 +151,6 @@ namespace Cynthia.Card.Server
         public async Task<bool> UpdateAvatar(string playername, string AvatarID) // updates the avatar of the user
         {
         _databaseService.UpdateAvatar(playername, AvatarID);
-        user.CurrentAvatar = AvatarID;
         return true;
         }
 
