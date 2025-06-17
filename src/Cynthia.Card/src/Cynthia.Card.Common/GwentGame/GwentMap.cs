81836a06075bbfca2cbc8ba0f8df5efc0dde0f24
Pippow <filippodealfaro@hotmail.fr>
2025-06-17 19:32:30 +0200

    translations - pr finished

--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
@@ -10760,7 +10760,7 @@ namespace Cynthia.Card
                 {
                     CardId ="64028", //Dimun Corsair
                     Name="迪门家族海贼",
-                    Strength=3,
+                    Strength=1,
                     Group=Group.Copper,
                     Faction = Faction.Skellige,
                     CardUseInfo = CardUseInfo.MyRow,
@@ -13381,7 +13381,7 @@ namespace Cynthia.Card
                     IsDerive = false,
                     Categories = new Categorie[]{ Categorie.Cultist},
                     Flavor = "史凯利格容不下罪犯和无赖……他们的罪行必须偿还。",
-                    Info = "部署：对一个敌军单位造成两倍于场上“倾盆大雨”数量的伤害。",
+                    Info = "部署：对一个敌军单位造成两倍于场上“倾盆大雨”数量的伤害，并获得两倍于场上“倾盆大雨”数量的护甲。",
                     CardArtsId = "202288",
 					LinkedCards=new List<String> {},
                 }
