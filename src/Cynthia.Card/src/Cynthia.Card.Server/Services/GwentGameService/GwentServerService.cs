79601afe02c180f197da4eab8d22778d26f696f2
Pippow <filippodealfaro@hotmail.fr>
2025-08-04 05:41:01 +0200

    updated client latest version number on server

--- src/Cynthia.Card/src/Cynthia.Card.Server/Services/GwentGameService/GwentServerService.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Server/Services/GwentGameService/GwentServerService.cs
@@ -401,7 +401,7 @@ namespace Cynthia.Card.Server
         public async Task<string> GetLatestVersion(string connectionId)
         {
             await Task.CompletedTask;
-            return "2.0.1";
+            return "2.0.3";
         }
 
         public async Task<string> GetNotes(string connectionId)
@@ -656,7 +656,7 @@ When other players are available, player matchmaking will be prioritized. Add #f
         public async Task<string> GetLatestClientVersion(string connectionId)
         {
             await Task.CompletedTask;
-            return @"2.0.2";
+            return @"2.0.3";
         }
         //-------------------------------------------------------------------------
         public int GetUserCount()
