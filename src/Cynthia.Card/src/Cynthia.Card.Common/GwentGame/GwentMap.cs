c0390f57932af5e484b7a2bbb79e52de13a8f363
Pippow <filippodealfaro@hotmail.fr>
2025-08-04 16:36:16 +0200

    first commit

--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
@@ -8,7 +8,7 @@ namespace Cynthia.Card
     public static class GwentMap
     {
         //更新CardMap内容请务必将CardMapVersion更新
-        public static Version CardMapVersion { get; } = new Version(1, 0, 0, 122);
+        public static Version CardMapVersion { get; } = new Version(1, 0, 0, 125);
         public static IDictionary<string, int> CardIdMap { get; set; }
         public static string[] CardIdIndexMap { get; set; }
 
@@ -14033,7 +14033,7 @@ namespace Cynthia.Card
                     IsDerive = false,
                     Categories = new Categorie[]{ },
                     Flavor = "有些乌鸦会说人话。而有些人，学会了乌鸦的语言。",
-                    Info = "丢弃牌组中的三张铜色特殊牌，己方每丢弃一张特殊牌时，生成一只乌鸦。",
+                    Info = "临时削弱",
                     CardArtsId = "202514",
 					LinkedCards=new List<String> {"70136"},
                 }
