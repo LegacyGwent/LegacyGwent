0e37ca2bcc3f0d2c13ac014624e021e3f3d60c49
Pippow <filippodealfaro@hotmail.fr>
2025-08-04 04:23:00 +0200

    fixed rope bug and put limit back to 90

--- src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Script/GamePlay/GameUIInfo/RopeController.cs
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Script/GamePlay/GameUIInfo/RopeController.cs
@@ -16,37 +16,39 @@ public class RopeController : MonoBehaviour
     private float endWaitTime = 0.5f;
     private int shakeRange = 1;
     public bool isRunning = false;
-    public float remainingTime = 120f;
+    public float remainingTime = 90f;
     public int skipedTurns=0;
+    private int count = 0; // count for fixed update to record if it's the first time it is called during a turn
+    private int oldCount = 0;
     public async void Surrender()
     {
         await DependencyResolver.Container.Resolve<GwentClientService>().Surrender();
     }
-    public void StartRopeTimer(float totalTime = 120f)
+    public void StartRopeTimer(float totalTime = 90f)
     {
         rope.gameObject.SetActive(false); // is not active until ropeTime
         remainingTime = totalTime;
         isRunning = true;
     }
-    public void StopRopeTimer()
+    public void StopRopeTimer(int count)
     {
         GameEvent gameEvent = FindObjectOfType<GameEvent>();
-        
-        
+
+
         // Check if it's the local player's turn by checking the coin state
         bool isLocalPlayerTurn = gameEvent.Coin.IsMyRound;
-        
-        if (isLocalPlayerTurn)
+        if (isLocalPlayerTurn && count != (oldCount +1))
         {
+            // If it's the local player's turn and this is the first time the rope timer is called during this turn
+            // We assume that the local player has triggered the timeout
+            // This is to prevent the rope timer from being triggered multiple times in a single turn
             Debug.Log("Local player triggered the rope timer timeout");
             skipedTurns = skipedTurns + 1;
             Debug.Log("Skipped Turns: " + skipedTurns);
-
-            // Handle local player timeout
             gameEvent.shorterTimer = true;
-        }        
+        }
         Debug.Log("TIMED-OUT");
-        
+
         if (skipedTurns >= 3)
         {
             Surrender();
@@ -54,6 +56,7 @@ public class RopeController : MonoBehaviour
         rope.gameObject.SetActive(false);
         remainingTime = 0;
         isRunning = false;
+        oldCount = count;
     }
     private void FixedUpdate()
     {
@@ -61,6 +64,7 @@ public class RopeController : MonoBehaviour
         {
             return;
         }
+        count += 1;
         remainingTime -= Time.deltaTime;
         if (remainingTime < ropeTime + endWaitTime)
         {
@@ -75,7 +79,7 @@ public class RopeController : MonoBehaviour
         }
         if (remainingTime <= 0)
         {
-            StopRopeTimer();
+            StopRopeTimer(count);
         }
     }
     void Update()
