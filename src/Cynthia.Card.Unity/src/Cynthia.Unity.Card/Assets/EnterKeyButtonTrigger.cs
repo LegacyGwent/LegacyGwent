2b976e1192d83eb2b283ee833eb9bcca04449ad4
kajtek14102 <kajtek14102@gmail.com>
2025-09-02 01:24:39 +0200

    enter to start a game

---
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/EnterKeyButtonTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterKeyButtonTrigger : MonoBehaviour
+{
+    public Button targetButton; // Assign your button in the Inspector
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            if (targetButton != null && targetButton.interactable)
+            {
+                targetButton.onClick.Invoke();
+            }
+        }
+    }
+}
