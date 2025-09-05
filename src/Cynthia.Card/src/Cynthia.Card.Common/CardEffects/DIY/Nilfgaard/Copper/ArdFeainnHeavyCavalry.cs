85f3d6429f5889a7857f985583a5d030e19c5afc
Pippow <filippodealfaro@hotmail.fr>
2025-09-05 16:49:46 +0200

    bumped GwentMap and fixed ArdFainHeavyCavalry

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Nilfgaard/Copper/ArdFeainnHeavyCavalry.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Nilfgaard/Copper/ArdFeainnHeavyCavalry.cs
@@ -5,6 +5,7 @@ namespace Cynthia.Card
     [CardEffectId("202453")]//日轮之师重骑兵 ArdFeainnHeavyCavalry
     public class ArdFeainnHeavyCavalry : CardEffect, IHandlesEvent<AfterUnitDown>, IHandlesEvent<AfterRoundOver>
     {//Boost self by 4 when a leader is played. Decrease the boost by 1 for each time this card has been boosted by its own ability previously this round - 2 armor.
+        public ArdFeainnHeavyCavalry(GameCard card) : base(card) { }
         private int boostValue = 4;
 
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
@@ -13,7 +14,7 @@ namespace Cynthia.Card
             return 0;
         }
 
-        public ArdFeainnHeavyCavalry(GameCard card) : base(card) { }
+        
         public async Task HandleEvent(AfterUnitDown @event)
         {
             if (@event.Target.Status.Group == Group.Leader && boostValue > 0)
