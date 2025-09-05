93c32a4076dbf3801bff3777bfc1d46cb5932a48
Pippow <filippodealfaro@hotmail.fr>
2025-09-05 17:22:16 +0200

    ArdFea...; cavalry fixed

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Nilfgaard/Copper/ArdFeainnHeavyCavalry.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Nilfgaard/Copper/ArdFeainnHeavyCavalry.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace Cynthia.Card
 {
-    [CardEffectId("202453")]//日轮之师重骑兵 ArdFeainnHeavyCavalry
+    [CardEffectId("70184")]//日轮之师重骑兵 ArdFeainnHeavyCavalry
     public class ArdFeainnHeavyCavalry : CardEffect, IHandlesEvent<AfterUnitDown>, IHandlesEvent<AfterRoundOver>
     {//Boost self by 4 when a leader is played. Decrease the boost by 1 for each time this card has been boosted by its own ability previously this round - 2 armor.
         public ArdFeainnHeavyCavalry(GameCard card) : base(card) { }
@@ -10,14 +10,14 @@ namespace Cynthia.Card
 
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
-            await Card.Effect.Armor(2, Card);
+            await Card.Effect.Armor(3, Card);
             return 0;
         }
 
         
         public async Task HandleEvent(AfterUnitDown @event)
         {
-            if (@event.Target.Status.Group == Group.Leader && boostValue > 0)
+            if (@event.Target.Status.Group == Group.Leader && boostValue > 0 && Card.Status.CardRow.IsOnPlace())
             {
                 await Card.Effect.Boost(boostValue, Card);
                 boostValue -= 1;
