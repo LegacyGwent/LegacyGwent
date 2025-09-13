3250be358f42fac2a3dc5839e34810864a930adb
kajtek14102 <kajtek14102@gmail.com>
2025-09-13 12:15:17 +0200

    startScreen fix

---
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/StartScreenStarter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenStarter : MonoBehaviour
+{
+    public GameObject StartScreen;
+    
+    void Start()
+    {
+        StartScreen.SetActive(true);
+    }
+
+}
