e3bb88a757d175dd45b3d355b19884b454948d9a
kajtek14102 <kajtek14102@gmail.com>
2026-02-02 16:09:05 +0100

    MoveButton (nonFunctional)

---
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/DeckMooveToggle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckMooveToggle : MonoBehaviour
+{
+    public static bool DeckMooveMode = false;
+
+    private Button button;
+    private Image image;
+
+    public Sprite offSprite; // Button
+    public Sprite onSprite;  // ButtonOn
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+        image = button.targetGraphic as Image;
+
+        ApplyVisualState();
+    }
+
+    public void ToggleDeckMooveMode()
+    {
+        DeckMooveMode = !DeckMooveMode;
+        Debug.Log("DeckMooveMode changed to: " + DeckMooveMode);
+
+        ApplyVisualState();
+    }
+
+    void ApplyVisualState()
+    {
+        image.sprite = DeckMooveMode ? onSprite : offSprite;
+    }
+}
