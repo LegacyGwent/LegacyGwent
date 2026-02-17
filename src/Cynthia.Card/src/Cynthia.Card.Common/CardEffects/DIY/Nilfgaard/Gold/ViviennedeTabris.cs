2ada4b43f0e78a05e3c3840863729023265fe282
Pippow <filippodealfaro@hotmail.fr>
2026-02-17 13:08:24 +0100

    reverted Vivienne change

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Nilfgaard/Gold/ViviennedeTabris.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Nilfgaard/Gold/ViviennedeTabris.cs
@@ -25,10 +25,7 @@ namespace Cynthia.Card
             }
             var id = targetList[targetIndex].CardId;
             await target.Effect.Transform(id, Card);
-            var cards = await Game.GetSelectPlaceCards(Card, selectMode: SelectModeType.MyRow);
-            if (!cards.Any()) return 0;
-            var card = cards.Single();
-            await card.Effect.Boost(2, Card);
+            await target.Effect.Boost(2, Card);
             return 0;
         }
     }
