0868ec2677cd681497721c7792a28aa91839de20
Pippow <filippodealfaro@hotmail.fr>
2026-05-20 18:54:36 +0200

    fixed red riders

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Monsters/Copper/RedRider.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Monsters/Copper/RedRider.cs
@@ -5,10 +5,10 @@ using Alsein.Extensions;
 namespace Cynthia.Card
 {
     [CardEffectId("70083")]//红骑士
-    public class RedRider : CardEffect, IHandlesEvent<BeforeCardToCemetery>, IHandlesEvent<AfterCardToCemetery>
-    {//每当有位于“刺骨冰霜”之下的敌军单位被摧毁时，从牌组召唤1张它的同名牌。
+    public class RedRider : CardEffect, IHandlesEvent<BeforeCardToCemetery>, IHandlesEvent<AfterCardToCemetery>, IHandlesEvent<AfterTurnOver>
+    {//每当有位于"刺骨冰霜"之下的敌军单位被摧毁时，从牌组召唤1张它的同名牌。
         public RedRider(GameCard card) : base(card) { }
-        private bool IsToSummon = false;
+        public bool IsToSummon = false;
         public async Task HandleEvent(BeforeCardToCemetery @event)
         {
             if (@event.Target.Status.Type == CardType.Unit && @event.Target.PlayerIndex == AnotherPlayer && Card.Status.CardRow.IsInDeck())
@@ -21,7 +21,7 @@ namespace Cynthia.Card
                 bool isInFrost = Game.GameRowEffect[@event.Target.PlayerIndex][@event.DeathLocation.RowPosition.MyRowToIndex()].RowStatus == RowStatus.BitingFrost;
                 if (isInFrost)
                 {
-                    var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == Card.Status.CardId).ToList();
+                    var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == Card.Status.CardId && (x.Effect as RedRider)?.IsToSummon == false).ToList();
                     if (list.Count() == 0)
                     {
                         return;
@@ -35,17 +35,16 @@ namespace Cynthia.Card
             }
         }
         public async Task HandleEvent(AfterCardToCemetery @event)
-        {//move the actual summon to a separate event to avoid summoning all copies of this card when only one enemy dies to frost
-            if (!IsToSummon)
-            {
-                return;
-            }
-            if (@event.Target.Status.Type == CardType.Unit && @event.Target.PlayerIndex == AnotherPlayer && Card.Status.CardRow.IsInDeck())
+        {
+            if (IsToSummon)
             {
                 IsToSummon = false;
                 await Card.Effect.Summon(Game.GetRandomCanPlayLocation(Card.PlayerIndex, true), Card);
             }
-            return;
+        }
+        public async Task HandleEvent(AfterTurnOver @event)
+        {
+           IsToSummon = false; 
         }
 
     }
