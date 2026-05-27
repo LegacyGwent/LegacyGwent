1c2201a9c4757cadf4993fe575115d2a368a00c0
kajtek14102 <kajtek14102@gmail.com>
2026-05-27 11:09:58 +0200

    script to add version file to client

---
+++ src/Cynthia.Card.Unity/src/Cynthia.Unity.Card/Assets/Editor/BuildVersionWriter.cs
@@ -0,0 +1,19 @@
+// Assets/Editor/BuildVersionWriter.cs
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using System.IO;
+
+public class BuildVersionWriter : IPostprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        string outputDir = Path.GetDirectoryName(report.summary.outputPath);
+        string versionFile = Path.Combine(outputDir, "version.txt");
+        File.WriteAllText(versionFile, Application.version);
+        Debug.Log("BuildVersionWriter: version.txt written -> " + Application.version);
+    }
+}
