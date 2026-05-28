eb5a2a09a406829e45e30f060dd1686d361e8db1
Pippow <filippodealfaro@hotmail.fr>
2026-05-28 13:41:49 +0200

    simplified code further and added back Chinese commentary

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Monsters/Copper/RedRider.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Monsters/Copper/RedRider.cs
@@ -6,8 +6,7 @@ namespace Cynthia.Card
 {
     [CardEffectId("70083")]//红骑士
     public class RedRider : CardEffect, IHandlesEvent<BeforeCardToCemetery>
-    {// When an enemy unit is moved to the graveyard from a row with Biting Frost, if this card is in your deck, summon it on a random row.
-        public bool IsToSummon = false;
+    {//每当有位于"刺骨冰霜"之下的敌军单位被摧毁时，从牌组召唤1张它的同名牌。
         public RedRider(GameCard card) : base(card) { }
         public async Task HandleEvent(BeforeCardToCemetery @event)
         {
@@ -21,7 +20,7 @@ namespace Cynthia.Card
                 bool isInFrost = Game.GameRowEffect[@event.Target.PlayerIndex][@event.DeathLocation.RowPosition.MyRowToIndex()].RowStatus == RowStatus.BitingFrost;
                 if (isInFrost)
                 {
-                    var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == Card.Status.CardId && (x.Effect as RedRider)?.IsToSummon == false).ToList();
+                    var list = Game.PlayersDeck[Card.PlayerIndex].Where(x => x.Status.CardId == Card.Status.CardId).ToList();
                     if (list.Count() == 0)
                     {
                         return;
