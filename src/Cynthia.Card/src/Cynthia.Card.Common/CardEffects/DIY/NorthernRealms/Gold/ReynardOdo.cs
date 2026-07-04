3787b5064355f1117d8324c14e0fd677008f0be0
Pippow <filippodealfaro@hotmail.fr>
2026-07-04 18:10:05 +0200

    adjusted Reynard

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/NorthernRealms/Gold/ReynardOdo.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/NorthernRealms/Gold/ReynardOdo.cs
@@ -9,13 +9,19 @@ namespace Cynthia.Card
     {//雷纳德·奥多
     // 回合结束时，若至少具有3点增益，将增益转化为护甲，并使同排没有护甲的友军单位获得2点增益。
     // Reynard Odo:
-    // On turn end, if it has at least 3 Boosts, convert its Boosts into Armor and Boost all allies on the same row without Armor by 2.
+    // On turn end, if it has at least 3 Boosts, convert its Boosts into Armor and Boost all allies on the same row without Armor by 2. Repeat up to 2 times.
 
 
         public ReynardOdo(GameCard card) : base(card) { }
+
+        public override async Task<int> CardUseEffect()
+        {
+            await Card.Effect.SetCountdown(value: 3);
+            return 0;
+        }
         public async Task HandleEvent(AfterTurnOver @event)
         {
-            if (@event.PlayerIndex != Card.PlayerIndex || !Card.Status.CardRow.IsOnPlace())
+            if (@event.PlayerIndex != Card.PlayerIndex || !Card.Status.CardRow.IsOnPlace() || Card.Status.Countdown <= 0)
             {
                 return;
             }
@@ -28,6 +34,7 @@ namespace Cynthia.Card
                 {
                     await card.Effect.Boost(1, Card);
                 }
+                await Card.Effect.SetCountdown(offset: -1);
                 return;
             }
             return;
