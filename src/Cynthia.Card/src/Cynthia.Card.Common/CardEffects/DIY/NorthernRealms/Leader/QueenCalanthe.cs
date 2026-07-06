a1394858acf28940e2cb129c89f9d9475a4884d0
Pippow <filippodealfaro@hotmail.fr>
2026-07-06 16:44:16 +0200

    The card summoned by Calanthe now resolves before a card has to be returned to the deck

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/NorthernRealms/Leader/QueenCalanthe.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/NorthernRealms/Leader/QueenCalanthe.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
@@ -7,9 +8,13 @@ namespace Cynthia.Card
     public class QueenCalanthe : CardEffect
     { 
         public QueenCalanthe(GameCard card) : base(card) { }
+
+        private bool playedFromDeck = false;
+
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {   
-            
+            playedFromDeck = false;
+
             // Play the lowest non-gold unit from your deck.
             var list = Game.PlayersDeck[PlayerIndex]
             .Where(x => (x.Status.Group == Group.Copper || x.Status.Group == Group.Silver) &&
@@ -17,13 +22,23 @@ namespace Cynthia.Card
             if (list.Count() == 0) return 0;
             var moveCard = list.Mess(RNG).First();
             await moveCard.MoveToCardStayFirst();
+            playedFromDeck = true;
+            return 1;
+        }
 
-            //then shuffle a non-gold ally back to your deck without changing its power
+        public override async Task CardDownEffect(bool isSpying, bool isReveal)
+        {
+            if (!playedFromDeck)
+            {
+                return;
+            }
+            playedFromDeck = false;
+
+            // Then shuffle a non-gold ally back to your deck without changing its power.
             var result = await Game.GetSelectPlaceCards(Card, filter: x => x.Status.Group == Group.Copper || x.Status.Group == Group.Silver, selectMode: SelectModeType.MyRow);
-            if (result.Count() == 0) return 0;
+            if (result.Count() == 0) return;
             var mycard = result.Single();
             await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck, RNG.Next(0, Game.PlayersDeck[Card.PlayerIndex].Count)), mycard, refreshPoint: false);
-            return 1;
         }
     }
 }
