4f5956b06b6345d49990ec4b0800701a190a5993
Pippow <filippodealfaro@hotmail.fr>
2026-07-23 22:45:23 +0200

    Reverted Calanthe

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/NorthernRealms/Leader/QueenCalanthe.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/NorthernRealms/Leader/QueenCalanthe.cs
@@ -1,44 +1,27 @@
 using System.Linq;
 using System.Threading.Tasks;
-using Alsein.Extensions;
 
 namespace Cynthia.Card
 {
-    [CardEffectId("70179")]//Deploy: Play the lowest non-gold unit from your deck, thenshuffle a non-gold ally back to your deck without changing its power.
+    [CardEffectId("70179")]//Deploy: shuffle a non-gold ally back to your deck without changing its power, then play the lowest non-gold unit from your deck.
     public class QueenCalanthe : CardEffect
     { 
         public QueenCalanthe(GameCard card) : base(card) { }
-
-        private bool playedFromDeck = false;
-
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
-        {   
-            playedFromDeck = false;
+        {   //Deploy: shuffle a non-gold ally back to your deck without changing its power
+            var result = await Game.GetSelectPlaceCards(Card, filter: x => x.Status.Group == Group.Copper || x.Status.Group == Group.Silver, selectMode: SelectModeType.MyRow);
+            if (result.Count() == 0) return 0;
+            var mycard = result.Single();
 
-            // Play the lowest non-gold unit from your deck.
+            await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck, RNG.Next(0, Game.PlayersDeck[Card.PlayerIndex].Count)), mycard, refreshPoint: false);
+            // then play the lowest non-gold unit from your deck.
             var list = Game.PlayersDeck[PlayerIndex]
             .Where(x => (x.Status.Group == Group.Copper || x.Status.Group == Group.Silver) &&
                     x.CardInfo().CardType == CardType.Unit).WhereAllLowest().ToList();
             if (list.Count() == 0) return 0;
             var moveCard = list.Mess(RNG).First();
             await moveCard.MoveToCardStayFirst();
-            playedFromDeck = true;
             return 1;
-        }
-
-        public override async Task CardDownEffect(bool isSpying, bool isReveal)
-        {
-            if (!playedFromDeck)
-            {
-                return;
-            }
-            playedFromDeck = false;
-
-            // Then shuffle a non-gold ally back to your deck without changing its power.
-            var result = await Game.GetSelectPlaceCards(Card, filter: x => x.Status.Group == Group.Copper || x.Status.Group == Group.Silver, selectMode: SelectModeType.MyRow);
-            if (result.Count() == 0) return;
-            var mycard = result.Single();
-            await Game.ShowCardMove(new CardLocation(RowPosition.MyDeck, RNG.Next(0, Game.PlayersDeck[Card.PlayerIndex].Count)), mycard, refreshPoint: false);
         }
     }
 }
--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
@@ -14982,7 +14982,7 @@ namespace Cynthia.Card
                     Categories = new Categorie[]{ Categorie.Leader,Categorie.Cintra},
                     CrewCount = 1,
                     Flavor = "人们叫她“辛特拉的母狮”。这可不是毫无根据的。",
-                    Info = "从牌组打出一个最弱的银色/铜色单位牌，然后保留一个银色/铜色友方单位的战力不变，并将其收回牌组。",
+                    Info = "保留一个银色/铜色友方单位的战力不变，并将其收回牌组，然后从牌组打出一个最弱的银色/铜色单位牌。",
                     CardArtsId = "18340",
                     LinkedCards=new List<String> {},
                 }
