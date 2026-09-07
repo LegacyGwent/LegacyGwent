3ecc57e4f28740b221b7471496f149b45c5b0cb5
Pippow <48692951+Pippow@users.noreply.github.com>
2026-09-07 02:56:17 +0200

    cleanup of previous PR (#814)

--- src/Cynthia.Card/src/Cynthia.Card.Server/Startup.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Server/Startup.cs
@@ -34,7 +34,6 @@ namespace Cynthia.Card.Server
             services.AddSingleton<Random>(x => new Random((int)DateTime.UtcNow.Ticks));
             // Add the scheduled event service
             services.AddHostedService<ScheduledEventService>();
-            services.AddHostedService<DatabaseMigrationService>();
             services.AddAntDesign();
             services.AddBlazoredLocalStorage();
             services.AddTransient<IMongoClient, MongoClient>(x => new MongoClient(GetConnectionString()));
